1b18bad0601c7a48ae0dd9243dd864b3d246cb04
Sam Tolkin <sbtolkin4@gmail.com>
2016-08-25 22:36:49 -0400

    Issue working on gameView
    Ran into a potential backend issue with actually being ready for a game
    and swtiching game view

--- assets/js/angular-controllers/gamesController.js
+++ assets/js/angular-controllers/gamesController.js
@@ -1,4 +1,4 @@
-app.controller("lobbyController", ['$scope', '$http', function ($scope, $http) {
+app.controller("gamesController", ['$scope', '$http', function ($scope, $http) {
 	var menu = $scope.menu;
 	var self = this;
 }]);
