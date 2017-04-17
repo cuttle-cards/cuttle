8e3bab2ebf9961205da8adc321593827649d1e26
ryan <itsalaidbacklife@gmail.com>
2017-04-17 12:09:41 +0200

    Reverted config in models.js

--- config/models.js
+++ config/models.js
@@ -29,7 +29,7 @@ module.exports.models = {
   ***************************************************************************/
 
   migrate: 'drop',
-  connection: 'sqlCuttle',
+  connection: 'cuttleSql',
 
 
 };
