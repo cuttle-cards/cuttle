44e94a760bd201b9631b260c328b07f01d47f072
= <=>
2020-10-12 17:23:51 -0400

    chore: remove stale import to gameService, which is autowired globally w/o import

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -9,7 +9,6 @@
  // Dependencies //
  //////////////////
 var Promise = require('bluebird');
-const gameService = require('../services/gameService');
 var gameAPI = sails.hooks['customgamehook'];
 var userAPI = sails.hooks['customuserhook'];
 
