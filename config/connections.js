ce292931e2bce3d8e029685a05efc983af9aced8
= <=>
2021-06-27 18:15:11 -0400

    chore: update comments

--- config/connections.js
+++ config/connections.js
@@ -30,17 +30,17 @@
  }
 module.exports.connections = {
 
-  // /***************************************************************************
-  // *                                                                          *
-  // * Local disk storage for DEVELOPMENT ONLY                                  *
-  // *                                                                          *
-  // * Installed by default.                                                    *
-  // *                                                                          *
-  // ***************************************************************************/
+  /***************************************************************************
+  *                                                                          *
+  * Local disk storage for DEVELOPMENT ONLY                                  *
+  *                                                                          *
+  * Installed by default.                                                    *
+  *                                                                          *
+  ***************************************************************************/
   localDiskDb: {
     adapter: 'sails-disk'
   },
-  
+
   sqlHeroku: {
     adapter: 'sails-postgresql',
     ssl: true,
@@ -51,55 +51,5 @@ module.exports.connections = {
     password: dbUrl.auth.split(':')[1],
     port: dbUrl.port,
   },
-
-  // /***************************************************************************
-  // *                                                                          *
-  // * MySQL is the world's most popular relational database.                   *
-  // * http://en.wikipedia.org/wiki/MySQL                                       *
-  // *                                                                          *
-  // * Run: npm install sails-mysql                                             *
-  // *                                                                          *
-  // ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER',
-  //   password: 'YOUR_MYSQL_PASSWORD',
-  //   database: 'YOUR_MYSQL_DB'
-  // },
-
-  // /***************************************************************************
-  // *                                                                          *
-  // * MongoDB is the leading NoSQL database.                                   *
-  // * http://en.wikipedia.org/wiki/MongoDB                                     *
-  // *                                                                          *
-  // * Run: npm install sails-mongo                                             *
-  // *                                                                          *
-  // ***************************************************************************/
-
-
-  // /***************************************************************************
-  // *                                                                          *
-  // * PostgreSQL is another officially supported relational database.          *
-  // * http://en.wikipedia.org/wiki/PostgreSQL                                  *
-  // *                                                                          *
-  // * Run: npm install sails-postgresql                                        *
-  // *                                                                          *
-  // *                                                                          *
-  // ***************************************************************************/
-  // somePostgresqlServer: {
-  //   adapter: 'sails-postgresql',
-  //   host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_POSTGRES_USER',
-  //   password: 'YOUR_POSTGRES_PASSWORD',
-  //   database: 'YOUR_POSTGRES_DB'
-  // }
-
-
-  // /***************************************************************************
-  // *                                                                          *
-  // * More adapters: https://github.com/balderdashy/sails                      *
-  // *                                                                          *
-  // ***************************************************************************/
 
 };
