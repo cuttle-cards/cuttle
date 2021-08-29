2c39017bf6fe094027b431d100c0f6b61b00f0a2
Ryan Emberling <itsalaidbacklife@gmail.com>
2021-08-29 11:47:50 -0400

    fix: wrap the req is for Game.unsubscribe in an array

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -2318,9 +2318,9 @@ module.exports = {
 	},
 
 	gameOver: function (req, res) {
-		var promisePlayer = userService.findUser({userId: req.session.usr})
+		const promisePlayer = userService.findUser({userId: req.session.usr})
 		.then(function deleteSessionData (player) {
-			Game.unsubscribe(req, req.session.game);
+			Game.unsubscribe(req, [req.session.game]);
 			delete(req.session.game);
 			delete(req.session.pNum);
 			return res.ok();
