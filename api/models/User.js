69b1054ee17f953297211f43e81e5ec09c6ab144
Ryan Emberling <itsalaidbacklife@gmail.com>
2021-12-05 22:20:41 -0500

    chore: whitespace (whole file)

--- api/models/User.js
+++ api/models/User.js
@@ -7,39 +7,39 @@
 
 module.exports = {
   attributes: {
-  	email: {
-  		type: 'string',
-  		required: true,
-  	},
-  	encryptedPassword: {
-  		type: 'string',
-  		required: true,
-  	},
-  	game: {
-  		model: 'game',
-	  },
-	/**
-	 * Index of this user within a Game's players collection
-	 * @value null iff not in game
-	 * @value 0 or 1 if player 0 or player 1, respectively
-	 */
-    pNum: {
+		email: {
+			type: 'string',
+			required: true,
+		},
+		encryptedPassword: {
+			type: 'string',
+			required: true,
+		},
+		game: {
+			model: 'game',
+		},
+		/**
+		 * Index of this user within a Game's players collection
+		 * @value null iff not in game
+		 * @value 0 or 1 if player 0 or player 1, respectively
+		 */
+		pNum: {
 			type: 'number',
 			allowNull: true,
 			isIn: [0, 1],
-    },
-    hand: {
-    	collection: 'card',
-    	via: 'hand',
-    },
-  	points: {
-  		collection: 'card',
-  		via: 'points',
-  	},
-  	faceCards: {
-  		collection: 'card',
-  		via: 'faceCards',
-	  },
+		},
+		hand: {
+			collection: 'card',
+			via: 'hand',
+		},
+		points: {
+			collection: 'card',
+			via: 'points',
+		},
+		faceCards: {
+			collection: 'card',
+			via: 'faceCards',
+		},
 		/**
 		 * Id of a card in player's hand that cannot be played this turn
 		 * @value null iff no card is frozen
