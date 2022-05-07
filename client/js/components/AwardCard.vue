15abdd6d6b3266b335c73a1d19e6ccbc260974b6
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-05-07 12:30:33 -0400

    feat: add dummy AwardCard component

---
+++ client/js/components/AwardCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="d-flex">
+    <div>
+      <v-icon x-large>
+        mdi-medal
+      </v-icon>
+    </div>
+    <h3>
+      {{ username }}
+    </h3>
+    {{ placeText }}
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AwardCard',
+  props: {
+    username: String,
+    placeText: String,
+  },
+};
+</script>
