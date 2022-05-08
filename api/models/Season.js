5c46bb6a4b1fc3d9168ce23e9c60da4b1065cd70
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-05-07 21:35:00 -0400

    chore: add Match and Season models

---
+++ api/models/Season.js
@@ -0,0 +1,33 @@
+/**
+ * Season.js
+ *
+ * @description :: Defines a season of competitive cuttle,
+ * its start and end time, and the tournament champions
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+  attributes: {
+    name: {
+      type: 'string',
+      required: true,
+    },
+    startTime: {
+      type: 'number',
+      required: true,
+    },
+    endTime: {
+      type: 'number',
+      required: true,
+    },
+    firstPlace: {
+      model: 'user',
+    },
+    secondPlace: {
+      model: 'user',
+    },
+    thirdPlace: {
+      model: 'user',
+    },
+  }, // end attributes
+}; // end exports
