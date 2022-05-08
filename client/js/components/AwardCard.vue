02506b2f4edeb7617d1594ee1b039eff653c7c13
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-05-07 20:22:58 -0400

    feat: display season champions in stats

--- client/js/components/AwardCard.vue
+++ client/js/components/AwardCard.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-card class="d-flex">
-    <div>
-      <v-icon x-large>
+  <v-card class="d-flex align-center">
+    <div class="medal-icon pa-8 d-flex align-center">
+      <v-icon size="48" :color="medalColor">
         mdi-medal
       </v-icon>
     </div>
-    <h3>
-      {{ username }}
-    </h3>
-    {{ placeText }}
+    <div class="text d-flex flex-column align-center px-16 justify-center">
+      <h3 class="mx-4">
+        {{ username }}
+      </h3>
+      {{ place }}{{ suffix }}
+    </div>
   </v-card>
 </template>
 
@@ -17,7 +19,42 @@ export default {
   name: 'AwardCard',
   props: {
     username: String,
-    placeText: String,
+    place: Number,
+  },
+  computed: {
+    medalColor() {
+      switch (this.place) {
+        case 1:
+          return '#AF9500';
+        case 2:
+          return '#B4B4B4';
+        case 3:
+          return '#6A3805';
+        default:
+          return '#000';
+      }
+    },
+    suffix() {
+      switch (this.place) {
+        case 1:
+          return 'st';
+        case 2:
+          return 'nd';
+        case 3:
+          return 'rd';
+        default:
+          return 'th';
+      }
+    },
   },
 };
 </script>
+
+<style scoped>
+.medal-icon {
+  background-color: #EEE;
+}
+.text {
+  height: 100%;
+}
+</style>
