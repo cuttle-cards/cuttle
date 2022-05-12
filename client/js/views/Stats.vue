eb779a9c15b766b1dc767c3569d37970d1bec9bb
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-05-12 09:16:45 -0400

    style: set stats page font to pt-serif

--- client/js/views/Stats.vue
+++ client/js/views/Stats.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
-    <!-- Upper Servicd -->
+  <div id="stats-wrapper">
+    <!-- Upper Service -->
     <section id="upper-surface" class="pa-8 mb-6">
       <h1 class="text-h2">
         Clubs 2022
       </h1>
     </section>
     <section class="px-8">
-      <h2 class="text-h3 mb-4">
-        Season Champions
-      </h2>
-      <div class="d-flex justify-space-between flex-wrap">
-        <award-card
-          username="fooey"
-          :place="1"
-          class="mb-4"
-        />
-        <award-card username="booey" :place="2" class="mb-4"/>
-        <award-card username="gluey" :place="3" class="mb-4"/>
+      <!-- Season Champions -->
+      <div class="mb-10">
+        <h2 class="text-h3 mb-4">
+          Season Champions
+        </h2>
+        <div class="d-flex justify-space-between flex-wrap">
+          <award-card username="fooey" :place="1" class="mb-4" />
+          <award-card username="booey" :place="2" class="mb-4" />
+          <award-card username="gluey" :place="3" class="mb-4" />
+        </div>
       </div>
+      <!-- Rankings Table -->
       <h2 class="text-h3 my-4">
         Weekly Rankings
       </h2>
@@ -340,7 +340,10 @@ export default {
 </script>
 
 <style scoped>
-h1 {
+#stats-wrapper * {
+  font-family: 'PT Serif', serif !important;
+}
+#stats-wrapper h1 {
   background: linear-gradient(
     268.89deg,
     rgba(98, 2, 238, 0.87) 73.76%,
