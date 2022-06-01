97567e6ecea4ff380efae40f6cc14353c8b2bf3e
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-06-01 08:54:24 -0400

    test: signup 4 users in stats test setup

---
+++ tests/e2e/fixtures/userFixtures.js
@@ -0,0 +1,21 @@
+const playerOne = {
+  username: 'Player1',
+  password: 'player1DaBest',
+};
+
+const playerTwo = {
+  username: 'Player2',
+  password: 'player2DaBest',
+};
+
+const playerThree = {
+  username: 'Player3',
+  password: 'player3DaBest',
+};
+
+const playerFour = {
+  username: 'Player4',
+  password: 'player4DaBest',
+};
+
+export { playerOne, playerTwo, playerThree, playerFour };
--- tests/e2e/specs/out_of_game/stats.spec.js
+++ tests/e2e/specs/out_of_game/stats.spec.js
@@ -1,8 +1,25 @@
 import { username, validPassword } from '../../support/helpers';
+import { playerOne, playerTwo, playerThree, playerFour } from '../../fixtures/userFixtures';
+import { seasonFixture } from '../../fixtures/statsFixtures';
+
 function setup() {
   cy.wipeDatabase();
   cy.visit('/');
   cy.signupPlayer(username, validPassword);
+  cy.signupOpponent(playerOne.username, playerOne.password).as('playerOneId');
+  cy.signupOpponent(playerTwo.username, playerTwo.password).as('playerTwoId');
+  cy.signupOpponent(playerThree.username, playerThree.password).as('playerThreeId');
+  cy.signupOpponent(playerFour.username, playerFour.password)
+    .as('playerFourId')
+    .then(function() {
+      const season = {
+        ...seasonFixture,
+        firstPlace: this.playerOneId,
+        secondPlace: this.playerTwoId,
+        thirdPlace: this.playerThreeId,
+        fourthPlace: this.playerFourId,
+      };
+    });
   cy.vueRoute('/stats');
 }
 
