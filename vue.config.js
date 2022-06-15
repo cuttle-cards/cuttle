95c6fd82e609861a920d9801449a16ec7151e295
Sean Kennedy <sean@codeserious.com>
2022-06-15 14:39:40 -0400

    Commiting a sin via prettier

--- vue.config.js
+++ vue.config.js
@@ -14,17 +14,21 @@ module.exports = {
       template: 'client/public/index.html',
       filename: 'index.html',
       // https://devtools.vuejs.org/guide/installation.html#standalone
-      ...(process.env.NODE_ENV != 'production' ? {
-        vueDevToolsScript: '<script src="http://localhost:8098"></script>',
-      } : {}),
+      ...(process.env.NODE_ENV != "production"
+        ? {
+            vueDevToolsScript: '<script src="http://localhost:8098"></script>',
+          }
+        : {}),
     },
   },
 
   configureWebpack: {
     // https://blog.scottlogic.com/2017/11/01/webpack-source-map-options-quick-guide.html
-    ...(process.env.NODE_ENV != 'production' ? {
-      devtool: 'cheap-module-eval-source-map',
-    } : {}),
+    ...(process.env.NODE_ENV != "production"
+      ? {
+          devtool: "cheap-module-eval-source-map",
+        }
+      : {}),
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'client/js'),
