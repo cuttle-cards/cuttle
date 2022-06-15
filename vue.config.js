2bcf35db555014497f9b0fc5b0123e739ffec4bd
Sean Kennedy <sean@codeserious.com>
2022-06-15 14:18:24 -0400

    Add standalone Vue DevTools for debugging purposes

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,30 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   transpileDependencies: ['vuetify'],
-  configureWebpack: {
-    devtool: 'source-map',
-  },
+
   outputDir: process.env.VUE_APP_BUILD_OUTPUT || 'dist',
 
   lintOnSave: false,
 
-  outputDir: 'assets',
   pages: {
     index: {
       entry: 'client/js/main.js',
       template: 'client/public/index.html',
       filename: 'index.html',
+      // https://devtools.vuejs.org/guide/installation.html#standalone
+      ...(process.env.NODE_ENV != 'production' ? {
+        vueDevToolsScript: '<script src="http://localhost:8098"></script>',
+      } : {}),
     },
   },
 
   configureWebpack: {
+    // https://blog.scottlogic.com/2017/11/01/webpack-source-map-options-quick-guide.html
+    ...(process.env.NODE_ENV != 'production' ? {
+      devtool: 'cheap-module-eval-source-map',
+    } : {}),
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'client/js'),
