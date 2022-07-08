637c214dd23299c71160c75300de2c4fc3a12158
Pete Rancourt <p.ranc315@gmail.com>
2022-07-08 16:34:47 -0400

    aligns class naming convention

--- client/js/components/GameView/UsernameToolTip.vue
+++ client/js/components/GameView/UsernameToolTip.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div class="username-container__outer" :class="{ 'username-container__outer--rtl': !isPlayer }">
-      <div class="username-container">
-        <h4 class="truncate" :data-cy="dataCyProp">{{ username }}</h4>
+    <div class="username__outer" :class="{ 'username__outer--rtl': !isPlayer }">
+      <div class="username">
+        <h4 class="username__text" :data-cy="dataCyProp">{{ username }}</h4>
       </div>
     </div>
   </div>
@@ -30,7 +30,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.username-container {
+.username {
   /* Change this to see truncation */
   background-color: transparent;
   padding: 1em;
@@ -39,27 +39,28 @@ export default {
 
   &__outer {
     width: auto;
+
+    &--rtl {
+      direction: rtl;
+    }
   }
 
-  &__outer--rtl {
-    direction: rtl;
+  &__text {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    width: 100%;
+    transition: 0.25s all ease-in-out;
+
+    .username:hover & {
+      width: fit-content;
+    }
   }
 
   &:hover {
     background-color: rgba(0, 0, 0, 0.75);
-    width: fit-content;
-  }
-}
-.truncate {
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  width: 100%;
-  transition: 0.25s all ease-in-out;
-
-  .username-container:hover & {
     width: fit-content;
   }
 }
