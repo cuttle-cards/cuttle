63f8ecea69bf5fbd8b07da60ec6d986a19684c4b
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-08-07 17:11:48 -0400

    Fix/seven end of deck (#68)

--- tests/e2e/specs/in-game/sevens.spec.js
+++ tests/e2e/specs/in-game/sevens.spec.js
@@ -1337,3 +1337,107 @@ describe('Opponent playing SEVENS', () => {
     });
   }); // End Opponent seven one-off describe
 }); // End opponent plays seven describe
+
+describe('Playing sevens at the end of the deck', () => {
+  it('Plays the last card for points from a seven', () => {
+    setupGameAsP1();
+    cy.loadGameFixture({
+      p0Hand: [],
+      p0Points: [],
+      p0FaceCards: [],
+      p1Hand: [Card.SEVEN_OF_CLUBS],
+      p1Points: [],
+      p1FaceCards: [],
+      topCard: Card.FOUR_OF_CLUBS,
+      secondCard: Card.SIX_OF_DIAMONDS,
+    });
+    cy.get('[data-player-hand-card]').should('have.length', 1);
+    cy.log('Loaded fixture');
+
+    cy.deleteDeck();
+    cy.get('#deck').should('contain', 2);
+
+    cy.drawCardOpponent();
+    cy.playOneOffAndResolveAsPlayer(Card.SEVEN_OF_CLUBS);
+    cy.get('[data-top-card=6-1]').click();
+    cy.get('[data-move-choice=points]').click();
+
+    cy.get('#deck').find('#empty-deck-text').should('contain', 'PASS');
+  });
+
+  it('Plays the top card of the deck when there are two cards left', () => {
+    setupGameAsP0();
+    cy.loadGameFixture({
+      p0Hand: [Card.SEVEN_OF_CLUBS],
+      p0Points: [],
+      p0FaceCards: [],
+      p1Hand: [],
+      p1Points: [],
+      p1FaceCards: [],
+      topCard: Card.FOUR_OF_CLUBS,
+      secondCard: Card.SIX_OF_DIAMONDS,
+    });
+    cy.get('[data-player-hand-card]').should('have.length', 1);
+    cy.log('Loaded fixture');
+
+    cy.deleteDeck();
+    cy.get('#deck').should('contain', 2);
+
+    cy.playOneOffAndResolveAsPlayer(Card.SEVEN_OF_CLUBS);
+    cy.get('[data-top-card=4-0]').click();
+    cy.get('[data-move-choice=points]').click();
+
+    cy.get('#deck').should('contain', '(1)');
+    cy.drawCardOpponent();
+
+    cy.get('#deck').find('#empty-deck-text').should('contain', 'PASS');
+
+    assertGameState(0, {
+      p0Hand: [],
+      p0Points: [Card.FOUR_OF_CLUBS],
+      p0FaceCards: [],
+      p1Hand: [Card.SIX_OF_DIAMONDS],
+      p1Points: [],
+      p1FaceCards: [],
+      scrap: [Card.SEVEN_OF_CLUBS],
+    });
+  });
+
+  it('Plays the 2nd card in the deck when there are two cards left', () => {
+    setupGameAsP0();
+    cy.loadGameFixture({
+      p0Hand: [Card.SEVEN_OF_CLUBS],
+      p0Points: [],
+      p0FaceCards: [],
+      p1Hand: [],
+      p1Points: [],
+      p1FaceCards: [],
+      topCard: Card.FOUR_OF_CLUBS,
+      secondCard: Card.SIX_OF_DIAMONDS,
+    });
+    cy.get('[data-player-hand-card]').should('have.length', 1);
+    cy.log('Loaded fixture');
+
+    cy.deleteDeck();
+    cy.get('#deck').should('contain', 2);
+
+    cy.playOneOffAndResolveAsPlayer(Card.SEVEN_OF_CLUBS);
+    cy.get('[data-second-card=6-1]').click();
+    cy.get('[data-move-choice=points]').click();
+
+    cy.get('#deck').should('contain', '(1)');
+    cy.drawCardOpponent();
+
+    cy.get('#deck').find('#empty-deck-text').should('contain', 'PASS');
+
+    assertGameState(0, {
+      p0Hand: [],
+      p0Points: [Card.SIX_OF_DIAMONDS],
+      p0FaceCards: [],
+      p1Hand: [Card.FOUR_OF_CLUBS],
+      p1Points: [],
+      p1FaceCards: [],
+      scrap: [Card.SEVEN_OF_CLUBS],
+    });
+  });
+});
