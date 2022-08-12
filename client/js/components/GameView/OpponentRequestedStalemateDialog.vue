93b7893eb4a288f1320f68f18d9612b01893997e
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-08-12 08:57:38 -0400

    feat: add buttons and icons to opponent requested stalemate dialog

--- client/js/components/GameView/OpponentRequestedStalemateDialog.vue
+++ client/js/components/GameView/OpponentRequestedStalemateDialog.vue
@@ -4,7 +4,15 @@
       <v-card-title>Accept Stalemate?</v-card-title>
       <v-card-text>
         <p>Your opponent has requested a stalemate. If you accept, the game will end in a tie.</p>
+        <div class="d-flex justify-center">
+          <v-icon class="mr-8" size="80px">mdi-offer</v-icon>
+          <v-icon size="80px">mdi-help-circle</v-icon>
+        </div>
       </v-card-text>
+      <v-card-actions class="d-flex justify-end">
+        <v-btn outlined color="primary" class="mr-4">Reject Request</v-btn>
+        <v-btn color="error" depressed>Accept Stalemate</v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
