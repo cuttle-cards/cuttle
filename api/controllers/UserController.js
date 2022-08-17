75c1d5565e7a7721afd2cb446c495d58d72e00c3
Sean Kennedy <sean@codeserious.com>
2022-08-17 00:04:32 -0400

    Remove erroneous import

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,7 +5,6 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-const { multiply } = require('lodash');
 const { getLobbyDataByGame } = require('../../utils/game-utils');
 
 //////////////////
