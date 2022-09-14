25f54baf24215ae14e44638e6a51f54866af6ba7
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-09-14 08:55:04 -0400

    feat: add cypress project id to config

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 const { isProd } = require('../utils/config-utils');
 
 module.exports = defineConfig({
+  projectId: 'i8bxr8',
   pluginsFile: 'tests/e2e/plugins/index.js',
   baseUrl: process.env.VUE_APP_API_URL || 'http://localhost:8080',
   // Always test spec.js files, but switch between spec.prod and spec.dev based on the environment
