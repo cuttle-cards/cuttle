57f4f94e4178c66c2435b771a66cc186c92f07c9
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-09-20 08:55:48 -0400

    test: assert that 2nd game between the players gets added to the same match

--- tests/e2e/specs/in-game/ranked-matches.spec.js
+++ tests/e2e/specs/in-game/ranked-matches.spec.js
@@ -1,5 +1,5 @@
 const dayjs = require('dayjs');
-import { setupGameAsP0 } from '../../support/helpers';
+import { setupGameAsP0, setupGameAsP1, Card } from '../../support/helpers';
 import { seasonFixtures } from '../../fixtures/statsFixtures';
 import { playerOne, playerTwo } from '../../fixtures/userFixtures';
 
@@ -14,7 +14,7 @@ describe('Creating And Updating Ranked Matches', () => {
     cy.loginPlayer(playerOne.username, playerOne.password);
     setupGameAsP0(true, true);
     // Set up season
-    const [clubsSeason, diamondsSeason] = seasonFixtures;
+    const [_clubsSeason, diamondsSeason] = seasonFixtures;
     diamondsSeason.startTime = dayjs().subtract(2, 'week').subtract(1, 'day').valueOf();
     diamondsSeason.endTime = dayjs().add(11, 'weeks').valueOf();
     cy.loadSeasonFixture([diamondsSeason]);
@@ -39,6 +39,36 @@ describe('Creating And Updating Ranked Matches', () => {
       expect(match.startTime).to.be.greaterThan(0);
       expect(match.endTime).to.eq(0);
       expect(match.games.length).to.eq(1);
+      expect(match.games[0].result).to.eq(0); // P0 should have won the first game
+    });
+    // 2nd game: Player is now p1 and loses by points
+    setupGameAsP1(true, true);
+    // Set Up
+    cy.loadGameFixture({
+      p0Hand: [Card.ACE_OF_SPADES],
+      p0Points: [Card.TEN_OF_SPADES, Card.TEN_OF_HEARTS],
+      p0FaceCards: [],
+      p1Hand: [Card.ACE_OF_DIAMONDS],
+      p1Points: [],
+      p1FaceCards: [],
+    });
+    cy.get('[data-player-hand-card]').should('have.length', 1);
+    cy.log('Loaded fixture');
+
+    // Opponent wins with points
+    cy.playPointsOpponent(Card.ACE_OF_SPADES);
+    cy.get('[data-cy=gameover-go-home]').click();
+    cy.url().should('not.include', '/game');
+
+    // The match for these two players should now have two games
+    cy.request('http://localhost:1337/match').then((res) => {
+      expect(res.body.length).to.eq(1);
+      const [match] = res.body;
+      expect(match.player1.id).to.eq(this.playerOneId);
+      expect(match.player2.id).to.eq(this.playerTwoId);
+      expect(match.startTime).to.be.greaterThan(0);
+      expect(match.endTime).to.eq(0);
+      expect(match.games.length).to.eq(2);
       expect(match.games[0].result).to.eq(0); // P0 should have won the first game
     });
   });
