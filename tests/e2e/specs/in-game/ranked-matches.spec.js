c8078a89525eb96cf0c92f05becd5cc3a3c9d13d
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-09-21 08:37:59 -0400

    test: assert that stalemate gets associated with match

--- tests/e2e/specs/in-game/ranked-matches.spec.js
+++ tests/e2e/specs/in-game/ranked-matches.spec.js
@@ -42,14 +42,14 @@ describe('Creating And Updating Ranked Matches', () => {
       expect(match.games[0].result).to.eq(0); // P0 should have won the first game
       cy.log('Match data is correct after first game', res.body);
     });
-    // 2nd game: Player is now p1 and loses by points
+    // 2nd game: Player is now p0 and loses by points
     setupGameAsP1(true, true);
     // Set Up
     cy.loadGameFixture({
       p0Hand: [Card.ACE_OF_SPADES],
       p0Points: [Card.TEN_OF_SPADES, Card.TEN_OF_HEARTS],
       p0FaceCards: [],
-      p1Hand: [Card.ACE_OF_DIAMONDS],
+      p1Hand: [Card.THREE_OF_CLUBS],
       p1Points: [],
       p1FaceCards: [],
     });
@@ -67,11 +67,44 @@ describe('Creating And Updating Ranked Matches', () => {
       const [match] = res.body;
       expect(match.player1.id).to.eq(this.playerOneId);
       expect(match.player2.id).to.eq(this.playerTwoId);
+      expect(match.games.length).to.eq(2);
+      expect(match.startTime).to.be.greaterThan(0);
+      // Match is incomplete
+      expect(match.endTime).to.eq(0);
+      expect(match.winner).to.eq(null);
+      expect(match.games[0].result).to.eq(0); // P0 should have won the first game
+      expect(match.games[1].result).to.eq(0); // P1 should have won the first game
+      cy.log('Match data is correct after second game', res);
+    });
+
+    // 3rd game: Player is now p1 and game ends in stalemate
+    setupGameAsP0(true, true);
+    // Request stalemate
+    cy.get('#game-menu-activator').click();
+    cy.get('#game-menu').should('be.visible').get('[data-cy=stalemate-initiate]').click();
+    cy.get('#request-gameover-dialog')
+      .should('be.visible')
+      .get('[data-cy=request-gameover-confirm]')
+      .click();
+    cy.get('#waiting-for-opponent-stalemate-scrim').should('be.visible');
+    // Opponent confirms
+    cy.stalemateOpponent();
+    cy.get('[data-cy=gameover-go-home]').click();
+    cy.url().should('not.include', '/game');
+
+    // The match for these two players should now have two games
+    cy.request('http://localhost:1337/match').then((res) => {
+      expect(res.body.length).to.eq(1);
+      const [match] = res.body;
+      expect(match.player1.id).to.eq(this.playerOneId);
+      expect(match.player2.id).to.eq(this.playerTwoId);
       expect(match.startTime).to.be.greaterThan(0);
       expect(match.endTime).to.eq(0);
-      expect(match.games.length).to.eq(2);
-      expect(match.games[0].result).to.eq(0); // P0 should have won the first game
-      cy.log('Match data is correct after second game', res);
+      expect(match.games.length).to.eq(3);
+      // Match is incomplete
+      expect(match.endTime).to.eq(0);
+      expect(match.winner).to.eq(null);
+      cy.log('Match data is correct after third game', res);
     });
   });
 
