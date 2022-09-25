93325edea8b75101dbf1b98ed5ef012415aba704
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-09-25 15:10:44 -0400

    chore: remove .only()

--- tests/e2e/specs/in-game/gameOver.spec.js
+++ tests/e2e/specs/in-game/gameOver.spec.js
@@ -433,7 +433,7 @@ describe('Creating And Updating Ranked Matches', () => {
     cy.loginPlayer(playerOne.username, playerOne.password);
     setupGameAsP0(true, true);
   });
-  it.only('Creates a match when two players play a ranked game for the first time this week', function () {
+  it('Creates a match when two players play a ranked game for the first time this week', function () {
     // There should be two matches initially (one from last week and one with a different opponent)
     cy.request('http://localhost:1337/match').then((res) => {
       expect(res.body.length).to.eq(2);
