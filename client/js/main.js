2b6e0d2ae3b02d11335970c6d1c9705e25ed5fc3
Sean Kennedy <sean@codeserious.com>
2022-09-27 18:40:32 -0400

    Adjusting packages to vue 3, again :)

--- client/js/main.js
+++ client/js/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import store from './store/store';
-import router from './router';
+// import store from './store/store';
+// import router from './router';
 Vue.config.productionTip = false;
 
 const app = new Vue({
   vuetify,
-  store,
-  router,
+  // store,
+  // router,
   render: (h) => h(App),
 }).$mount('#app');
 
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,38 @@
-import path from 'path';
+import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 // vite.config.js
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue2'
+import vue from '@vitejs/plugin-vue2';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      _: resolve(__dirname),
+      '@': resolve(__dirname, 'client/js'),
+    },
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    proxy: {
+      '/game': {
+        target: 'http://localhost:1337',
+        changeOrigin: true,
+      },
+      '/user': {
+        target: 'http://localhost:1337',
+        changeOrigin: true,
+      },
+      '/test': {
+        target: 'http://localhost:1337',
+        changeOrigin: true,
+      },
     },
   },
 });
