2d734fc26e99ab1786e2e827ecf982f6d5b9c1ef
Sean Kennedy <sean@codeserious.com>
2022-09-30 13:34:10 -0400

    Feature: Upgrade Cypress to Version 10

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('cypress');
+
+const { isProd } = require('./utils/config-utils');
+
+module.exports = defineConfig({
+  // https://docs.cypress.io/guides/references/configuration#Global
+  env: {
+    ENABLE_VUE_DEVTOOLS: process.env.ENABLE_VUE_DEVTOOLS,
+    NODE_ENV: isProd ? 'production' : 'dev',
+  },
+  // https://docs.cypress.io/guides/references/configuration#e2e
+  e2e: {
+    baseUrl: process.env.VUE_APP_API_URL || 'http://localhost:8080',
+    specPattern: [
+      ...(isProd ? ['tests/e2e/specs/**/*.spec.prod.js'] : ['tests/e2e/specs/**/*.spec.dev.js']),
+      'tests/e2e/specs/**/*.spec.js',
+    ],
+    supportFile: 'tests/e2e/support/index.js',
+  },
+  // https://docs.cypress.io/guides/references/configuration#Videos
+  video: false,
+  // https://docs.cypress.io/guides/references/configuration#Folders-Files
+  downloadsFolder: 'tests/e2e/downloads',
+  fixturesFolder: 'tests/e2e/fixtures',
+  screenshotsFolder: 'tests/e2e/screenshots',
+  videosFolder: 'tests/e2e/videos',
+});
