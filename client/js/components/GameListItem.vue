ec5dd86b6227e6c24fa8196bdeaaca7d758da6d8
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-10-03 10:13:28 -0400

    style: rework game-list-item component into new card style

--- client/js/components/GameListItem.vue
+++ client/js/components/GameListItem.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
+  <v-card max-width="350">
+    <v-card-title class="d-flex justify-start align-center">
+      <img
+        :src="require('../img/logo_head.svg')"
+        class="mr-8"
+        contain
+        :height="$vuetify.breakpoint.smAndUp ? 64 : 32"
+      />
+      <span>
+        <h4>{{ name }}</h4>
+        <p class="mb-0">{{ readyText }}</p>
+      </span>
+    </v-card-title>
+    <v-card-actions>
+      <v-btn
+        block
+        color="primary"
+        rounded
+        outlined
+        min-width="200"
+        :disabled="!status"
+        :loading="joiningGame"
+        @click="subscribeToGame"
+      >
+        {{ buttonText }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  <!-- <div>
     <v-row class="list-item" data-cy="game-list-item">
       <v-col sm="2" lg="3">
         <v-img
@@ -32,7 +60,7 @@
       </v-col>
     </v-row>
     <v-divider class="mb-4" />
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -101,30 +129,30 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-.list-item {
-  margin-left: 0;
-  margin-right: 0;
-  text-align: left;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  & .game-name {
-    font-weight: 600;
-  }
-}
-.list-item-button {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-@media (max-width: 979px) and (orientation: landscape) {
-  .list-item {
-    margin: 0;
-    & p {
-      line-height: 1rem;
-      margin: 3px auto;
-    }
-  }
-}
+// .list-item {
+//   margin-left: 0;
+//   margin-right: 0;
+//   text-align: left;
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   align-items: center;
+//   & .game-name {
+//     font-weight: 600;
+//   }
+// }
+// .list-item-button {
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// }
+// @media (max-width: 979px) and (orientation: landscape) {
+//   .list-item {
+//     margin: 0;
+//     & p {
+//       line-height: 1rem;
+//       margin: 3px auto;
+//     }
+//   }
+// }
 </style>
