65cdb7acbc911962efc4135f287141c049a661d4
Ryan Emberling <itsalaidbacklife@gmail.com>
2022-10-03 11:12:22 -0400

    style: update heading layout in Home page

--- client/js/views/Home.vue
+++ client/js/views/Home.vue
@@ -4,11 +4,7 @@
       <div id="game-list-card">
         <v-row>
           <v-col cols="9">
-            <v-row id="card-content-header" class="mb-4">
-              <v-col cols="4">
-                <h1 id="home-card-title">Games</h1>
-              </v-col>
-            </v-row>
+            <h1 id="home-card-title">Games</h1>
             <div id="game-list">
               <p v-if="gameList.length === 0" data-cy="text-if-no-game">No Active Games</p>
               <div v-for="game in gameList" :key="game.id">
