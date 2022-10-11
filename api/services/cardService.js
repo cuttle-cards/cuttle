e74ff953d798442c9d4ae5ccaee9bfefddf54ec3
Ben Yarmis <ben@yarm.is>
2022-10-10 20:41:13 -0500

    Card service cleanup and card name suits are now emoji

--- api/services/cardService.js
+++ api/services/cardService.js
@@ -28,120 +28,83 @@ module.exports = {
    */
   createCard: function (options) {
     return new Promise(function (resolve, reject) {
-      var validArgs =
-        options.suit > -1 &&
-        options.suit < 4 &&
-        options.suit > -1 &&
-        options.suit < 14 &&
+      const validArgs =
+        options.suit >= 0 &&
+        options.suit <= 3 &&
+        options.rank >= 1 &&
+        options.rank <= 13 &&
         options.gameId;
-      if (validArgs) {
-        var { gameId } = options;
-        var { suit } = options;
-        var { rank } = options;
-        var str_rank = '';
-        var str_suit = '';
-        var str_name = '';
-        var ruleText = '';
-        // Stringify Rank
-        switch (rank) {
-          case 1:
-            str_rank = 'Ace';
-            break;
-          case 11:
-            str_rank = 'Jack';
-            break;
-          case 12:
-            str_rank = 'Queen';
-            break;
-          case 13:
-            str_rank = 'King';
-            break;
-          default:
-            str_rank = rank;
-            break;
-        }
-        // Stringify Suit
-        switch (suit) {
-          case 0:
-            str_suit = 'Clubs';
-            break;
-          case 1:
-            str_suit = 'Diamonds';
-            break;
-          case 2:
-            str_suit = 'Hearts';
-            break;
-          case 3:
-            str_suit = 'Spades';
-            break;
-        }
-        str_name = str_rank + ' of ' + str_suit; //Assign str name
-
-        // Assign Rule Text
-        switch (rank) {
-          case 1:
-            ruleText = 'Scrap all points';
-            break;
-          case 2:
-            ruleText = 'Scrap target Royal or Glasses eight';
-            break;
-          case 3:
-            ruleText = 'Choose 1 card in the Scrap and put it to your hand';
-            break;
-          case 4:
-            ruleText = 'Your opponent discards two cards of their choice from their hand';
-            break;
-          case 5:
-            ruleText = 'Draw two cards from the deck';
-            break;
-          case 6:
-            ruleText = 'Scrap all Royals and Glasses eights';
-            break;
-          case 7:
-            ruleText =
-              'Play one of the top two cards of the deck and put the other back (both are revealed)';
-            break;
-          case 8:
-            ruleText = 'Your opponent plays with an open hand (their cards are revealed to you)';
-            break;
-          case 9:
-            ruleText = "Return target card to its controller's hand. They can't play it next turn";
-            break;
-          case 10:
-            ruleText = 'No effect';
-            break;
-          case 11:
-            ruleText = 'Play on top of target point card to steal it';
-            break;
-          case 12:
-            ruleText = 'Your other cards may only be targeted by scuttles';
-            break;
-          case 13:
-            ruleText =
-              'Reduces the points you need to win. (1K: 14pts, 2K: 10pts, 3K: 7pts, 4K: 5pts)';
-            break;
-        }
-
-        // Create card record
-        return Card.create({
-          suit: suit,
-          rank: rank,
-          name: str_name,
-          ruleText: ruleText,
-          deck: gameId,
-        })
-          .fetch()
-          .then((card) => {
-            return resolve(card);
-          })
-          .catch((err) => {
-            if (err) {
-              return reject(err);
-            }
-            return reject({ message: `Error creating card: ${suit} of ${rank}` });
-          });
+      if (!validArgs) {
+        return reject({ message: 'Invalid Arguments for createCard service' });
       }
-      return reject({ message: 'Invalid Arguments for createCard service' });
+
+      const { gameId, suit, rank } = options;
+      // Stringify Rank
+      const str_rank =
+        {
+          1: 'Ace',
+          11: 'Jack',
+          12: 'Queen',
+          13: 'King',
+        }[rank] ?? rank;
+      /*
+      So, a slight aside: || would work above instead of the nullish coalescing
+      operator (??) since the options that the first operation (the {...}[rank]
+      bit) could return are not falsey (i.e. not an empty string).  But, in
+      principle, we're coalescing nulls instead of falsey things.
+
+      If you're not familiar, ?? is like ||, but it picks the second
+      argument iff the first argument is null or undefined
+
+      1    || 'a' -> 1
+      null || 'a' -> 'a'
+      0    || 'a' -> 'a'
+
+      1    ?? 'a' -> 1
+      null ?? 'a' -> 'a'
+      0    ?? 'a' -> 0
+       */
+
+      // Stringify Suit
+      const str_suit = ['♣', '♦', '♥', '♠'][suit];
+
+      const str_name = str_rank + str_suit;
+
+      const ruleText = [
+        null,
+        'Scrap all points',
+        'Scrap target Royal or Glasses eight',
+        'Choose 1 card in the Scrap and put it to your hand',
+        'Your opponent discards two cards of their choice from their hand',
+        'Draw two cards from the deck',
+        'Scrap all Royals and Glasses eights',
+        'Play one of the top two cards of the deck and put the other back (both are revealed)',
+        'Your opponent plays with an open hand (their cards are revealed to you)',
+        "Return target card to its controller's hand. They can't play it next turn",
+        'No effect',
+        'Play on top of target point card to steal it',
+        'Your other cards may only be targeted by scuttles',
+        'Reduces the points you need to win. (1K: 14pts, 2K: 10pts, 3K: 7pts, 4K: 5pts)',
+      ][rank];
+
+      // Create card record
+      return Card.create({
+        suit: suit,
+        rank: rank,
+        name: str_name,
+        ruleText: ruleText,
+        deck: gameId,
+      })
+        .fetch()
+        .then((card) => {
+          return resolve(card);
+        })
+        .catch((err) => {
+          if (err) {
+            return reject(err);
+          }
+          return reject({ message: `Error creating card: ${str_name}` });
+        });
     });
   },
 
@@ -151,20 +114,20 @@ module.exports = {
    */
   findPoints: function (options) {
     return new Promise(function (resolve, reject) {
-      if (options.userId) {
-        Card.find({ points: options.userId })
-          .populate('attachments', { sort: 'index' })
-          .exec(function (err, cards) {
-            if (err) {
-              return reject(err);
-            } else if (!cards) {
-              return reject({ message: "Can't find cards in points" });
-            }
-            return resolve(cards);
-          }); //End find()
-      } else {
+      if (!options.userId) {
         return reject({ message: "Don't have userId to find cards in user's points" });
       }
+      Card.find({ points: options.userId })
+        .populate('attachments', { sort: 'index' })
+        .then(function (cards) {
+          if (!cards) {
+            return reject({ message: "Can't find cards in points" });
+          }
+          return resolve(cards);
+        })
+        .catch((err) => {
+          return reject(err);
+        }); //End find()
     }); //End returned Promise
   },
 
@@ -174,16 +137,15 @@ module.exports = {
    */
   saveCard: function (options) {
     return new Promise(function (resolve, reject) {
-      if (options.card) {
-        options.card.save(function (err) {
-          if (err) {
-            return reject(err);
-          }
-          return resolve(options.card);
-        });
-      } else {
+      if (!options.card) {
         return reject({ message: "Can't save card without card record" });
       }
+      options.card.save(function (err) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(options.card);
+      });
     });
   },
 };
