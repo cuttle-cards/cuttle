6484dbfb76d031590cf79c7adecd100f5d753094
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2022-10-30 23:36:43 -0300

    Feature: adds total matches count on tableRows StatsLeaderboard

--- client/js/components/StatsLeaderboard.vue
+++ client/js/components/StatsLeaderboard.vue
@@ -102,8 +102,11 @@ export default {
       return this.season.rankings.map((playerStats, index) => {
         const playerWins = this.playerWins[index];
         const playerScores = this.playerScores[index];
+        const playerMatchesCount = Object.values(playerStats.matches)
+          .reduce((totalCount, weekMatches) => totalCount += weekMatches.length, 0)
         const res = {
           username: playerStats.username,
+          week_total_count: playerMatchesCount,
           week_total_wins: playerWins.total,
           week_total_points: playerScores.total,
           week_total: playerScores.total,
@@ -111,6 +114,7 @@ export default {
         };
         for (const weekNum in playerStats.matches) {
           res[`week_${weekNum}`] = playerScores[weekNum];
+          res[`week_${weekNum}_count`] = playerStats.matches[weekNum].length;
           res[`week_${weekNum}_wins`] = playerWins[weekNum];
           res[`week_${weekNum}_points`] = playerScores[weekNum];
         }
