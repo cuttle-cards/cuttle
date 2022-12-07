e3db8a0488bb3efb5b7d6bd911695a4630667bf6
P-DR0ZD <drozdpiotr@outlook.com>
2022-12-06 23:04:13 -0500

    Improved MyProfile Page Didn't implement backend

--- client/js/views/MyProfile.vue
+++ client/js/views/MyProfile.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="profile">
+  <v-container class="profile">
     <h1>{{ user }} Profile Settings</h1>
-    <label for="email">Email:</label>
-    <input id="email" type="email" name="email" onsubmit=""/>
-  </div>
+    <br />
+    <v-text-field
+      v-model="email"
+      outlined
+      :dense="$vuetify.breakpoint.mdAndDown ? true : false"
+      hint="email"
+      data-cy="email"
+      autocomplete="email"
+      placeholder="email"
+      @keydown.enter.prevent="submitEmail"
+    />
+    <!-- <h3>Current Email: test</h3> -->
+  </v-container>
 </template>
 <script>
 export default {
@@ -11,14 +21,28 @@ export default {
   data() {
     return {
       user: this.$store.state.auth.username,
+      email: '',
     };
   },
   computed: {},
   mounted() {},
   methods: {
     submitEmail() {
-
+      //ToDo Email Change function
+      console.log("Test");
     },
   },
 };
 </script>
+
+<style scoped lang="scss">
+.profile {
+  width: 75%;
+  margin: 10px auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: left;
+  padding-top: 10px auto;
+}
+</style>
