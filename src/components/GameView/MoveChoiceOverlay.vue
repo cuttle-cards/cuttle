7039c2f71060290d00295050931bdae837c1d3b3
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-01-16 12:15:34 -0500

    style: apply 'text'm variant to icon buttons

--- src/components/GameView/MoveChoiceOverlay.vue
+++ src/components/GameView/MoveChoiceOverlay.vue
@@ -7,7 +7,12 @@
   >
     <!-- Cancel button -->
     <div id="close-wrapper" class="d-flex justify-end my-4">
-      <v-btn icon data-cy="cancel-move" @click="$emit('cancel')">
+      <v-btn
+            icon
+            variant="text"
+            color="white"
+            size="x-large"
+            @click="$emit('cancel')">
         <v-icon icon="mdi-close" size="x-large" />
       </v-btn>
     </div>
