4f9361155922b1f3838f3d178ba4c4eef5cf8113
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-02-05 09:15:55 -0500

    Update src/components/Global/BaseSnackbar.vue
    Replace an unnecessary slot with a template that fills the v-snackbar's #actions slot

--- src/components/Global/BaseSnackbar.vue
+++ src/components/Global/BaseSnackbar.vue
@@ -8,11 +8,11 @@
     v-bind="$attrs"
   >
     <slot />
-    <slot name="actions">
+    <template #actions>
       <v-btn data-cy="close-snackbar" icon variant="text" @click="clear">
         <v-icon icon="mdi-close" />
       </v-btn>
-    </slot>
+    </template>
   </v-snackbar>
 </template>
 
