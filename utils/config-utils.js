21e8932ab3b3c0ec83beb1e7792fdd275a67a2e7
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-02-13 19:32:06 -0500

    fix: separate devtools from adding app to window

--- utils/config-utils.js
+++ utils/config-utils.js
@@ -7,22 +7,17 @@ export function initCuttleGlobals(app) {
     console.error('No window object found');
   }
 
-  // This is required to mock a prod-like scenario in Cypress to check the existence (or lack
-  // there-of) of the devtools instance
-  const isMockProd = new URLSearchParams(window.location.search).get('mock-prod') === 'true';
-  const includeDevtools = (!import.meta.env.PROD || window.Cypress) && !isMockProd;
-
   const test = window.Cypress != null;
   const cuttle = {
     version,
     // Expose app for debugging/testing in lower envs
-    app: includeDevtools ? app : null,
+    app: window.Cypress ? app : null,
     test,
   };
 
   window.cuttle = cuttle;
 
-  if (includeDevtools) {
+  if (!import.meta.env.PROD) {
     // Connect the devtools -- non-prod only
     console.log('Connecting devtools');
     devtools.connect(null, 8098);
