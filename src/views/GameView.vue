b4f3458f6d0d640c8d237589a17b2a697cf25889
Tommy Schmid <thomas.albrecht.apply@gmail.com>
2023-04-03 12:57:01 -0400

    Correcting version bump

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1185,6 +1185,8 @@ export default {
     &.reveal-top-two {
       height: auto;
       align-self: start;
+      color: white;
+      background-image: none;
       & .resolving-seven-card {
         width: 9.5rem;
       }
