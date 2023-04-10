e7c5b06a89d8feba9962c7251c45c7e602333a9a
Henry Aviles <haviles04@gmail.com>
2023-04-10 19:34:42 -0400

    fix: changed the id to match

--- src/components/GameView/SpectatorListOverlay.vue
+++ src/components/GameView/SpectatorListOverlay.vue
@@ -5,7 +5,7 @@
       <v-btn
         data-cy="spectate-list-button"
         class="pl-2"
-        id="game-menu-activator"
+        id="spectating-menu-activator"
         v-bind="props"
         icon
         variant="text"
@@ -15,7 +15,6 @@
       </v-btn>
     </template>
     <!-- Menu -->
-
     <v-list id="spectatorList" data-cy="spectate-list-menu">
       <v-list-item><h3>Spectators</h3></v-list-item>
       <v-list-item v-for="spectator in spectatingUsers">{{ spectator }}</v-list-item>
