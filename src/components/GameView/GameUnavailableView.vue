56bd76d64ed011dc57bb2da394c6277cb2b3533a
Henry Aviles <haviles04@gmail.com>
2023-05-30 17:55:24 -0400

    refactor: moved to own component, updated computed logic to dive deeper

--- src/components/GameView/GameUnavailableView.vue
+++ src/components/GameView/GameUnavailableView.vue
@@ -6,41 +6,21 @@
     </template>
     <!-- Game doesn't exist-->
     <template v-else>
-      <v-overlay
-        data-cy="unavailable-game-overlay"
-        id="unavailable-game-scrim"
-        v-model="unavailableGame"
-        class="game-overlay text-center d-flex justify-center align-center"
-      >
-        <div class="overlay-header">
-          <h1 :class="[this.$vuetify.display.xs === true ? 'text-h5' : 'text-h3', ['font-weight-bold']]">
-            Oops!
-          </h1>
-          <p>Looks like this game isn't available</p>
-        </div>
-        <v-btn
-          color="secondary"
-          class="mt-4"
-          data-cy="leave-unavailable-game-button"
-          :loading="leavingGame"
-          @click="this.$router.push('/')"
-        >
-          Go Home
-        </v-btn>
-      </v-overlay>
+      <game-unavailable-overlay :unavailableGame="unavailableGame" />
     </template>
   </div>
 </template>
 
 <script>
 import ReauthenticateDialog from '@/components/GameView/ReauthenticateDialog.vue';
+import GameUnavailableOverlay from './GameUnavailableOverlay.vue';
 
 export default {
   name: 'GameUnavailable',
-  components: { ReauthenticateDialog },
+  components: { ReauthenticateDialog, GameUnavailableOverlay },
   computed: {
     unavailableGame() {
-      return !this.$store.game;
+      return !this.$store.state.game.id;
     },
     mustReauthenticate: {
       get() {
@@ -53,23 +33,3 @@ export default {
   },
 };
 </script>
-
-<style scoped lang="scss">
-:deep(.v-overlay__content) {
-  left: 0;
-}
-.game-overlay {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.overlay-header {
-  font-weight: bold;
-  background-color: rgba(var(--v-theme-surface-2));
-  color: rgba(var(--v-theme-surface-1));
-  padding: 24px;
-  text-align: center;
-  width: 100vw;
-}
-</style>
