1b88bb275acc5ac18063825264d7a263cabf0256
Henry Aviles <haviles04@gmail.com>
2023-06-10 20:10:43 -0400

    test: renamed and re-ordred test

--- tests/e2e/specs/in-game/sevens.spec.js
+++ tests/e2e/specs/in-game/sevens.spec.js
@@ -1488,27 +1488,7 @@ describe('Playing sevens at the end of the deck', () => {
     });
   });
 
-  it('Cannot play one-off on last card in deck after playing seven one off - special case', () => {
-    cy.setupGameAsP1();
-    cy.loadGameFixture(1, {
-      p0Hand: [],
-      p0Points: [],
-      p0FaceCards: [],
-      p1Hand: [Card.SEVEN_OF_CLUBS, Card.ACE_OF_CLUBS],
-      p1Points: [],
-      p1FaceCards: [],
-      topCard: Card.SIX_OF_HEARTS,
-      secondCard: Card.SEVEN_OF_HEARTS,
-      deck: [],
-    });
-    cy.drawCardOpponent();
-    cy.get('#deck').click();
-    cy.passOpponent();
-    cy.get('[data-player-hand-card=7-0]').click();
-    cy.get('[data-move-choice=oneOff').should('have.class', 'v-card--disabled');
-  });
-
-  it('Cannot play one-off if no cards in deck', () => {
+  it('Cannot play one-off if deck is empty', () => {
     cy.setupGameAsP1();
     cy.loadGameFixture(1, {
       p0Hand: [],
@@ -1524,6 +1504,26 @@ describe('Playing sevens at the end of the deck', () => {
     cy.drawCardOpponent();
     cy.get('#deck').click();
     cy.drawCardOpponent();
+    cy.get('[data-player-hand-card=7-0]').click();
+    cy.get('[data-move-choice=oneOff').should('have.class', 'v-card--disabled');
+  });
+
+  it('Cannot play one-off on last card (7) in deck after playing 7 one off - special case', () => {
+    cy.setupGameAsP1();
+    cy.loadGameFixture(1, {
+      p0Hand: [],
+      p0Points: [],
+      p0FaceCards: [],
+      p1Hand: [Card.SEVEN_OF_CLUBS, Card.ACE_OF_CLUBS],
+      p1Points: [],
+      p1FaceCards: [],
+      topCard: Card.SIX_OF_HEARTS,
+      secondCard: Card.SEVEN_OF_HEARTS,
+      deck: [],
+    });
+    cy.drawCardOpponent();
+    cy.get('#deck').click();
+    cy.passOpponent();
     cy.get('[data-player-hand-card=7-0]').click();
     cy.get('[data-move-choice=oneOff').should('have.class', 'v-card--disabled');
   });
