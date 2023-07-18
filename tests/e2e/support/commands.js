353d6b5e0940b13ddcc86498b030e21a83a6416c
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-07-18 16:51:39 -0400

    choore: remove cy.logs

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -78,9 +78,7 @@ Cypress.Commands.add('setupGameAsP0', (alreadyAuthenticated = false, isRanked = 
     try {
       cy.subscribeOpponent(gameSummary.gameId);
     } catch {
-      cy.log('Caught failed subsciption to game');
       cy.recoverSessionOpponent(opponentOne);
-      cy.log('Successfully recovered session');
       cy.subscribeOpponent(gameSummary.gameId);
     }
     cy.readyOpponent();
