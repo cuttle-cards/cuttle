457847613f3dad70f922b4c7c8a37c5da27449a1
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-08-02 21:52:53 -0400

    fix: stop requesting to leave games from the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -164,16 +164,6 @@ export default {
     },
   },
   async created() {
-    // Leave any existing lobbies before getting the game list
-    // Need to make sure we do these in order so the order of operations is:
-    //     leave -> list
-    try {
-      await this.$store.dispatch('requestLeaveLobby');
-    } catch (err) {
-      // A user may not actually be in a lobby, so we need to
-      // swallow this error so the component doesn't error out
-      // TODO: Improve this logic with an isInLobby helper
-    }
     await this.$store.dispatch('requestGameList');
   },
   methods: {
