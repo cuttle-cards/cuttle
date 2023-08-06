3dc3bd513a6f42ccd7784d36d2f4d54a6e76450b
Harsh <harsh.acharya2001@gmail.com>
2023-08-07 00:24:41 +0530

    Fix issue: #473

--- src/components/GameView/ScrapDialog.vue
+++ src/components/GameView/ScrapDialog.vue
@@ -5,43 +5,38 @@
         <slot name="activator" />
       </span>
     </template>
-    <v-card id="scrap-dialog">
-      <v-card-title class="d-flex justify-space-between">
-        <h1>Scrap Pile</h1>
-        <v-btn icon data-cy="close-scrap-dialog-x" @click="show = false">
-          <v-icon icon="mdi-close" size="large" />
-        </v-btn>
-      </v-card-title>
-      <v-card-text>
-        <div class="mt-4">
-          <card-list-sortable
-            :cards="scrap"
-            empty-text="There are no cards in the scrap pile."
-            data-selector-prefix="scrap-dialog"
-          />
-        </div>
-      </v-card-text>
-      <v-card-actions class="d-flex justify-end my-2">
-        <v-btn
-          color="primary"
-          variant="outlined"
-          data-cy="close-scrap-dialog-button"
-          @click="show = false"
-        >
-          Close
-        </v-btn>
-      </v-card-actions>
-    </v-card>
   </v-dialog>
+  <base-dialog 
+    v-if="scrap" 
+    v-model="show"
+    title="Scrap Pile" 
+    id="scrap-pile-dialog">
+    <template #body>
+      <div class="mt-4">
+        <card-list-sortable
+          :cards="scrap"
+          empty-text="There are no cards in the scrap pile."
+          data-selector-prefix="scrap-dialog"
+        />
+      </div>
+    </template>
+    <template #actions>
+      <v-btn data-cy="close-scrap-dialog-button" color="surface-1" variant="flat" @click="show = false">
+        Close
+      </v-btn>
+    </template>
+  </base-dialog>
 </template>
 
 <script>
 import CardListSortable from '@/components/GameView/CardListSortable.vue';
+import BaseDialog from '@/components/Global/BaseDialog.vue';
 
 export default {
   name: 'ScrapDialog',
   components: {
     CardListSortable,
+    BaseDialog,
   },
   props: {
     scrap: {
