8d33557295dc52641e6814091234f03993946376
Harsh <harsh.acharya2001@gmail.com>
2023-08-08 23:21:04 +0530

    Resolved e2e Test issue

--- src/components/GameView/ScrapDialog.vue
+++ src/components/GameView/ScrapDialog.vue
@@ -1,9 +1,23 @@
 <template>
-  <base-dialog v-model="show" title="Scrap Pile" id="scrap-pile-dialog">
+  <base-dialog v-model="show" id="scrap-dialog">
     <template #activator="{ props }">
       <span v-bind="props">
         <slot name="activator" />
       </span>
+    </template>
+    <template #title>
+      <div class="d-flex justify-space-between align-center w-100">
+        <h1>Scrap Pile</h1>
+        <v-btn 
+          icon
+          color="surface-1"
+          elevation="0"
+          data-cy="close-scrap-dialog-x" 
+          @click="show = false"
+        >
+          <v-icon icon="mdi-close" size="large" />
+        </v-btn>
+      </div>
     </template>
     <template #body>
       <div class="mt-4">
@@ -15,7 +29,12 @@
       </div>
     </template>
     <template #actions>
-      <v-btn data-cy="close-scrap-dialog-button" color="surface-1" variant="flat" @click="show = false">
+      <v-btn
+        data-cy="close-scrap-dialog-button"
+        color="surface-1"
+        variant="flat"
+        @click="show = false"
+      >
         Close
       </v-btn>
     </template>
