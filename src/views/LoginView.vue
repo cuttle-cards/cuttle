a852ddffe5b16fcdb6e0bdd4b203b7476ac6a60b
Harsh <harsh.acharya2001@gmail.com>
2023-08-11 21:18:55 +0530

    Improved Fix

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -108,8 +108,7 @@
               </div>
 
               <v-btn
-                class="w-100 my-10  h-auto py-2"
-                :class="$vuetify.display.xs ? 'text-subtitle-2' : 'text-h6'"
+                class="w-100 my-10 text-subtitle-2 text-sm-subtitle-1 text-md-h6 h-auto py-2"
                 size="large"
                 color="newSecondary"
                 href="https://discord.com/invite/9vrAZ8xGyh"
