b7ec0e59f759d49f21fc87fac39e3b9f1b907650
Henry Aviles <haviles04@gmail.com>
2023-08-25 10:41:42 -0400

    chore: removed .only

--- tests/e2e/specs/in-game/gameOver.spec.js
+++ tests/e2e/specs/in-game/gameOver.spec.js
@@ -485,7 +485,7 @@ describe('Creating And Updating Ranked Matches', () => {
     cy.loginPlayer(playerOne);
     cy.setupGameAsP0(true, true);
   });
-  it.only('Creates a match when two players play a ranked game for the first time this week', function () {
+  it('Creates a match when two players play a ranked game for the first time this week', function () {
     // There should be two matches initially (one from last week and one with a different opponent)
     cy.request('http://localhost:1337/match').then((res) => {
       expect(res.body.length).to.eq(2);
