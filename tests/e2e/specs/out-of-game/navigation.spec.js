4a08121b245339a9caeaa92f5da7d9912cb6f046
Harsh <harsh.acharya2001@gmail.com>
2023-08-27 21:23:27 +0530

    Expand/collapse navbar test removed

--- tests/e2e/specs/out-of-game/navigation.spec.js
+++ tests/e2e/specs/out-of-game/navigation.spec.js
@@ -7,40 +7,6 @@ describe('Navigation Drawer', () => {
     cy.signupPlayer(playerOne);
     cy.vueRoute('/');
   });
-  it('Expands and collapses navbar on desktop and always collapses on mobile', () => {
-    // Desktop display
-    cy.viewport(1920, 1080);
-    cy.get('[data-cy=nav-drawer]')
-      .should('be.visible')
-      .should('contain', 'Log Out')
-      .should('contain', 'Rules')
-      .should('not.have.class', 'v-navigation-drawer--rail')
-      // Collapse nav
-      .find('[data-cy=collapse-nav]')
-      .click();
-    // Nav drawer should be collapsed
-    cy.get('[data-cy=nav-drawer]')
-      .should('have.class', 'v-navigation-drawer--rail')
-      // Expand nav
-      .find('[data-cy=expand-nav]')
-      .click();
-    // Should be expanded again
-    cy.get('[data-cy=nav-drawer]')
-      .should('be.visible')
-      .should('contain', 'Log Out')
-      .should('contain', 'Rules')
-      .should('not.have.class', 'v-navigation-drawer--rail');
-
-    // Mobile display
-    cy.viewport('iphone-8');
-    cy.get('[data-cy=nav-drawer]')
-      .should('be.visible')
-      .should('have.class', 'v-navigation-drawer--rail')
-      // Collapse and expand buttons not displayed on mobile
-      .find('[data-cy=expand-nav]')
-      .should('not.exist');
-    cy.get('[data-cy=collapse-nav]').should('not.exist');
-  });
 
   describe('Navigates to Rules, Home, Stats, and Login pages', () => {
     function verifyAuthenticatedLinks() {
