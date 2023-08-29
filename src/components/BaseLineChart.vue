9700027282b18a14e7727cb74f3bcacd954cd3b5
kawaljen <kawaljen@hotmail.fr>
2023-08-29 15:44:53 +0200

    fix lint error

--- src/components/BaseLineChart.vue
+++ src/components/BaseLineChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <Line
+  <LineChart
     :id="id"
     :options="chartOptions"
     :data="chartData"
@@ -7,14 +7,14 @@
 </template>
 
 <script>
-import { Line } from 'vue-chartjs';
+import {Line as LineChart} from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale } from 'chart.js';
 
 ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, CategoryScale, LinearScale);
 
 export default {
   name: 'BarChart',
-  components: { Line },
+  components: { LineChart },
   props: {
     id: {
       type: String,
