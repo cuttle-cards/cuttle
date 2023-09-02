a5a3e9499c23be545cd596ff47df9e1347d3402b
Sean Kennedy <skennedy@urbn.com>
2023-09-02 18:25:46 -0400

    Add bump scripts and a fun ascii image

---
+++ tools/msg-bump-version.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-useless-escape */
+
+const msg = `
+
+      ,-.           ,.---'''^\                  O
+      {   \       ,__\,---''''''-.,      O    O
+      |    \     ','^           _  '..     o
+      \  ,.J..-''          // (O)   ,,X,    o
+      /  (_               ((   ~  ,;:'''  o
+      /   ,.X'.,            \\      ':;;;
+      (_../      -._                 ,,'
+                K.=,;.__ /^~/___..'''
+                        /  /'
+
+  Don't forget to bump the version in package.json!
+    - patch: 0.0.X
+    - minor: 0.X.0
+    - major: X.0.0
+
+`;
+
+console.log(msg);
