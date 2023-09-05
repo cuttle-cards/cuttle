1656d8c90655fc5c141fb68462127cbdfbe138d7
Harsh <harsh.acharya2001@gmail.com>
2023-09-06 00:56:25 +0530

    Added Mobile Nav Test

--- tests/e2e/specs/out-of-game/navigation.spec.js
+++ tests/e2e/specs/out-of-game/navigation.spec.js
@@ -1,6 +1,6 @@
 import { playerOne } from '../../fixtures/userFixtures';
 
-describe('Navigation Drawer', () => {
+describe('Navigation', () => {
   beforeEach(() => {
     cy.wipeDatabase();
     cy.visit('/');
@@ -8,29 +8,49 @@ describe('Navigation Drawer', () => {
     cy.vueRoute('/');
   });
 
-  describe('Navigates to Rules, Home, Stats, and Login pages', () => {
-    function verifyAuthenticatedLinks() {
-      // Navigate to Rules
+  describe('Desktop Navigation', () => {
+    beforeEach(() => {
       cy.viewport(1920, 1080);
-      cy.get('[data-cy=About]').click();
-      cy.hash().should('equal', '#/rules');
-      // Navigate to Home (Play)
-      cy.get('[data-cy=Play]').click();
-      cy.hash().should('equal', '#/');
-      // Navigate to Stats
-      cy.get('[data-cy=Stats]').click();
-      cy.hash().should('equal', '#/stats');
-      // Log out
-      cy.get('[data-cy="user-menu"]').click();
-      cy.get("[data-nav='Log Out']").click();
-      cy.hash().should('equal', '#/login');
-    }
+    });
+
     it('When authenticated', () => {
       verifyAuthenticatedLinks();
     });
+
     it('When authenticated on reload', () => {
       cy.reload();
       verifyAuthenticatedLinks();
     });
   });
+
+  describe('Mobile Navigation', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-8');
+    });
+
+    it('When authenticated', () => {
+      verifyAuthenticatedLinks();
+    });
+
+    it('When authenticated on reload', () => {
+      cy.reload();
+      verifyAuthenticatedLinks();
+    });
+  });
+
+  function verifyAuthenticatedLinks() {
+    // Navigate to Rules
+    cy.get('[data-cy=About]').click();
+    cy.hash().should('equal', '#/rules');
+    // Navigate to Home (Play)
+    cy.get('[data-cy=Play]').click();
+    cy.hash().should('equal', '#/');
+    // Navigate to Stats
+    cy.get('[data-cy=Stats]').click();
+    cy.hash().should('equal', '#/stats');
+    // Log out
+    cy.get('[data-cy="user-menu"]').click();
+    cy.get("[data-nav='Log Out']").click();
+    cy.hash().should('equal', '#/login');
+  }
 });
