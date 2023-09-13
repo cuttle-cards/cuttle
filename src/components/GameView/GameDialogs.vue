4715d0007a5b30b8518c7da51ff633389dcd9140
Henry Aviles <haviles04@gmail.com>
2023-09-13 19:44:46 -0400

    refactor: converted to use pinia

--- src/components/GameView/GameDialogs.vue
+++ src/components/GameView/GameDialogs.vue
@@ -12,13 +12,13 @@
     <cannot-counter-dialog
       :model-value="showCannotCounterDialog"
       :one-off="game.oneOff"
-      :opponent-queen-count="opponentQueenCount"
+      :opponent-queen-count="gameStore.opponentQueenCount"
       :player-two-count="playerTwoCount"
       :twos-played="twosPlayed"
       :target="game.oneOffTarget"
       @resolve="resolve"
     />
-    <four-dialog :model-value="discarding" @discard="discard" />
+    <four-dialog :model-value="gameStore.discarding" @discard="discard" />
     <three-dialog
       :model-value="pickingFromScrap"
       :one-off="game.oneOff"
@@ -34,7 +34,7 @@
     <game-over-dialog
       v-if="gameIsOver"
       :model-value="gameIsOver"
-      :player-wins-game="playerWins"
+      :player-wins-game="gameStore.playerWins"
       :stalemate="stalemate"
     />
     <reauthenticate-dialog :model-value="mustReauthenticate" />
@@ -43,9 +43,9 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { mapState } from 'vuex';
-
+import { mapStores } from 'pinia';
+import { useGameStore } from '@/stores/game';
+import { useAuthStore } from '@/stores/auth';
 import CannotCounterDialog from '@/components/GameView/CannotCounterDialog.vue';
 import CounterDialog from '@/components/GameView/CounterDialog.vue';
 import FourDialog from '@/components/GameView/FourDialog.vue';
@@ -69,23 +69,13 @@ export default {
   },
   emits: ['clear-selection', 'handle-error'],
   computed: {
-    ...mapState({
-      playingFromDeck: ({ game }) => game.playingFromDeck,
-    }),
-
-    ...mapGetters([
-      'discarding',
-      'player',
-      'opponent',
-      'opponentPointTotal',
-      'opponentQueenCount',
-      'playerWins',
-    ]),
+    ...mapStores(useGameStore),
+    ...mapStores(useAuthStore),
     game() {
       // TODO: Figure out a better way to do this, mapping the whole module is a
       // bit unusual-- the usages can probably be changed to only need a subset,
       // or moved to some sort of store method or getter
-      return this.$store.state.game;
+      return this.gameStore;
     },
     gameIsOver() {
       return this.game.gameIsOver;
@@ -99,57 +89,57 @@ export default {
     // TODO: Refactor and combine usage in GameView.vue
     mustReauthenticate: {
       get() {
-        return this.$store.state.auth.mustReauthenticate;
+        return this.authStore.mustReauthenticate;
       },
       set(val) {
-        this.$store.commit('setMustReauthenticate', val);
+        this.authStore.setMustReauthenticate(val);
       },
     },
     pickingFromScrap() {
       return this.game.pickingFromScrap;
     },
     playerTwoCount() {
-      return this.twoCount(this.player);
+      return this.twoCount(this.gameStore.player);
     },
     scrap() {
       return this.game.scrap;
     },
     secondCard() {
-      return this.$store.state.game.secondCard;
+      return this.gameStore.secondCard;
     },
     showCannotCounterDialog() {
       return (
         (this.myTurnToCounter && !this.hasTwoInHand) ||
-        (this.myTurnToCounter && this.hasTwoInHand && this.opponentQueenCount > 0)
+        (this.myTurnToCounter && this.hasTwoInHand && this.gameStore.opponentQueenCount > 0)
       );
     },
     showCounterDialog() {
-      return this.myTurnToCounter && this.hasTwoInHand && this.opponentQueenCount === 0;
+      return this.myTurnToCounter && this.hasTwoInHand && this.gameStore.opponentQueenCount === 0;
     },
     showSevenDoubleJacksDialog() {
       return (
-        this.playingFromDeck &&
+        this.gameStore.playingFromDeck &&
         this.topCard.rank === 11 &&
         (!this.secondCard || this.secondCard.rank === 11) &&
-        (this.opponentPointTotal === 0 || this.opponentQueenCount > 0)
+        (this.gameStore.opponentPointTotal === 0 || this.gameStore.opponentQueenCount > 0)
       );
     },
     stalemate() {
-      return this.$store.state.game.gameIsOver && this.$store.state.game.winnerPNum === null;
+      return this.gameStore.gameIsOver && this.gameStore.winnerPNum === null;
     },
     consideringOpponentStalemateRequest: {
       get() {
-        return this.$store.state.game.consideringOpponentStalemateRequest;
+        return this.gameStore.consideringOpponentStalemateRequest;
       },
       set(val) {
-        this.$store.commit('setConsideringOpponentStalemateRequest', val);
+        this.gameStore.setConsideringOpponentStalemateRequest(val);
       },
     },
     topCard() {
-      return this.$store.state.game.topCard;
+      return this.gameStore.topCard;
     },
     twosInHand() {
-      return this.player.hand.filter((card) => card.rank === 2);
+      return this.gameStore.player.hand.filter((card) => card.rank === 2);
     },
     twosPlayed() {
       return this.game.twos;
@@ -160,12 +150,12 @@ export default {
       this.$emit('clear-selection');
     },
     counter(twoId) {
-      this.$store.dispatch('requestCounter', twoId).then(this.clearSelection).catch(this.handleError);
+      this.gameStore.requestCounter(twoId).then(this.clearSelection).catch(this.handleError);
     },
     discard(cardIds) {
       const [cardId1] = cardIds;
       const cardId2 = cardIds.length > 1 ? cardIds[1] : null;
-      this.$store.dispatch('requestDiscard', {
+      this.gameStore.requestDiscard({
         cardId1,
         cardId2,
       });
@@ -174,14 +164,13 @@ export default {
       this.$emit('handle-error');
     },
     resolve() {
-      this.$store.dispatch('requestResolve').then(this.clearSelection).catch(this.handleError);
+      this.gameStore.requestResolve().then(this.clearSelection).catch(this.handleError);
     },
     resolveThree(cardId) {
-      this.$store.dispatch('requestResolveThree', cardId).then(this.clearSelection).catch(this.handleError);
+      this.gameStore.requestResolveThree(cardId).then(this.clearSelection).catch(this.handleError);
     },
     resolveSevenDoubleJacks({ cardId, index }) {
-      this.$store
-        .dispatch('requestResolveSevenDoubleJacks', { cardId, index })
+      this.gameStore.requestResolveSevenDoubleJacks({ cardId, index })
         .then(this.clearSelection)
         .catch(this.handleError);
     },
