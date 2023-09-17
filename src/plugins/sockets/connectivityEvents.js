a7b859f61f4b189c6b64f7bd00b89f0bb8381dbe
Henry Aviles <haviles04@gmail.com>
2023-09-17 17:21:11 -0400

    fix: fixed minor mistakes

--- src/plugins/sockets/connectivityEvents.js
+++ src/plugins/sockets/connectivityEvents.js
@@ -3,19 +3,18 @@ import router from '@/router.js';
 import { ROUTE_NAME_GAME, ROUTE_NAME_SPECTATE, ROUTE_NAME_HOME } from '@/router';
 
 export function handleConnect() {
-  const authUserStore = useAuthStore();
+  const authStore = useAuthStore();
   // Request latest game state if socket reconnects during game
-  const { username } = authUserStore;
   switch (router.currentRoute.value.name) {
     case ROUTE_NAME_GAME:
-      return authUserStore.requestReauthenticate({ username });
+      return authStore.requestReauthenticate(authStore.username);
     case ROUTE_NAME_SPECTATE: {
       const gameId = Number(router.currentRoute.value.params.gameId);
       if (!Number.isInteger(gameId)) {
         router.push(ROUTE_NAME_HOME);
         return;
       }
-      return authUserStore.requestSpectate(gameId);
+      return authStore.requestSpectate(gameId);
     }
     default:
       return;
