dee0e19f95f239f17ad0ed290eb7e99469e7a5c2
Harsh <harsh.acharya2001@gmail.com>
2023-09-18 15:02:22 +0530

    refactor: i18n local abbervations

--- src/i18n.js
+++ src/i18n.js
@@ -4,22 +4,22 @@ import { createI18n } from 'vue-i18n';
 
 // TODO we should lazyload non global translations on a per page basis
 // https://vue-i18n.intlify.dev/guide/advanced/lazy.html
-import English from '@/translations/en.json';
-import Español from '@/translations/es.json';
-import Française from '@/translations/fr.json';
+import en from '@/translations/en.json';
+import es from '@/translations/es.json';
+import fr from '@/translations/fr.json';
 
 const messages = {
- English,
- Español,
- Française,
+ en,
+ es,
+ fr,
 };
 
 const i18n = createI18n({
   legacy: false,
   globalInjection: true,
-  locale: 'English',
+  locale: 'en',
   // https://vue-i18n.intlify.dev/guide/essentials/fallback.html
-  fallbackLocale: 'English',
+  fallbackLocale: 'en',
   messages,
 });
 
