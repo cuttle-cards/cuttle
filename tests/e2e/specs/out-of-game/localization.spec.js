0a260238a08333b234dc8b54d8c607eb79e6e86e
Harsh Acharya <72140070+haaarsh619@users.noreply.github.com>
2023-09-26 03:44:48 +0530

    feat: using preferred language from local storage (#574)

--- tests/e2e/specs/out-of-game/localization.spec.js
+++ tests/e2e/specs/out-of-game/localization.spec.js
@@ -1,37 +1,56 @@
 import { myUser } from '../../fixtures/userFixtures';
 import es from '../../../../src/translations/es.json';
 import fr from '../../../../src/translations/fr.json';
+import en from '../../../../src/translations/en.json';
 
-function setup() {
-  cy.viewport(1920, 1080);
-  cy.wipeDatabase();
-  cy.visit('/');
-  cy.signupPlayer(myUser);
-  cy.vueRoute('/');
-}
-
-describe('Localization ', () => {
-  beforeEach(setup);
-
-  it('Checking Translation For Spanish', () => {
-    checkTranslation ('es', es);
+describe('Localization', () => {
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
+    cy.wipeDatabase();
+    cy.visit('/');
+    cy.signupPlayer(myUser);
+    cy.vueRoute('/');
   });
 
-  it('Checking Translation For French', () => {
-    checkTranslation ('fr', fr);
+  const checkAndChangeLanguage = (name, lang) => {
+    // Open the user menu
+    cy.get('[data-cy="user-menu"]').click();
+    cy.get('[data-cy="language-menu"]').click();
+
+    // Select the language
+    cy.get(`[data-lang="${name}"]`).click();
+
+    // Check the translation
+    checkTranslation(lang);
+  };
+
+  const checkTranslation = (lang) => {
+    // Check elements in the navbar for language translation
+    cy.get('[data-cy="Play"]').should('contain', lang.global.play);
+
+    // Log out and check the login page translation
+    cy.get('[data-nav="Log Out"]').click();
+    cy.reload();
+    cy.contains('h1', lang.login.title);
+    cy.get('[data-cy="submit"]').should('contain', lang.global.login);
+    cy.loginPlayer(myUser);
+    cy.reload();
+
+    // Check elements in the navbar again
+    cy.get('[data-cy="Play"]').should('contain', lang.global.play);
+    cy.get('[data-cy="user-menu"]').click();
+    cy.get('[data-nav="Log Out"]').should('contain', lang.global.logout);
+  };
+
+  it('Should check translation for English (default)', () => {
+    checkAndChangeLanguage('en', en);
+  });
+
+  it('Should check translation for Spanish', () => {
+    checkAndChangeLanguage('es', es);
+  });
+
+  it('Should check translation for French', () => {
+    checkAndChangeLanguage('fr', fr);
   });
 });
-
-function checkTranslation(name, lang) {
-  // Open the menu
-  cy.get('[data-cy="user-menu"]').click();
-  cy.get('[data-cy="language-menu"]').click();
-
-  //Selects the language 
-  cy.get(`[data-lang="${name}"]`).click();
-
-  // checks elements in navbar has changed language
-  cy.get('[data-cy="Play"]').should('contain', lang.global.play);
-  cy.get('[data-cy="user-menu"]').click();
-  cy.get('[data-nav="Log Out"]').should('contain', lang.global.logout);
-}
