4f83916b662f067a7f2d00a02ec3a7082116a15d
Archit Nautiyal <nautiyalarchit187@gmail.com>
2023-10-11 19:35:34 +0530

    fix: removed extra whitespaces

--- src/routes/home/components/GameListItem.vue
+++ src/routes/home/components/GameListItem.vue
@@ -55,7 +55,7 @@
 </template>
 
 <script>
-import GameStatus  from '_/utils/GameStatus.json';
+import GameStatus from '_/utils/GameStatus.json';
 import { mapStores } from 'pinia';
 import { useGameStore } from '@/stores/game';
 import { useGameListStore } from '@/stores/gameList';
