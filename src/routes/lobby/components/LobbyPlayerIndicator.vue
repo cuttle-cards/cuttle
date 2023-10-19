77be8d986a3cd5117fc781f2e45df7b241702865
Harsh <harsh.acharya2001@gmail.com>
2023-10-20 00:46:38 +0530

    feat: redesign lobby

--- src/routes/lobby/components/LobbyPlayerIndicator.vue
+++ src/routes/lobby/components/LobbyPlayerIndicator.vue
@@ -1,21 +1,16 @@
 <template>
-  <div
-    class="player-indicator"
-    :class="{ ready: playerReady, 'player-in': playerUsername != null }"
-    :style="{ padding: playerPadding }"
-    elevation="1"
-  >
-    <div class="avatar text-truncate">
+  <div v-if="playerUsername" class="player-card">
+    <span class="player-name">{{ playerUsername }}</span>
+    <Transition name="card-flip">
+      <img v-if="playerReady" src="/img/cards/card-ready.png" class="opponent-card-back" alt="card back" />
+      <img v-else src="/img/cards/card-back.png" class="opponent-card-back" alt="card back" />
+    </Transition>
+  </div>
+  <div v-else class="player-indicator" :style="{ padding: playerPadding }">
+    <div class="avatar">
       <h3>
         {{ message }}
       </h3>
-    </div>
-    <div
-      v-if="playerReady"
-      class="ready-overlay text-truncate"
-      :style="{ 'font-size': readyFontSize, 'padding-bottom': readyPadding }"
-    >
-      {{ t('lobby.ready') }}
     </div>
   </div>
 </template>
@@ -56,16 +51,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.opponent-card-back {
+  border-radius: 10px;
+  max-height: 40vh;
+  max-width: calc(40vh / 1.45);
+  width: 100%;
+}
+.card-flip-enter-active {
+  transition: all 1s;
+}
+.card-flip-enter-from {
+  transform: rotateY(-90deg);
+}
+.card-flip-enter-to {
+  transform: rotateY(0deg);
+}
+.player-card {
+  display: grid;
+  justify-content: center;
+}
+.player-name {
+  padding: 5px;
+  font-size: 1.5rem;
+}
 .player-indicator {
   position: relative;
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(var(--v-theme-neutral-lighten3));
   border-radius: 8px;
-}
-.player-in {
-  background-image: linear-gradient(45deg, rgba(var(--v-theme-secondary)), rgba(var(--v-theme-primary)));
 }
 .avatar {
   display: inline-block;
@@ -76,24 +90,19 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 4px dashed rgba(var(--v-theme-accent-darken1));
-  background-color: rgba(var(--v-theme-neutral));
+  border: 4px dashed rgba(var(--v-theme-surface-2));
+  background-color: rgba(var(--v-theme-surface-1));
   z-index: 1;
 }
-.ready .avatar {
-  border: 4px solid rgba(var(--v-theme-accent));
-}
-.ready-overlay {
-  display: flex;
-  position: absolute;
-  justify-content: center;
-  align-items: flex-end;
-  padding-top: 16px;
-  height: 100%;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  color: #ffffff;
-  border-radius: 8px;
-  box-sizing: border-box;
+
+@media (max-width: 660px) {
+  .player-name {
+    padding: 2px;
+    font-size: 1rem;
+  }
+  .opponent-card-back {
+    max-height: 20vh;
+    max-width: calc(20vh / 1.45);
+  }
 }
 </style>
