36eec5bb61715ecfd007923d2abdb5720178ae98
Henry Aviles <haviles04@gmail.com>
2023-10-31 16:35:03 -0400

    removed pwa plugin, added manifest

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
-import { VitePWA } from 'vite-plugin-pwa';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -27,76 +26,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       vue(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        devOptions: {
-          enabled: true,
-          type: 'module',
-        },
-        selfDestroying: true,
-        manifest: {
-          name: 'Cuttle.cards',
-          short_name: 'Cuttle',
-          description: 'The deepest card game under the sea!',
-          theme_color: '#FFF4D7',
-          background_color: '#FFF4D7',
-          display: 'fullscreen',
-          id: '/',
-          start_url: '/',
-          icons: [
-            {
-              src: '/pwa-icons/logo-head-72.png',
-              sizes: '72x72',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-96.png',
-              sizes: '96x96',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-120.png',
-              sizes: '120x120',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-128.png',
-              sizes: '128x128',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-144.png',
-              sizes: '144x144',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-152.png',
-              sizes: '152x152',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-180.png',
-              sizes: '180x180',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-384.png',
-              sizes: '384x384',
-              type: 'image/png',
-            },
-            {
-              src: '/pwa-icons/logo-head-512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
-        },
-      }),
       vuetify({
         autoImport: false,
         styles: { configFile: 'src/sass/variables.scss' },
