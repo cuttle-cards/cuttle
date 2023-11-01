68630d26ffbbe0490295cd255ffafbfe941c431c
Henry Aviles <haviles04@gmail.com>
2023-11-01 17:21:28 -0400

    test: added the

--- tests/e2e/specs/in-game/one-offs/0_countering.spec.js
+++ tests/e2e/specs/in-game/one-offs/0_countering.spec.js
@@ -199,7 +199,7 @@ describe('Countering One-Offs', () => {
     });
   });
 
-  it('Triple counters successfully', () => {
+  it.only('Triple counters successfully', () => {
     cy.loadGameFixture(1, {
       // Opponent is P0
       p0Hand: [Card.ACE_OF_CLUBS, Card.TWO_OF_CLUBS],
@@ -227,9 +227,8 @@ describe('Countering One-Offs', () => {
     cy.log('Player counters again (3rd counter');
     cy.get('#counter-dialog')
       .should('be.visible')
-      .contains('Your opponent has played 2♣️ to Counter your 2♥️', {includeShadowDom: true});
-    cy.get('[data-cy=counter]')
-      .click();
+      .contains('Your opponent has played the 2♣️ to Counter your 2♥️', { includeShadowDom: true });
+    cy.get('[data-cy=counter]').click();
     cy.get('#choose-two-dialog').should('be.visible').get('[data-counter-dialog-card=2-3]').click();
     cy.get('#waiting-for-opponent-counter-scrim').should('be.visible');
     // Opponent resolves
@@ -406,9 +405,8 @@ describe('Countering One-Offs P0 Perspective', () => {
     cy.log('Player counters back (2nd counter)');
     cy.get('#counter-dialog')
       .should('be.visible')
-      .contains('Your opponent has played 2♥️ to Counter', {includeShadowDom: true});
-    cy.get('[data-cy=counter]')
-      .click();
+      .contains('Your opponent has played 2♥️ to Counter', { includeShadowDom: true });
+    cy.get('[data-cy=counter]').click();
 
     cy.get('#choose-two-dialog').should('be.visible').get('[data-counter-dialog-card=2-0]').click();
 
@@ -420,9 +418,8 @@ describe('Countering One-Offs P0 Perspective', () => {
     cy.log('Player counters (4th counter)');
     cy.get('#counter-dialog')
       .should('be.visible')
-      .contains('Your opponent has played 2♠️ to Counter your 2♣️', {includeShadowDom: true});
-    cy.get('[data-cy=counter]')
-      .click();
+      .contains('Your opponent has played 2♠️ to Counter your 2♣️', { includeShadowDom: true });
+    cy.get('[data-cy=counter]').click();
     cy.get('#choose-two-dialog').should('be.visible').get('[data-counter-dialog-card=2-1]').click();
     cy.get('#waiting-for-opponent-counter-scrim').should('be.visible');
 
@@ -475,9 +472,8 @@ describe('Countering One-Offs P0 Perspective', () => {
     cy.get('#cannot-counter-dialog')
       .should('be.visible')
       .should('contain', 'You cannot Counter, because your opponent has a queen.')
-      .contains('Your opponent has played the 2♥️ to Counter', {includeShadowDom: true});
-    cy.get('[data-cy=cannot-counter-resolve]')
-      .click();
+      .contains('Your opponent has played the 2♥️ to Counter', { includeShadowDom: true });
+    cy.get('[data-cy=cannot-counter-resolve]').click();
   });
 });
 
