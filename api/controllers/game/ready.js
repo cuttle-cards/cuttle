d9521fb807bcdd5d8f8a3c8ccd5189c0d20f07ea
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-11-21 10:02:34 -0500

    fix(ready.js): query for players and add them to the game at beginning of method

--- api/controllers/game/ready.js
+++ api/controllers/game/ready.js
@@ -1,10 +1,12 @@
-const userAPI = sails.hooks['customuserhook'];
-
 module.exports = async function (req, res) {
   try {
+    // Query for game and users
     const game =  await sails.helpers.lockGame(req.session.game);
-    const user = await userAPI.findUser(req.session.usr);
+    const players = await User.find({game: req.session.game}).sort((p1, p2) => p1.pNum - p2.pNum);
+    const user = players[ req.session.pNum ];
+    game.players = players;
 
+    // Determine who is ready
     let { pNum } = user;
     let bothReady = false;
     const gameUpdates = {};
@@ -22,6 +24,8 @@ module.exports = async function (req, res) {
         }
         break;
     }
+
+    // Start game if both players are ready
     if (bothReady) {
       // Inform all clients this game has started
       sails.sockets.blast('gameStarted', { gameId: game.id });
