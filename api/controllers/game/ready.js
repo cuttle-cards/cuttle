0d6d3d74dd8ab6e591063fb0c6ccbe2123c4c6f1
Ryan Emberling <itsalaidbacklife@gmail.com>
2023-11-21 08:46:42 -0500

    refactor(ready.js): rework ready.js to use async/await

--- api/controllers/game/ready.js
+++ api/controllers/game/ready.js
@@ -1,61 +1,50 @@
 const userAPI = sails.hooks['customuserhook'];
 
 module.exports = async function (req, res) {
-  if (req.session.game && req.session.usr) {
-    const promiseGame = sails.helpers.lockGame(req.session.game);
-    const promiseUser = userAPI.findUser(req.session.usr);
-    Promise.all([promiseGame, promiseUser])
-      // Assign player readiness
-      .then(function foundRecords(values) {
-        const [game, user] = values;
-        const { lock } = game;
-        let { pNum } = user;
-        let bothReady = false;
-        const gameUpdates = {};
-        switch (pNum) {
-          case 0:
-            gameUpdates.p0Ready = !game.p0Ready;
-            if (game.p1Ready) {
-              bothReady = true;
-            }
-            break;
-          case 1:
-            gameUpdates.p1Ready = !game.p1Ready;
-            if (game.p0Ready) {
-              bothReady = true;
-            }
-            break;
-        }
-        if (bothReady) {
-          // Inform all clients this game has started
-          sails.sockets.blast('gameStarted', { gameId: game.id });
-          // Ensure game is no longer available for new players to join
-          gameUpdates.status = gameService.GameStatus.STARTED;
-          // Create Cards
-          return gameService.dealCards(game, gameUpdates);
-        }
+  try {
+    const game =  await sails.helpers.lockGame(req.session.game);
+    const user = await userAPI.findUser(req.session.usr);
 
-        // Send socket message to players
-        Game.publish([game.id], {
-          change: 'ready',
-          userId: user.id,
-          pNum: user.pNum,
-          gameId: game.id,
-        });
+    let { pNum } = user;
+    let bothReady = false;
+    const gameUpdates = {};
+    switch (pNum) {
+      case 0:
+        gameUpdates.p0Ready = !game.p0Ready;
+        if (game.p1Ready) {
+          bothReady = true;
+        }
+        break;
+      case 1:
+        gameUpdates.p1Ready = !game.p1Ready;
+        if (game.p0Ready) {
+          bothReady = true;
+        }
+        break;
+    }
+    if (bothReady) {
+      // Inform all clients this game has started
+      sails.sockets.blast('gameStarted', { gameId: game.id });
+      // Ensure game is no longer available for new players to join
+      gameUpdates.status = gameService.GameStatus.STARTED;
+      // Create Cards
+      return gameService.dealCards(game, gameUpdates);
+    }
 
-        // Update database
-        const updateGamePromise = Game.updateOne({ id: game.id }).set(gameUpdates);
-        const unlockGamePromise = sails.helpers.unlockGame(lock);
-        return Promise.all([updateGamePromise, unlockGamePromise]);
-      }) //End foundRecords
-      .then(function respond() {
-        return res.ok();
-      })
-      .catch(function failed(err) {
-        return res.badRequest(err);
-      });
-  } else {
-    const err = { message: 'Missing game or player id' };
+    // Send socket message
+    Game.publish([game.id], {
+      change: 'ready',
+      userId: user.id,
+      pNum: user.pNum,
+      gameId: game.id,
+    });
+
+    const updateGamePromise = Game.updateOne({ id: game.id }).set(gameUpdates);
+    const unlockGamePromise = sails.helpers.unlockGame(game.lock);
+    await Promise.all([updateGamePromise, unlockGamePromise]);
+
+    return res.ok();
+  } catch (err) {
     return res.badRequest(err);
   }
 };
