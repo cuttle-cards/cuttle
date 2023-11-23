8d8ba47335f289a9cbde3f707f1d3b12d8f97c5e
Henry Aviles <haviles04@gmail.com>
2023-11-23 10:56:14 -0500

    Squashed commit of the following:
    commit 1ff82a8ba7cb405cdb59a6fcc5d530a4bf7ea52b
    Author: Cuttle Continuous Integration <itsalaidbacklife@gmail.com>
    Date:   Thu Nov 23 15:52:08 2023 +0000
    
        6.1.0
    
    commit 3400b75fe4d682df679bc771eb274d612ec5c16c
    Author: Ryan Emberling <itsalaidbacklife@gmail.com>
    Date:   Thu Nov 23 10:51:53 2023 -0500
    
        Fix/application locking (#783)
    
        * [WIP]feat: create baseline implementation of lock-game
    
        * feat: apply retries on delay to application lock
    
        * refactor: call .valueOf() earlier on timestamps that don't require manipulation
    
        * fix: set lockIsStaleTimeout freshly on each loop
    
        * feat: set lock-game to make 10 attempts over 5 seconds
    
        * chore: comment lock-game helper
    
        * add lock and lockedAt columns to Game
    
        * feat: implement unlock-game
    
        * feat: set lock retry time to 250ms (from 500)
    
        * feat: apply application lock/unlock to game/ready action
    
        * refactor: remove use of max time and replace with max attempts, replacing while loop with for loop
    
        * fix: await updateGame() call in lock-game
    
        * fix: allow null for lockedAt
    
        * refactor(lock-game): rename param to durationInMs
    
        * chore(lock-game): comment catch block
    
        * chore(lock-game): add whitespace inside {}s
    
        * chore(lock-game): add more whitespace around another {}
    
        * chore: remove comment about node version
    
        * Re-add audio tags and redefine opponent username (#719)
    
        * 5.48.6
    
        * feat: change lobby sound to bubbles3.mp3 (#787)
    
        * 5.48.7
    
        * rebase fix/application-locking from main
    
        * rebase fix/application-locking from main
    
        * 6.0.0
    
        * style(GameOverDialog): change button color to surface-2 (#800)
    
        * 6.0.1
    
        * fix(RulesView): replace mid-crown icon with mdi-crown (#799)
    
        * 6.0.2
    
        * refactor: revise lock-game helper to return the game record rather than just the lock
    
        * chore(lock-game): update comment
    
        * refactor(ready.js): rework ready.js to use async/await
    
        * fix(ready.js): replace a return statement with await
    
        * refactor(ready.js): update database before publishing socket event
    
        * refactor(ready.js): apply isInGame policy to ready.js
    
        * fix(ready.js): query for players and add them to the game at beginning of method
    
        * fix(ready.js): revise .sort() call to use sails ORM sort instead of native js array sort
    
        * fix(ready): prevent redundant socket emit and db update when game initializes
    
        * chore(ready.js): update comments
    
        * chore(unlock-game): update description to explain no-op success
    
        * feat(Game): switch lockedAt columnType to bigint
    
        ---------
    
        Co-authored-by: Jonas <40244574+JonasThers@users.noreply.github.com>

--- api/controllers/game/ready.js
+++ api/controllers/game/ready.js
@@ -1,55 +1,56 @@
-const gameAPI = sails.hooks['customgamehook'];
-const userAPI = sails.hooks['customuserhook'];
+module.exports = async function (req, res) {
+  try {
+    // Query for game and users
+    const game =  await sails.helpers.lockGame(req.session.game);
+    const players = await User.find({game: req.session.game}).sort('pNum');
+    const user = players[ req.session.pNum ];
+    game.players = players;
 
-module.exports = function (req, res) {
-  if (req.session.game && req.session.usr) {
-    const promiseGame = gameAPI.findGame(req.session.game);
-    const promiseUser = userAPI.findUser(req.session.usr);
-    Promise.all([promiseGame, promiseUser])
-      // Assign player readiness
-      .then(function foundRecords(values) {
-        const [game, user] = values;
-        let { pNum } = user;
-        let bothReady = false;
-        const gameUpdates = {};
-        switch (pNum) {
-          case 0:
-            gameUpdates.p0Ready = !game.p0Ready;
-            if (game.p1Ready) {
-              bothReady = true;
-            }
-            break;
-          case 1:
-            gameUpdates.p1Ready = !game.p1Ready;
-            if (game.p0Ready) {
-              bothReady = true;
-            }
-            break;
+    // Determine who is ready
+    let { pNum } = user;
+    let bothReady = false;
+    const gameUpdates = {};
+    switch (pNum) {
+      case 0:
+        gameUpdates.p0Ready = !game.p0Ready;
+        if (game.p1Ready) {
+          bothReady = true;
         }
-        if (bothReady) {
-          // Inform all clients this game has started
-          sails.sockets.blast('gameStarted', { gameId: game.id });
-          // Ensure game is no longer available for new players to join
-          gameUpdates.status = gameService.GameStatus.STARTED;
-          // Create Cards
-          return gameService.dealCards(game, gameUpdates);
+        break;
+      case 1:
+        gameUpdates.p1Ready = !game.p1Ready;
+        if (game.p0Ready) {
+          bothReady = true;
         }
-        Game.publish([game.id], {
-          change: 'ready',
-          userId: user.id,
-          pNum: user.pNum,
-          gameId: game.id,
-        });
-        return Game.updateOne({ id: game.id }).set(gameUpdates);
-      }) //End foundRecords
-      .then(function respond() {
-        return res.ok();
-      })
-      .catch(function failed(err) {
-        return res.badRequest(err);
+        break;
+    }
+
+    // Start game if both players are ready
+    if (bothReady) {
+      // Inform all clients this game has started
+      sails.sockets.blast('gameStarted', { gameId: game.id });
+      gameUpdates.status = gameService.GameStatus.STARTED;
+      // Deal cards (also emits socket event)
+      await gameService.dealCards(game, gameUpdates);
+
+    // Otherwise send socket message that player is ready
+    } else {
+
+      await Game.updateOne({ id: game.id }).set(gameUpdates);
+
+      Game.publish([game.id], {
+        change: 'ready',
+        userId: user.id,
+        pNum: user.pNum,
+        gameId: game.id,
       });
-  } else {
-    const err = { message: 'Missing game or player id' };
+
+    }
+
+    await sails.helpers.unlockGame(game.lock);
+
+    return res.ok();
+  } catch (err) {
     return res.badRequest(err);
   }
 };
