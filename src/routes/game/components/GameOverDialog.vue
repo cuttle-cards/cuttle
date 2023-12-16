3234c8ee611c0671a4396cc613d3c585f3f75058
kawaljen <declicsweb@gmail.com>
2023-12-16 10:34:00 +0100

    Translate the rematch text in the GameOverDialog

--- src/routes/game/components/GameOverDialog.vue
+++ src/routes/game/components/GameOverDialog.vue
@@ -56,10 +56,10 @@
         </div>
       </template>
       <p v-if="opponentWantsToRematch" data-cy="opponent-wants-rematch">
-        {{ gameStore.opponent.username }} {{t('game.dialogs.gameOverDialog.askRematch')}}
+        {{ gameStore.opponent.username }} {{ t('game.dialogs.gameOverDialog.askRematch') }}
       </p>
       <p v-if="opponentDeclinedRematch" data-cy="opponent-declined-rematch">
-        {{ gameStore.opponent.username }}  {{t('game.dialogs.gameOverDialog.declineRematch')}}
+        {{ gameStore.opponent.username }}  {{ t('game.dialogs.gameOverDialog.declineRematch') }}
       </p>
     </template>
 
