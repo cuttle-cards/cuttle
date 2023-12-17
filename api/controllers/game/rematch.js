e90e9103352d605373ecc1d576c08e5ce0592ae7
Henry Aviles <haviles04@gmail.com>
2023-12-17 15:50:08 -0500

    fix: updated names

--- api/controllers/game/rematch.js
+++ api/controllers/game/rematch.js
@@ -49,24 +49,24 @@ module.exports = async function (req, res) {
     updatedGame.rematchGame = newGame.id;
 
     //Get all exisiting rematchGames
-    let pastGames = [];
-    const getGames = async (gameId) => {
+    let rematchGames = [];
+    const getRematchGames = async (gameId) => {
       const [gameToAdd] = await Game.find({ rematchGame: gameId });
       if (gameToAdd) {
-        pastGames.unshift(gameToAdd);
+        rematchGames.unshift(gameToAdd);
         if (gameToAdd.rematchGame) {
-          await getGames(gameToAdd.id);
+          await getRematchGames(gameToAdd.id);
         }
       }
       return;
     };
-    await getGames(newGame.id);
-    const seriesP0 = [p0, p1].find(({ id }) => id === pastGames[0].p0);
-    const seriesP1 = [p0, p1].find(({ id }) => id === pastGames[0].p1);
+    await getRematchGames(newGame.id);
+    const seriesP0 = [p0, p1].find(({ id }) => id === rematchGames[0].p0);
+    const seriesP1 = [p0, p1].find(({ id }) => id === rematchGames[0].p1);
     //Get rematchGame win counts
-    let player0Wins = pastGames.filter(({winner}) => winner === seriesP0.id).length;
-    let player1wins = pastGames.filter(({winner}) => winner === seriesP1.id).length;
-    let stalemates = pastGames.filter(({winner}) => winner === null).length;
+    let player0Wins = rematchGames.filter(({winner}) => winner === seriesP0.id).length;
+    let player1wins = rematchGames.filter(({winner}) => winner === seriesP1.id).length;
+    let stalemates = rematchGames.filter(({winner}) => winner === null).length;
     
     const newName = `${seriesP0.username} VS ${seriesP1.username} ${player0Wins}-${player1wins}-${stalemates}`;
     await Game.updateOne({ id: newGame.id }).set({ name: newName });
