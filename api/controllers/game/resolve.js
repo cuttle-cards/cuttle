a1220b4472821e62da766457ed157a218ed5915a
Henry Aviles <haviles04@gmail.com>
2024-01-13 17:19:16 -0500

    fix: changed wording

--- api/controllers/game/resolve.js
+++ api/controllers/game/resolve.js
@@ -121,7 +121,7 @@ module.exports = function (req, res) {
               resolving: game.oneOff.id,
               log: [
                 ...game.log,
-                `The ${getCardName(game.oneOff)} one-off resolves; ${player.username} must discard one card, and will draw up to three cards.`,
+                `The ${getCardName(game.oneOff)} one-off resolves; ${player.username} must discard one card, and will draw up to three.`,
               ],
             };
             break;
