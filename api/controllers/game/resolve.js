d27665bd6359a05bd9858bf13326912df242dd75
Henry Aviles <haviles04@gmail.com>
2024-01-13 09:22:30 -0500

    feat: updated resolve endpoint

--- api/controllers/game/resolve.js
+++ api/controllers/game/resolve.js
@@ -116,76 +116,14 @@ module.exports = function (req, res) {
             };
             break;
           case 5: {
-            //Draw top card
-            const handLen = player.hand.length;
-            const cardsToDraw = [game.topCard.id];
-            gameUpdates.topCard = null;
-            let cardsToRemoveFromDeck = [];
-            if (handLen < 7) {
-              //Draw second card, if it exists
-              if (game.secondCard) {
-                gameUpdates.log = [
-                  ...game.log,
-                  `The ${getCardName(game.oneOff)} one-off resolves; ${player.username} draws two cards.`,
-                ];
-                cardsToDraw.push(game.secondCard.id);
-                gameUpdates.secondCard = null;
-                cardsToRemoveFromDeck = [...cardsToDraw];
-
-                //Replace top card, if there's a card in deck
-                if (game.deck.length >= 1) {
-                  const newTopCardIndex = _.random(game.deck.length - 1);
-                  const newTopCard = game.deck.splice(newTopCardIndex, 1)[0].id;
-                  gameUpdates.topCard = newTopCard;
-                  cardsToRemoveFromDeck.push(newTopCard);
-
-                  // Replace second card, if possible (after choosing new topcard)
-                  if (game.deck.length >= 1) {
-                    const newSecondCardIndex = _.random(game.deck.length - 1);
-                    const newSecondCard = game.deck.splice(newSecondCardIndex, 1)[0].id;
-                    gameUpdates.secondCard = newSecondCard;
-                    cardsToRemoveFromDeck.push(newSecondCard);
-                  }
-                }
-                // Player drew last card
-              } else {
-                gameUpdates.log = [
-                  ...game.log,
-                  `The ${getCardName(game.oneOff)} one-off resolves; ${player.username} draws the last card.`,
-                ];
-              }
-              //Player could only draw one card, due to hand limit
-            } else {
-              // Replace top card with second card, if second card exists
-              if (game.secondCard) {
-                gameUpdates.topCard = game.secondCard.id;
-                gameUpdates.secondCard = null;
-
-                // If more cards are left in deck, replace second card with card from deck
-                if (game.deck.length >= 1) {
-                  const newSecondCardIndex = _.random(game.deck.length - 1);
-                  const newSecondCard = game.deck.splice(newSecondCardIndex, 1)[0].id;
-                  gameUpdates.secondCard = newSecondCard;
-                  cardsToRemoveFromDeck.push(newSecondCard);
-                  gameUpdates.log = [
-                    ...game.log,
-                    `The ${getCardName(game.oneOff)} one-off resolves; ${player.username} draws one card to reach the hand limit (8).`,
-                  ];
-
-                  // Player draws last card in deck, to reach hand limit (only draws 1)
-                }
-              } else {
-                gameUpdates.log = [
-                  ...game.log,
-                  `The ${getCardName(game.oneOff)} one-off resolves; ${player.username} draws one card (last in deck) to reach the hand limit (8).`,
-                ];
-              }
-            }
-            updatePromises = [
-              ...updatePromises,
-              Game.removeFromCollection(game.id, 'deck').members(cardsToRemoveFromDeck),
-              User.addToCollection(player.id, 'hand').members(cardsToDraw),
-            ];
+            gameUpdates = {
+              ...gameUpdates,
+              resolving: game.oneOff.id,
+              log: [
+                ...game.log,
+                `The ${getCardName(game.oneOff)} one-off resolves; ${opponent.username} must discard one card, and will draw up to three cards.`,
+              ],
+            };
             break; //End resolve FIVE
           }
           case 6: {
