54db09ae71735a78b4a8dd589b726a7036a838e7
Henry Aviles <haviles04@gmail.com>
2024-01-25 16:40:50 -0500

    feat: added new component to lobby view

---
+++ src/routes/lobby/components/LobbyErrorDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <BaseDialog
+    id="ErrorDialog"
+    :model-value="showLobbyError"
+    title="Oops!"
+    :opacity="1"
+    data-cy="create-game-dialog"
+  >
+    <template #body>
+      <div>
+        <h4>Cannot Join Lobby {{ errorMessage }}</h4>
+      </div>
+    </template>
+    <template #actions>
+      <v-btn
+        data-cy="how-it-works-okay"
+        color="surface-1"
+        variant="flat"
+        class="text-surface-2"
+        @click="close"
+      >
+        Go Home
+      </v-btn>
+    </template>
+  </BaseDialog>
+</template>
+
+<script setup>
+import BaseDialog from '@/components/BaseDialog.vue';
+import { useRouter, useRoute } from 'vue-router';
+const { showLobbyError } = defineProps({ showLobbyError: {type: Boolean, default: false } });
+const router = useRouter();
+const route = useRoute();
+
+const errorMessage = route.meta.error ? route.meta.error : 'Unknown Error';
+
+const close = () => {
+ router.push('/');
+};
+</script>
