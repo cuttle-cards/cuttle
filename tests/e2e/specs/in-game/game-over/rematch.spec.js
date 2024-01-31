04d1d95b4dfbac99d61c6a33ad7568e507049101
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-01-30 22:06:19 -0500

    test(rematch): update ranked match test to assert match data & remove previous test that's replaced with the new one

--- tests/e2e/specs/in-game/game-over/rematch.spec.js
+++ tests/e2e/specs/in-game/game-over/rematch.spec.js
@@ -89,7 +89,7 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
     cy.setupGameAsP0(true, true);
   });
 
-  it('Wins match played with Rematch/Continue Match button', () => {
+  it('Wins match played with Rematch/Continue Match button', function() {
     // Game 1: Opponent concedes
     cy.log('Game 1: opponent concedes');
     cy.concedeOpponent();
@@ -122,6 +122,17 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
       cy.joinRematchOpponent({ oldGameId });
     });
 
+    // Match should be incomplete with one game
+    cy.request('http://localhost:1337/match').then((res) => {
+      expect(res.body.length).to.eq(3);
+      const [, , currentMatch] = res.body;
+      expect(currentMatch.player1.id).to.eq(this.playerOneId);
+      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
+      expect(currentMatch.startTime).to.be.greaterThan(0);
+      expect(currentMatch.endTime).to.eq(null);
+      expect(currentMatch.games.length).to.eq(1);
+    });
+
     // Game 2: Player concedes
     cy.log('Game 2: Player concedes');
     cy.get('[data-player-hand-card]')
@@ -129,6 +140,19 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
 
     concedePlayer();
     assertLoss({wins: 1, losses: 1, stalemates: 0});
+    
+    // Match should be incomplete with two games
+    cy.request('http://localhost:1337/match').then((res) => {
+      expect(res.body.length).to.eq(3);
+      const [, , currentMatch] = res.body;
+      expect(currentMatch.player1.id).to.eq(this.playerOneId);
+      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
+      expect(currentMatch.games.length).to.eq(2);
+      expect(currentMatch.startTime).to.be.greaterThan(0);
+      // Match is incomplete
+      expect(currentMatch.endTime).to.eq(null);
+      expect(currentMatch.winner).to.eq(null);
+    });
 
     // Game 3 - Stalemate
     cy.log('Game 3: stalemate via player request');
@@ -136,35 +160,61 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
     cy.get('[data-player-hand-card]')
       .should('have.length', 5);
 
-      // Opponent requests stalemate
-      cy.stalemateOpponent();
+    // Opponent requests stalemate
+    cy.stalemateOpponent();
 
-      // Player accepts stalemate
-      cy.get('#opponent-requested-stalemate-dialog')
-        .should('be.visible')
-        .find('[data-cy=accept-stalemate]')
-        .click();
+    // Player accepts stalemate
+    cy.get('#opponent-requested-stalemate-dialog')
+      .should('be.visible')
+      .find('[data-cy=accept-stalemate]')
+      .click();
 
-      assertStalemate({wins: 1, losses: 1, stalemates: 1});
+    assertStalemate({wins: 1, losses: 1, stalemates: 1});
 
-      // Game 4 - Player wins match
-      startRematchPlayerFirst();
-      cy.get('[data-player-hand-card]').should('have.length', 6);
+    // Match should be incomplete with three games
+    cy.request('http://localhost:1337/match').then((res) => {
+      expect(res.body.length).to.eq(3);
+      const [, , currentMatch] = res.body;
+      expect(currentMatch.player1.id).to.eq(this.playerOneId);
+      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
+      expect(currentMatch.games.length).to.eq(3);
+      expect(currentMatch.startTime).to.be.greaterThan(0);
+      // Match is incomplete
+      expect(currentMatch.endTime).to.eq(null);
+      expect(currentMatch.winner).to.eq(null);
+    });
 
-      cy.concedeOpponent();
-      assertVictory({wins: 2, losses: 1, stalemates: 1});
+    // Game 4 - Player wins match
+    startRematchPlayerFirst();
+    cy.get('[data-player-hand-card]').should('have.length', 6);
 
-      // Rematch button and indicators should not display (match is over)
-      cy.get('[data-cy=gameover-rematch]')
-        .should('not.exist');
-      cy.get('[data-cy=my-rematch-indicator]').should('not.exist');
-      cy.get('[data-cy=opponent-rematch-indicator]').should('not.exist');
+    cy.concedeOpponent();
+    assertVictory({wins: 2, losses: 1, stalemates: 1});
 
-      // Player1 won and Player2 lost
-      cy.get('[data-cy=player-match-result]')
-        .find('[data-cy-result-img=won]');
-      cy.get('[data-cy=opponent-match-result]')
-        .find('[data-cy-result-img=lost]');
+    // Rematch button and indicators should not display (match is over)
+    cy.get('[data-cy=gameover-rematch]')
+      .should('not.exist');
+    cy.get('[data-cy=my-rematch-indicator]').should('not.exist');
+    cy.get('[data-cy=opponent-rematch-indicator]').should('not.exist');
+
+    // Player1 won and Player2 lost
+    cy.get('[data-cy=player-match-result]')
+      .find('[data-cy-result-img=won]');
+    cy.get('[data-cy=opponent-match-result]')
+      .find('[data-cy-result-img=lost]');
+
+    // Match should be completed with 4 games
+    cy.request('http://localhost:1337/match').then((res) => {
+      expect(res.body.length).to.eq(3);
+      const [, , currentMatch] = res.body;
+      expect(currentMatch.player1.id).to.eq(this.playerOneId);
+      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
+      expect(currentMatch.games.length).to.eq(4);
+      expect(currentMatch.startTime).to.be.greaterThan(0);
+      // Match is over - Player1 wins
+      expect(currentMatch.endTime).to.be.greaterThan(0);
+      expect(currentMatch.winner.id).to.eq(this.playerOneId);
+    });
   });
 
   it('Loses a ranked match played with the Rematch/Continue Match button', () => {
@@ -216,200 +266,6 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
 
     cy.get('[data-cy=gameover-rematch]')
       .should('be.disabled');
-  });
-  
-  it.only('Creates a match when two players play a ranked game for the first time this week, finish the match with rematch', function () {
-    // There should be two matches initially (one from last week and one with a different opponent)
-    cy.request('http://localhost:1337/match').then((res) => {
-      expect(res.body.length).to.eq(2);
-    });
-
-    // 1st game: Opponent concedes
-    cy.concedeOpponent();
-    assertVictory();
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        const winners = game.currentMatch.games.map((g) => g.winner);
-        cy.expect(winners[0]).to.eq(game.players[0].id);
-      });
-    cy.get('[data-cy=gameover-rematch]').click();
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        const { p0Rematch, p1Rematch } = game;
-
-        cy.expect(p0Rematch).to.eq(true);
-        cy.expect(p1Rematch).to.eq(null);
-      });
-    cy.get('#waiting-for-game-to-start-scrim').should('be.visible');
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        cy.rematchAndJoinRematchOpponent({ gameId: game.id });
-      });
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        const { p0Rematch, p1Rematch } = game;
-
-        cy.expect(p0Rematch).to.eq(true);
-        cy.expect(p1Rematch).to.eq(null);
-        cy.log('game data is correct after first game');
-      });
-
-    // There should now be one match for the two players
-    cy.request('http://localhost:1337/match').then((res) => {
-      expect(res.body.length).to.eq(3);
-      const [, , currentMatch] = res.body;
-      expect(currentMatch.player1.id).to.eq(this.playerOneId);
-      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
-      expect(currentMatch.startTime).to.be.greaterThan(0);
-      expect(currentMatch.endTime).to.eq(null);
-      expect(currentMatch.games.length).to.eq(1);
-    });
-
-    cy.log('Starting second game');
-    cy.url().should('include', '/game');
-
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        const { p0Rematch } = game;
-        const { p1Rematch } = game;
-
-        cy.expect(p0Rematch).to.eq(null);
-        cy.expect(p1Rematch).to.eq(null);
-      });
-
-    // 2nd game: Player concedes
-    cy.get('#game-menu-activator').click();
-    cy.get('#game-menu').should('be.visible').get('[data-cy=concede-initiate]').click();
-    cy.get('#request-gameover-dialog').should('be.visible').get('[data-cy=request-gameover-confirm]').click();
-
-    assertLoss();
-
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        const winners = game.currentMatch.games.map((g) => g.winner);
-        cy.expect(winners[1]).to.eq(game.players[0].id);
-      });
-
-    cy.log('Opponent requests rematch first');
-    cy.url().then((url) => {
-      const oldGameId = url.split('/').pop();
-      cy.rematchOpponent({ gameId: oldGameId, rematch: true });
-      cy.get('#game-over-dialog')
-        .should('be.visible')
-        .should('contain', 'Player2 wants to rematch')
-        .get('[data-cy=gameover-rematch]')
-        .click();
-
-      cy.joinRematchOpponent({ oldGameId });
-    });
-
-    cy.url().should('include', '/game');
-
-    // The match for these two players should now have two games
-    cy.request('http://localhost:1337/match').then((res) => {
-      expect(res.body.length).to.eq(3);
-      const [, , currentMatch] = res.body;
-      expect(currentMatch.player1.id).to.eq(this.playerOneId);
-      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
-      expect(currentMatch.games.length).to.eq(2);
-      expect(currentMatch.startTime).to.be.greaterThan(0);
-      // Match is incomplete
-      expect(currentMatch.endTime).to.eq(null);
-      expect(currentMatch.winner).to.eq(null);
-    });
-
-    // 3rd game: Ends via requested stalemate
-    // Request stalemate
-    cy.get('#game-menu-activator').click();
-    cy.get('#game-menu').should('be.visible').get('[data-cy=stalemate-initiate]').click();
-    cy.get('#request-gameover-dialog').should('be.visible').get('[data-cy=request-gameover-confirm]').click();
-    cy.get('#waiting-for-opponent-stalemate-scrim').should('be.visible');
-    // Opponent confirms
-    cy.stalemateOpponent();
-    assertStalemate();
-    
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        const winners = game.currentMatch.games.map((g) => g.winner);
-        cy.expect(winners[2]).to.eq(null);
-      });
-
-    // Play again
-    cy.get('#game-over-dialog').should('be.visible').get('[data-cy=gameover-rematch]').click();
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        cy.rematchAndJoinRematchOpponent({ gameId: game.id });
-      });
-    cy.url().should('include', '/game');
-
-    // Validate match data
-    cy.request('http://localhost:1337/match').then((res) => {
-      expect(res.body.length).to.eq(3);
-      const [, , currentMatch] = res.body;
-      expect(currentMatch.player1.id).to.eq(this.playerOneId);
-      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
-      expect(currentMatch.startTime).to.be.greaterThan(0);
-      expect(currentMatch.games.length).to.eq(3);
-      // Match is incomplete
-      expect(currentMatch.endTime).to.eq(null);
-      expect(currentMatch.winner).to.eq(null);
-      cy.log('Match data is correct after third game', res);
-    });
-
-    // 4th game
-    cy.get('#game-menu-activator').click();
-    cy.get('#game-menu').should('be.visible').get('[data-cy=concede-initiate]').click();
-    cy.get('#request-gameover-dialog').should('be.visible').get('[data-cy=request-gameover-confirm]').click();
-    assertLoss();
-
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        const winners = game.currentMatch.games.map((g) => g.winner);
-        cy.expect(winners[3]).to.eq(game.players[0].id);
-      });
-
-    cy.get('#game-over-dialog').should('be.visible').get('[data-cy=gameover-rematch]').click();
-    cy.window()
-      .its('cuttle.gameStore')
-      .then((game) => {
-        cy.rematchAndJoinRematchOpponent({ gameId: game.id });
-      });
-    cy.url().should('include', '/game');
-
-    // Game now should be unranked
-    // Validate match data
-    cy.request('http://localhost:1337/match').then((res) => {
-      expect(res.body.length).to.eq(3);
-      const [oldMatch, , currentMatch] = res.body;
-
-      // Expect old match to have no games associated with it
-      expect(oldMatch.games.length).to.eq(0);
-      expect(currentMatch.player1.id).to.eq(this.playerOneId);
-      expect(currentMatch.player2.id).to.eq(this.playerTwoId);
-      expect(currentMatch.startTime).to.be.greaterThan(0);
-      expect(currentMatch.games.length).to.eq(4);
-      // Match is complete
-      expect(currentMatch.winner.id).to.eq(this.playerTwoId);
-      expect(currentMatch.endTime).to.be.greaterThan(0);
-      cy.log('Match data is correctly unaffected after sixth game', res);
-
-      // Confirm game was set to unranked
-      cy.request('http://localhost:1337/game').then((res) => {
-        // Sort games by updatedAt asc
-        const games = res.body.sort((game1, game2) => game1.updatedAt - game2.updatedAt);
-        expect(games.length).to.eq(5, 'Expected 5 games');
-        expect(games[4].isRanked).to.eq(false, 'Expected last game to be set to unranked after completion');
-      });
-    });
   });
 });
 
