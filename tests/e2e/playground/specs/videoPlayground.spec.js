659296ed86f35368d68cf3b228b3b2a0e6c0da17
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-02-02 08:55:43 -0500

    test(videoPlayground): implement playing points 'test' for video recording

--- tests/e2e/playground/specs/videoPlayground.spec.js
+++ tests/e2e/playground/specs/videoPlayground.spec.js
@@ -1,3 +1,4 @@
+import { Card } from '../../fixtures/cards';
 
 describe('Video Playground', () => {
   beforeEach(() => {
@@ -6,6 +7,26 @@ describe('Video Playground', () => {
   });
 
   it('Plays Points', () => {
+    cy.loadGameFixture(0, {
+      p0Hand: [Card.ACE_OF_SPADES, Card.SIX_OF_SPADES, Card.EIGHT_OF_HEARTS, Card.SEVEN_OF_DIAMONDS],
+      p0Points: [Card.TEN_OF_SPADES],
+      p0FaceCards: [],
+      p1Hand: [
+        Card.THREE_OF_CLUBS,
+        Card.FIVE_OF_HEARTS,
+        Card.EIGHT_OF_CLUBS,
+        Card.NINE_OF_DIAMONDS,
+        Card.NINE_OF_HEARTS
+      ],
+      p1Points: [Card.FIVE_OF_HEARTS],
+      p1FaceCards: [],
+    });
 
+    cy.wait(1000);
+
+    cy.get('[data-player-hand-card=8-2]').click();
+    cy.wait(800);
+
+    cy.get('[data-move-choice=points]').click();
   });
 });
