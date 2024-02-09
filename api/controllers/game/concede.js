9809248e21d5a25435abda2da17a509174ce3a06
Henry Aviles <haviles04@gmail.com>
2024-02-09 17:17:35 -0500

    feat: updated api endpoint to store game and victory in lastEvent

--- api/controllers/game/concede.js
+++ api/controllers/game/concede.js
@@ -10,6 +10,7 @@ module.exports = async function (req, res) {
       status: gameService.GameStatus.FINISHED,
       winner: winningUser,
     });
+
     const game = await gameService.populateGame({ gameId: req.session.game });
     await gameService.clearGame({ userId: req.session.usr });
     const currentMatch = await sails.helpers.addGameToMatch(game);
@@ -20,6 +21,15 @@ module.exports = async function (req, res) {
       conceded: true,
       currentMatch,
     };
+
+    await Game.updateOne(req.session.game).set({
+      lastEvent: {
+        change: 'conceded',
+        game,
+        victory
+      }
+    });
+
     Game.publish([game.id], {
       change: 'concede',
       game,
