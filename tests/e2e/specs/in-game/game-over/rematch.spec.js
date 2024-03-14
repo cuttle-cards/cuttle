c69877428ba062139f35b10f3b451402d75a6e7f
Sean Kennedy <skennedy@urbn.com>
2024-03-13 20:48:52 -0400

    Revert "Add debugger for test as example"
    This reverts commit aa8015895db86b8fe6e5714775ea2221e56a9e62.

--- tests/e2e/specs/in-game/game-over/rematch.spec.js
+++ tests/e2e/specs/in-game/game-over/rematch.spec.js
@@ -89,7 +89,7 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
     cy.setupGameAsP0(true, true);
   });
 
-  it.only('Wins match played with Rematch/Continue Match button', function() {
+  it('Wins match played with Rematch/Continue Match button', function() {
     // Game 1: Opponent concedes
     cy.log('Game 1: opponent concedes');
     cy.concedeOpponent();
@@ -121,7 +121,6 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
 
     // Match should be incomplete with one game
     cy.request('http://localhost:1337/match').then((res) => {
-      debugger;
       expect(res.body.length).to.eq(3);
       const [, , currentMatch] = res.body;
       expect(currentMatch.player1.id).to.eq(this.playerOneId);
@@ -138,7 +137,7 @@ describe('Creating And Updating Ranked Matches With Rematch', () => {
 
     concedePlayer();
     assertLoss({wins: 1, losses: 1, stalemates: 0});
-
+    
     // Match should be incomplete with two games
     cy.request('http://localhost:1337/match').then((res) => {
       expect(res.body.length).to.eq(3);
@@ -271,7 +270,7 @@ describe('Creating And Updating Casual Games With Rematch', () => {
     cy.loginPlayer(playerOne);
     cy.setupGameAsP0(true, false);
   });
-
+  
   it('Unranked games with rematch', function () {
     // Game 1: Opponent concedes
     cy.concedeOpponent();
@@ -354,7 +353,7 @@ describe('Spectating Rematches', () => {
       cy.viewport(1920, 1080);
       cy.setupGameAsSpectator();
     });
-
+    
     it('Spectates a casual match using rematch', () => {
       cy.log('Game 1: player1 wins via opponent conceding');
       cy.recoverSessionOpponent(playerTwo);
@@ -372,7 +371,7 @@ describe('Spectating Rematches', () => {
         .should('be.visible')
         .find('[data-cy=gameover-rematch]')
         .click();
-
+        
       // Game 2
       cy.log('Game 2: player1 wins via concede again');
       cy.get('[data-cy=player-username]')
