42fb70a1f8eeeba91d46fa1396beb2f1542cb70d
Sean Kennedy <skennedy@urbn.com>
2024-03-18 18:09:37 -0400

    Add hash router redirect test

---
+++ tests/e2e/specs/out-of-game/other.spec.js
@@ -0,0 +1,21 @@
+const { version: pkgVersion } = require('../../../../package.json');
+
+describe('Health Page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('is available if db responds', () => {
+    cy.request('/health').then((res) => {
+      expect(res).property('status').to.equal(200);
+      expect(res.body).property('alive').to.eq(true);
+    });
+  });
+
+  it('returns the current package version', () => {
+    cy.request('/health').then((res) => {
+      expect(res).property('status').to.equal(200);
+      expect(res.body).property('version').to.eq(pkgVersion);
+    });
+  });
+});
