74c74c13180a4b88bfc9a67f8c7d9e665447b060
Henry Aviles <haviles04@gmail.com>
2024-04-06 19:14:23 -0400

    chore: spacing

--- src/components/CTABanner.vue
+++ src/components/CTABanner.vue
@@ -83,7 +83,7 @@
 </template>
 
 <script setup>
-  import BaseDialog from '@/components/BaseDialog.vue';
+import BaseDialog from '@/components/BaseDialog.vue';
 import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import GameCard from '@/routes/game/components/GameCard.vue';
@@ -91,7 +91,6 @@ import { getLocalStorage, setLocalStorage} from '_/utils/local-storage-utils.js'
 
 const { t } = useI18n();
 const fiveCards = [{suit: 0, rank:5},{suit: 1, rank:5},{suit: 2, rank:5},{suit: 3, rank:5},];
-
 const show = ref(false);
 const preferenceSaved= ref(false);
 
@@ -102,21 +101,18 @@ const close = () => {
   show.value = false;
 };
 
-
 onMounted(() => {
   if (!getLocalStorage('bannerDismissed')) {
     show.value = true;
     preferenceSaved.value = true;
   }
 });
-
 </script>
 
 <style scoped>
   .card:nth-child(n+2){
     margin-left: -9% !important;
   }
-
 
   p:nth-child(n+3){
     margin-bottom: 3%;
