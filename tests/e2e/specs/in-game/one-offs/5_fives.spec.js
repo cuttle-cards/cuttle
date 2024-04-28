db69660e548f1392edb1aa52e8e940c695c7f708
Henry Aviles <haviles04@gmail.com>
2024-04-28 16:00:03 -0400

    test: added edge case testing

--- tests/e2e/specs/in-game/one-offs/5_fives.spec.js
+++ tests/e2e/specs/in-game/one-offs/5_fives.spec.js
@@ -2,7 +2,6 @@ import { assertGameState, playOutOfTurn } from '../../../support/helpers';
 import { Card } from '../../../fixtures/cards';
 
 describe('FIVES', () => {
-  
   describe('Playing FIVES', () => {
     beforeEach(() => {
       cy.setupGameAsP0();
@@ -58,7 +57,7 @@ describe('FIVES', () => {
       cy.playOneOffAndResolveAsPlayer(Card.FIVE_OF_CLUBS);
       cy.get('[data-cy=five-discard-dialog]').should('be.visible');
       cy.get('[data-cy=submit-five-dialog]').click();
-      
+
       assertGameState(0, {
         p0Hand: [Card.THREE_OF_CLUBS, Card.EIGHT_OF_HEARTS, Card.SEVEN_OF_CLUBS],
         p0Points: [],
@@ -69,7 +68,7 @@ describe('FIVES', () => {
         scrap: [Card.FIVE_OF_CLUBS],
         topCard: null,
         secondCard: null,
-        deck: []
+        deck: [],
       });
       // Deck should now be empty
       cy.get('#deck').should('contain', '(0)').should('contain', 'PASS');
@@ -97,7 +96,7 @@ describe('FIVES', () => {
       cy.get('[data-cy=submit-five-dialog]').click();
 
       assertGameState(0, {
-        p0Hand: [Card.THREE_OF_CLUBS,Card.EIGHT_OF_HEARTS],
+        p0Hand: [Card.THREE_OF_CLUBS, Card.EIGHT_OF_HEARTS],
         p0Points: [],
         p0FaceCards: [],
         p1Hand: [],
@@ -106,19 +105,26 @@ describe('FIVES', () => {
         scrap: [Card.FIVE_OF_CLUBS],
         topCard: null,
         secondCard: null,
-        deck: []
+        deck: [],
       });
 
       cy.get('#deck').should('contain', '(0)').should('contain', 'PASS');
       cy.get('[data-player-hand-card]').should('have.length', 2);
-
     });
 
     it('Plays a 5 to draw two cards when already at hand limit (8)', () => {
       // Setup: there are three cards in the deck and player has a 5
       cy.loadGameFixture(0, {
-        p0Hand: [Card.FIVE_OF_CLUBS, Card.FIVE_OF_SPADES, Card.ACE_OF_DIAMONDS,
-        Card.EIGHT_OF_CLUBS, Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS, Card.THREE_OF_HEARTS, Card.FOUR_OF_HEARTS],
+        p0Hand: [
+          Card.FIVE_OF_CLUBS,
+          Card.FIVE_OF_SPADES,
+          Card.ACE_OF_DIAMONDS,
+          Card.EIGHT_OF_CLUBS,
+          Card.TEN_OF_CLUBS,
+          Card.TWO_OF_CLUBS,
+          Card.THREE_OF_HEARTS,
+          Card.FOUR_OF_HEARTS,
+        ],
         p0Points: [],
         p0FaceCards: [],
         p1Hand: [Card.TWO_OF_HEARTS],
@@ -135,10 +141,18 @@ describe('FIVES', () => {
       cy.get('[data-cy=five-discard-dialog]').should('be.visible');
       cy.get('[data-discard-card=5-3]').click();
       cy.get('[data-cy=submit-five-dialog]').click();
-      
+
       assertGameState(0, {
-        p0Hand: [Card.THREE_OF_CLUBS, Card.EIGHT_OF_HEARTS, Card.ACE_OF_DIAMONDS,
-        Card.EIGHT_OF_CLUBS, Card.TEN_OF_CLUBS, Card.TWO_OF_CLUBS, Card.THREE_OF_HEARTS, Card.FOUR_OF_HEARTS],
+        p0Hand: [
+          Card.THREE_OF_CLUBS,
+          Card.EIGHT_OF_HEARTS,
+          Card.ACE_OF_DIAMONDS,
+          Card.EIGHT_OF_CLUBS,
+          Card.TEN_OF_CLUBS,
+          Card.TWO_OF_CLUBS,
+          Card.THREE_OF_HEARTS,
+          Card.FOUR_OF_HEARTS,
+        ],
         p0Points: [],
         p0FaceCards: [],
         p1Hand: [Card.TWO_OF_HEARTS],
@@ -147,7 +161,7 @@ describe('FIVES', () => {
         scrap: [Card.FIVE_OF_CLUBS, Card.FIVE_OF_SPADES],
         topCard: Card.ACE_OF_DIAMONDS,
         secondCard: null,
-        deck: []
+        deck: [],
       });
       cy.get('#deck').should('contain', '(1)');
     });
@@ -165,8 +179,7 @@ describe('FIVES', () => {
         // Deck
         topCard: Card.THREE_OF_CLUBS,
         secondCard: Card.EIGHT_OF_HEARTS,
-        deck:[]
-
+        deck: [],
       });
       //player plays a card
       cy.get('[data-player-hand-card=2-0]').click();
@@ -189,7 +202,7 @@ describe('FIVES', () => {
         scrap: [Card.FIVE_OF_SPADES, Card.ACE_OF_CLUBS],
         topCard: null,
         secondCard: null,
-        deck: []
+        deck: [],
       });
     });
 
@@ -207,7 +220,7 @@ describe('FIVES', () => {
 
       cy.playOneOffAndResolveAsPlayer(Card.FIVE_OF_SPADES);
       cy.get('[data-cy=five-discard-dialog]').should('be.visible');
-      cy.get('[data-cy=submit-five-dialog]').should('be.disabled').click({force: true});
+      cy.get('[data-cy=submit-five-dialog]').should('be.disabled').click({ force: true });
       cy.get('[data-discard-card=2-0]').click();
       cy.get('[data-cy=submit-five-dialog]').click();
 
@@ -217,11 +230,103 @@ describe('FIVES', () => {
       cy.get('[data-opponent-point-card=4-0]').should('be.visible');
 
       cy.playOneOffAndResolveAsPlayer(Card.FIVE_OF_HEARTS);
-      cy.get('[data-cy=submit-five-dialog]').should('be.disabled').click({force: true});
+      cy.get('[data-cy=submit-five-dialog]').should('be.disabled').click({ force: true });
 
       cy.get('[data-discard-card=1-0]').click();
       cy.get('[data-cy=submit-five-dialog]').click();
       cy.get('[data-player-hand-card]').should('have.length', 6);
+    });
+
+    it('Plays a 5 with card to discard and 2 cards left in deck', () => {
+      cy.loadGameFixture(0, {
+        // Player is P0
+        p0Hand: [Card.FIVE_OF_SPADES, Card.TWO_OF_CLUBS],
+        p0Points: [],
+        p0FaceCards: [],
+        // Opponent is P1
+        p1Hand: [],
+        p1Points: [],
+        p1FaceCards: [],
+        topCard: Card.FOUR_OF_CLUBS,
+        secondCard: Card.ACE_OF_CLUBS,
+        deck: [],
+      });
+      cy.playOneOffAndResolveAsPlayer(Card.FIVE_OF_SPADES);
+      cy.get('[data-discard-card=2-0]').click();
+      cy.get('[data-cy=submit-five-dialog]').click();
+
+      assertGameState(0, {
+        p0Hand: [Card.FOUR_OF_CLUBS, Card.ACE_OF_CLUBS],
+        p0Points: [],
+        p0FaceCards: [],
+        p1Hand: [],
+        p1Points: [],
+        p1FaceCards: [],
+        scrap: [Card.FIVE_OF_SPADES, Card.TWO_OF_CLUBS],
+        topCard: null,
+        secondCard: null,
+        deck: [],
+      });
+
+      cy.get('#deck').should('contain', 'PASS');
+    });
+
+    it('Plays a 5 with card to discard and 1 card left in deck', () => {
+      cy.loadGameFixture(0, {
+        // Player is P0
+        p0Hand: [Card.FIVE_OF_SPADES, Card.TWO_OF_CLUBS],
+        p0Points: [],
+        p0FaceCards: [],
+        // Opponent is P1
+        p1Hand: [],
+        p1Points: [],
+        p1FaceCards: [],
+        topCard: Card.FOUR_OF_CLUBS,
+        secondCard: Card.ACE_OF_HEARTS,
+        deck: [Card.TWO_OF_HEARTS],
+      });
+      cy.get('#deck').click();
+      cy.drawCardOpponent();
+      cy.get('#deck').should('contain', '(1)');
+      cy.playOneOffAndResolveAsPlayer(Card.FIVE_OF_SPADES);
+      cy.get('[data-discard-card=2-0]').click();
+      cy.get('[data-cy=submit-five-dialog]').click();
+
+      assertGameState(0, {
+        p0Hand: [Card.FOUR_OF_CLUBS, Card.TWO_OF_HEARTS],
+        p0Points: [],
+        p0FaceCards: [],
+        p1Hand: [Card.ACE_OF_HEARTS],
+        p1Points: [],
+        p1FaceCards: [],
+        scrap: [Card.FIVE_OF_SPADES, Card.TWO_OF_CLUBS],
+        topCard: null,
+        secondCard: null,
+        deck: [],
+      });
+      cy.get('#deck').should('contain', 'PASS');
+    });
+
+    it('Attempts to play 5 with an empty deck', () => {
+      cy.loadGameFixture(0, {
+        // Player is P0
+        p0Hand: [Card.FIVE_OF_SPADES, Card.TWO_OF_CLUBS],
+        p0Points: [],
+        p0FaceCards: [],
+        // Opponent is P1
+        p1Hand: [],
+        p1Points: [],
+        p1FaceCards: [],
+        topCard: Card.FOUR_OF_CLUBS,
+        secondCard: Card.ACE_OF_HEARTS,
+        deck: [],
+      });
+      cy.get('#deck').click();
+      cy.drawCardOpponent();
+
+      cy.get('#deck').should('contain', 'PASS');
+      cy.get('[data-player-hand-card=5-3]').click();
+      cy.get('[data-move-choice=oneOff]').should('have.class', 'v-card--disabled');
     });
   });
 
@@ -243,7 +348,7 @@ describe('FIVES', () => {
         // Deck
         topCard: Card.THREE_OF_CLUBS,
         secondCard: Card.EIGHT_OF_HEARTS,
-        deck:[Card.SEVEN_OF_DIAMONDS]
+        deck: [Card.SEVEN_OF_DIAMONDS],
       });
       cy.playOneOffOpponent(Card.FIVE_OF_SPADES);
 
@@ -255,7 +360,13 @@ describe('FIVES', () => {
       cy.get('#waiting-for-opponent-discard-scrim').should('not.exist');
 
       assertGameState(1, {
-        p0Hand: [Card.ACE_OF_CLUBS, Card.TWO_OF_CLUBS,Card.THREE_OF_CLUBS,Card.EIGHT_OF_HEARTS, Card.SEVEN_OF_DIAMONDS],
+        p0Hand: [
+          Card.ACE_OF_CLUBS,
+          Card.TWO_OF_CLUBS,
+          Card.THREE_OF_CLUBS,
+          Card.EIGHT_OF_HEARTS,
+          Card.SEVEN_OF_DIAMONDS,
+        ],
         p0Points: [],
         p0FaceCards: [],
         p1Hand: [],
@@ -264,9 +375,8 @@ describe('FIVES', () => {
         scrap: [Card.FIVE_OF_SPADES, Card.FIVE_OF_HEARTS],
         topCard: null,
         secondCard: null,
-        deck: []
+        deck: [],
       });
-
     });
   });
 });
