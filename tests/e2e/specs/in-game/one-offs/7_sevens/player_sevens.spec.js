10cc164012ad3c255d88001ec5da8f8a8e91d857
Henry Aviles <haviles04@gmail.com>
2024-05-16 12:12:49 -0400

    test: moved location

--- tests/e2e/specs/in-game/one-offs/7_sevens/player_sevens.spec.js
+++ tests/e2e/specs/in-game/one-offs/7_sevens/player_sevens.spec.js
@@ -280,6 +280,32 @@ describe('Playing SEVENS', () => {
     });
   });
 
+  it('Resets state after conceding during 7 resolve', function () {
+    cy.loadGameFixture(0, {
+      p0Hand: [Card.SEVEN_OF_CLUBS],
+      p0Points: [],
+      p0FaceCards: [],
+      p1Hand: [],
+      p1Points: [Card.NINE_OF_CLUBS],
+      p1FaceCards: [],
+      topCard: Card.NINE_OF_DIAMONDS,
+      secondCard: Card.SIX_OF_DIAMONDS,
+    });
+
+    cy.playOneOffAndResolveAsPlayer(Card.SEVEN_OF_CLUBS);
+    cy.get('#waiting-for-opponent-counter-scrim').should('not.exist');
+    cy.get('[data-second-card=6-1]').should('exist').and('be.visible');
+    cy.get('#game-menu-activator').click();
+    cy.get('[data-cy=concede-initiate]').click();
+    cy.get('[data-cy=request-gameover-confirm]').click();
+    cy.get('[data-cy=gameover-rematch]').click();
+    cy.url().then((url) => {
+      const oldGameId = Number(url.split('/').pop());
+      cy.rematchAndJoinRematchOpponent({ gameId: oldGameId });
+    });
+    cy.get('#deck').should('not.have.class', 'reveal-top-two');
+  });
+
   describe('Plays face cards from a seven', () => {
     it('Plays king from a seven', () => {
       cy.loadGameFixture(0, {
@@ -439,32 +465,6 @@ describe('Playing SEVENS', () => {
       topCard: Card.SIX_OF_DIAMONDS,
     });
   }); // End scuttle from seven
-
-  it('Resets state after conceding during 7 resolve', function () {
-    cy.loadGameFixture(0, {
-      p0Hand: [Card.SEVEN_OF_CLUBS],
-      p0Points: [],
-      p0FaceCards: [],
-      p1Hand: [],
-      p1Points: [Card.NINE_OF_CLUBS],
-      p1FaceCards: [],
-      topCard: Card.NINE_OF_DIAMONDS,
-      secondCard: Card.SIX_OF_DIAMONDS,
-    });
-
-    cy.playOneOffAndResolveAsPlayer(Card.SEVEN_OF_CLUBS);
-    cy.get('#waiting-for-opponent-counter-scrim').should('not.exist');
-    cy.get('[data-second-card=6-1]').should('exist').and('be.visible');
-    cy.get('#game-menu-activator').click();
-    cy.get('[data-cy=concede-initiate]').click();
-    cy.get('[data-cy=request-gameover-confirm]').click();
-    cy.get('[data-cy=gameover-rematch]').click();
-    cy.url().then((url) => {
-      const oldGameId = Number(url.split('/').pop());
-      cy.rematchAndJoinRematchOpponent({ gameId: oldGameId });
-    });
-    cy.get('#deck').should('not.have.class', 'reveal-top-two');
-  });
 
   describe('Playing untargeted one-offs from a seven', () => {
     it('Plays an ACE from a seven', () => {
