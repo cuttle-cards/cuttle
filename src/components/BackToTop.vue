9e4d021503386fce2201b75bdd8f84cd09d85c8b
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-05-20 09:35:26 -0400

    refactor(BackToTop): pull styles into css from inline classes

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -4,7 +4,7 @@
       v-if="show"
       position="fixed"
       location="bottom right"
-      class="mt-auto mx-4 mb-16 fab"
+      class="fab"
       color="surface-2"
       elevation="8"
       icon="mdi-chevron-up"
@@ -33,6 +33,7 @@ function onClick() {
 
 <style scoped>
 .fab {
+  margin: auto 8px 64px 8px;
   z-index: 1;
 }
 </style>
