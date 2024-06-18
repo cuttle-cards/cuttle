52d2988fb9fc786520fd744eb9380d3535ca257f
Plubedam <pedroluisubedam@hotmail.com>
2024-06-19 01:14:43 +0200

    fix: renames GameState to GameStateRow, Phase to GamePhase, and fix columns of model

--- api/models/GameStateRow.js
+++ api/models/GameStateRow.js
@@ -1,13 +1,18 @@
-/**
- * GameState.js
- * 
- * @description :: A GameState record represents one move made by a player and the resulting game state
- */
+  /**
+   * GameState.js
+   * 
+   * @description :: A GameState record represents one move made by a player and the resulting game state
+   */
 
 module.exports = {
   attributes: {
-     // Which player made the move (0 if p0, 1 if p1)
-     playedBy: {
+    // FK to the games table
+    gameId: {
+      model: 'game',
+      required: true,
+    },
+    // Which player made the move (0 if p0, 1 if p1)
+    playedBy: {
       type: 'number',
       isIn: [0, 1],
       required: true,
@@ -36,24 +41,33 @@ module.exports = {
     */
     moveType: {
       type: 'number',
+      isIn: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
       required: true,
+      columnType: 'int4',
     },
     // The card that was played
     playedCardId: {
-      model: 'card',
+      type: 'string',
+      allowNull: true,
+      columnType: 'text',
     },
     // The card that was targeted
     targetCardId: {
-      model: 'card',
+      type: 'string',
+      allowNull: true,
+      columnType: 'text',
     },
     // The 2nd card that is relevant to the move
     targetCard2Id: {
-      model: 'card',
+      type: 'string',
+      allowNull: true,
+      columnType: 'text',
     },
     // Which turn number the move was made on
     turn: {
       type: 'number',
       required: true,
+      columnType: 'int4',
     },
     /**
      * Enum for phase:
@@ -62,79 +76,84 @@ module.exports = {
      * 3 - RESOLVING_THREE
      * 4 - RESOLVING_FOUR
      * 5 - RESOLVING_FIVE
-     * 6 - RESOLVING_SEVEN
+     * 7 - RESOLVING_SEVEN
     */
     phase: {
       type: 'number',
       required: true,
+      isIn: [1, 2, 3, 4, 5, 7],
     },
     // Cards in p0’s hand
     p0Hand: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Cards in p1’s hand
     p1Hand: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Cards in p0’s points
     p0Points: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Cards in p1’s points
     p1Points: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Cards in p0’s face cards
     p0FaceCards: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Cards in p1’s face cards
     p1FaceCards: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Cards in the deck, in order
     deck: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Cards in the scrap
     scrap: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // One-off card
     oneOff: {
       type: 'string',
+      allowNull: true,
+      defaultsTo: null,
     },
     // One-off target card
     oneOffTarget: {
       type: 'string',
+      allowNull: true,
+      defaultsTo: null,
     },
     // Twos
     twos: {
       type: 'json',
       columnType: 'text[]',
+      required: true,
     },
     // Resolving card
     resolving: {
       type: 'string',
+      allowNull: true,
+      defaultsTo: null,
     },
-    // FK to the games table
-    gameId: {
-      model: 'game',
-      required: true,
-    },
-    // When the move took place
-    createdAt: {
-      type: 'ref',
-      columnType: 'timestamp',
-      autoCreatedAt: true,
-    }
   },
 };
