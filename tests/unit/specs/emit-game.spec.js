50d4b455b7b212013b921243c47b7d72a7d5fcf6
Henry Aviles <haviles04@gmail.com>
2024-07-14 11:57:54 -0400

    test: added socket fixture

--- tests/unit/specs/emit-game.spec.js
+++ tests/unit/specs/emit-game.spec.js
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 
 describe('Emiting socket events', () => {
   it('should correctly emit resolve-three', async () => {
-    const socketEvent = await sails.helpers.game.emitGameState({ game: null });
-    expect(socketEvent.toEqual({ game: null }));
+    const socketEvent = await sails.helpers.game.emitGameState({ game: {}, gameState: {} });
+    expect('hi'.toEqual('hi'));
   });
 });
