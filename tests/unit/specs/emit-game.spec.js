d195906ff8e0a93ef94a76c02d170aa69a01d63f
Henry Aviles <haviles04@gmail.com>
2024-07-20 08:20:56 -0400

    test: updated test function

--- tests/unit/specs/emit-game.spec.js
+++ tests/unit/specs/emit-game.spec.js
@@ -1,8 +1,13 @@
 import { describe, expect, it } from 'vitest';
+import { resolveThreeGame } from '../fixtures/gameData';
+import { resolveThree } from '../fixtures/gameSocketEvent';
 
 describe('Emiting socket events', () => {
   it('should correctly emit resolve-three', async () => {
-    const socketEvent = await sails.helpers.game.emitGameState({ game: {}, gameState: {} });
-    expect('hi'.toEqual('hi'));
+    const socketEvent = await sails.helpers.game.emitGameState(
+      resolveThreeGame.game,
+      resolveThreeGame.gameState,
+    );
+    expect(socketEvent).toEqual(resolveThree.game);
   });
 });
