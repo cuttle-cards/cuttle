56514a944d641175d5540d36fb8a2703db8b3b52
Henry Aviles <haviles04@gmail.com>
2024-07-21 08:01:23 -0400

    feat: added passes

--- api/helpers/game/emit-game-state.js
+++ api/helpers/game/emit-game-state.js
@@ -20,18 +20,17 @@ module.exports = {
 
   fn: async function ({ game, gameState }, exits) {
     //Combine game and gamestate users and delete passwords
-    const p0 = { ...game.p0, ...gameState.p0 };
+    const p0 = { ...game.players[0], ...gameState.p0 };
     delete p0.encryptedPassword;
-    const p1 = { ...game.p1, ...gameState.p1 };
+    const p1 = { ...game.players[1], ...gameState.p1 };
     delete p1.encryptedPassword;
     const players = [p0, p1];
 
     const lastEventChange = Object.keys(GameMoveType).find((key) => GameMoveType[key] === gameState.moveType);
-    const lastEventPlayerChoosing = lastEventChange === 'resolveThree' ? gameState.playedBy : null;
 
     const lastEventTargetType = () => {
       if (!['targetedOneOff', 'sevenTargetedOneOff'].includes(lastEventChange)) {
-        return null;
+        return '';
       }
 
       if (gameState.targetCard?.rank === 11) {
@@ -42,30 +41,73 @@ module.exports = {
       }
       return 'point';
     };
-    //Only define what is changing, rest is defined by spread operator
-    const populatedGame = {
-      ...game,
-      ...gameState,
-      players,
-      deck: gameState.deck.slice(2),
-      topCard: gameState.deck[0],
-      secondCard: gameState.deck[1],
-      currentMatch: game.match,
-      lastEvent: {
-        change: lastEventChange,
-        oneOffRank: gameState.oneOff?.rank,
-        targetType: lastEventTargetType,
-        cardChosen: gameState.chosenCard,
-        playerChoosing: lastEventPlayerChoosing,
-        discardedCards: gameState.discardedCards,
-      },
-    };
 
     const victory = await sails.helpers.game.checkGameStateForWin(game, players);
 
+    const countPasses = () => {
+      if (game.gameStates.length < 3) {
+        return 0;
+      }
+      game.gameStates.slice(game.gameStates.length - 3).reduce((row, acc) => {
+        if (row.moveType !== '18') {
+          return acc;
+        }
+        return acc + 1;
+      }, 0);
+    };
+
+    /* Still needs
+      -waitingForOpponentToStalemate
+      -log
+      -passes
+      -oneOffTarget
+      -attachedtoTargetType
+    
+    */
+    const socketGame = {
+      players, //
+      id: game.id, //
+      createdAt: gameState.createdAt,
+      name: game.name, //
+      chat: game.chat, //
+      status: game.status, //
+      p0: game.p0,
+      p1: game.p1,
+      p0Ready: game.p0Ready, //
+      p1Ready: game.p1Ready, //
+      p0Rematch: game.p0Rematch, //
+      p1Rematch: game.p1Rematch, //
+      lock: game.lock,
+      lockedAt: game.lockedAt,
+      rematchGame: game.rematchGame,
+      spectatingUsers: game.spectatingUsers, //
+      isRanked: game.isRanked, //
+      winner: victory.winner, //
+      match: victory.currentMatch,
+      passes: countPasses(),
+      turn: gameState.turn, //
+      deck: gameState.deck.slice(2), //
+      scrap: gameState.scrap, //
+      topCard: gameState.deck[0], //
+      secondCard: gameState.deck[1], //
+      twos: gameState.twos, //
+      oneOff: gameState.oneOff, //
+      resolving: gameState.resolving,
+      oneOffTarget: gameState.oneOffTarget, //
+      oneOffTargetType: lastEventTargetType(),
+      lastEvent: {
+        change: lastEventChange, //
+        oneOffRank: gameState.oneOff?.rank ?? null, //
+        oneOffTargetType: lastEventTargetType(), //
+        chosenCard: gameState.targetCard ?? null, //
+        pNum: gameState.playedBy, //
+        discardedCards: gameState.discardedCards ?? null, //
+      },
+    };
+
     Game.publish([game.id], {
       change: lastEventChange,
-      game: populatedGame,
+      game: socketGame,
       victory,
     });
 
@@ -73,6 +115,6 @@ module.exports = {
       sails.sockets.blast('gameFinished', { gameId: game.id });
     }
 
-    return exits.success(victory, game);
+    return exits.success({ victory, game: socketGame });
   },
 };
