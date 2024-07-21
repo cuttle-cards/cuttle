9b34311999b0214c1c6dba7a692b85984ae74fae
Henry Aviles <haviles04@gmail.com>
2024-07-21 08:18:31 -0400

    refactor: destructer

--- api/helpers/game/emit-game-state.js
+++ api/helpers/game/emit-game-state.js
@@ -47,8 +47,8 @@ module.exports = {
       if (!game.gameStates.length < 3) {
         return 0;
       }
-      game.gameStates.reduce((row, totalPasses) => {
-        if (row.moveType !== '18') {
+      game.gameStates.reduce(({ moveType }, totalPasses) => {
+        if (moveType !== '18') {
           return totalPasses;
         }
         return totalPasses + 1;
