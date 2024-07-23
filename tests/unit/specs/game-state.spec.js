8406e06e39fc1d67bc42fc827ef5b1b00d5306ed
kawaljen <declicsweb@gmail.com>
2024-07-23 18:55:50 +0200

    lint

--- tests/unit/specs/game-state.spec.js
+++ tests/unit/specs/game-state.spec.js
@@ -1,4 +1,4 @@
-import { describe, it , expect} from 'vitest';
+import { describe, it , expect, beforeEach} from 'vitest';
 import { fixture } from '../fixtures/gameState';
 
 describe('Coverting GameState\'s and GameStateRow\'s', () => {
