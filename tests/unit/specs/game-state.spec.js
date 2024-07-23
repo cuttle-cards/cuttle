f5c715a4ad4634147de8e703a169187d5195aca1
Alexandra P <persiliealexandra@gmail.com>
2024-07-23 16:42:28 +0200

    Update tests/unit/specs/game-state.spec.js
    Co-authored-by: Ryan Emberling <itsalaidbacklife@gmail.com>

--- tests/unit/specs/game-state.spec.js
+++ tests/unit/specs/game-state.spec.js
@@ -3,14 +3,27 @@ import { fixture } from '../fixtures/gameState';
 
 describe('Coverting GameState\'s and GameStateRow\'s', () => {
 
+  beforeEach(async () => {
+    await sails.helpers.wipeDatabase();
+  });
+
   it('Converts GameState to GameStateRow (packing)', async () => {
     const gameStateRow = await sails.helpers.gamestate.saveGamestate(fixture.gameState);
 
     //remove attributes added while creating the entry in the database
-    const attributesToRemove = ['createdAt', 'id', 'updatedAt'];
-    attributesToRemove.forEach(el => delete gameStateRow[el]);
+    const stripDbAttributes = (obj) => {
+      const attributesToRemove = ['createdAt', 'id', 'updatedAt'];
+      attributesToRemove.forEach(attr => delete obj[attr]);      
+    };
 
-    expect(fixture.gameStateRow).toEqual(gameStateRow);
+    stripDbAttributes(gameStateRow);
+    expect(gameStateRow).toEqual(fixture.gameStateRow);
+
+    const dbGameStateRows = GameState.find({});
+    expect(dbGameSTateRows.length).toEqual(1);
+    const [ savedRow ] = dbGameStateRows;
+    stripDbAttributes(savedRow);
+    expect(savedRow).toEqual(fixture.gameStateRow);
   });
 
   it('Converts GameStateRow to GameState (unpacking)', () => {
