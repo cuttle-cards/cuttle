2baf4237e8327dac1843e9d7664003be4556c975
Henry Aviles <haviles04@gmail.com>
2024-07-28 12:21:59 -0400

    test: moved test into game-state spec

--- tests/unit/specs/game-state.spec.js
+++ tests/unit/specs/game-state.spec.js
@@ -1,8 +1,8 @@
-import { describe, it , expect, beforeEach} from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { fixture } from '../fixtures/gameState';
+import { resolveThreeData, resolveThreeSocket } from '../fixtures/gameSocketEvent';
 
-describe('Coverting GameState\'s and GameStateRow\'s', () => {
-
+describe("Coverting GameState's and GameStateRow's", () => {
   beforeEach(async () => {
     await sails.helpers.wipeDatabase();
   });
@@ -13,22 +13,32 @@ describe('Coverting GameState\'s and GameStateRow\'s', () => {
     //remove attributes added while creating the entry in the database
     const stripDbAttributes = (obj) => {
       const attributesToRemove = ['createdAt', 'id', 'updatedAt'];
-      attributesToRemove.forEach(attr => delete obj[attr]);
+      attributesToRemove.forEach((attr) => delete obj[attr]);
     };
 
     stripDbAttributes(gameStateRow);
     expect(gameStateRow).toEqual(fixture.gameStateRow);
 
-    const dbGameStateRows = await GameStateRow.find({ gameId : gameStateRow.gameId });
+    const dbGameStateRows = await GameStateRow.find({ gameId: gameStateRow.gameId });
     expect(dbGameStateRows.length).toEqual(1);
 
-    const [ savedRow ] = dbGameStateRows;
+    const [savedRow] = dbGameStateRows;
     stripDbAttributes(savedRow);
     expect(savedRow).toEqual(fixture.gameStateRow);
   });
 
   it('Converts GameStateRow to GameState (unpacking)', () => {
-    const gameState =  sails.helpers.gamestate.unpackGamestate(fixture.gameStateRow);
+    const gameState = sails.helpers.gamestate.unpackGamestate(fixture.gameStateRow);
     expect(fixture.gameState).toEqual(gameState);
+  });
+});
+
+describe('Emiting socket events', () => {
+  it('Correctly emits resolve-three', async () => {
+    const socketEvent = await sails.helpers.gamestate.emitGameState(
+      resolveThreeData.game,
+      resolveThreeData.gameState,
+    );
+    expect(socketEvent).toEqual(resolveThreeSocket);
   });
 });
