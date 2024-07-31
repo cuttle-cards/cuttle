bd21ab769771995b50eb17518a8ef86e6c88552a
Henry <haviles04@gmail.com>
2024-07-31 17:20:34 -0400

    test: moved test fixture and adjusted tests

--- tests/unit/specs/game-state.spec.js
+++ tests/unit/specs/game-state.spec.js
@@ -1,6 +1,11 @@
 import { describe, it, expect, beforeEach } from 'vitest';
-import { fixture } from '../fixtures/gameState';
-import { resolveThreeData, resolveThreeSocket } from '../fixtures/gameSocketEvent';
+import { fixture, resolveThree } from '../fixtures/gameState';
+
+//remove attributes added while creating the entry in the database
+const stripDbAttributes = (obj) => {
+  const attributesToRemove = ['createdAt', 'id', 'updatedAt'];
+  attributesToRemove.forEach((attr) => delete obj[attr]);
+};
 
 describe("Coverting GameState's and GameStateRow's", () => {
   beforeEach(async () => {
@@ -9,12 +14,6 @@ describe("Coverting GameState's and GameStateRow's", () => {
 
   it('Converts GameState to GameStateRow (packing)', async () => {
     const gameStateRow = await sails.helpers.gamestate.saveGamestate(fixture.gameState);
-
-    //remove attributes added while creating the entry in the database
-    const stripDbAttributes = (obj) => {
-      const attributesToRemove = ['createdAt', 'id', 'updatedAt'];
-      attributesToRemove.forEach((attr) => delete obj[attr]);
-    };
 
     stripDbAttributes(gameStateRow);
     expect(gameStateRow).toEqual(fixture.gameStateRow);
@@ -33,12 +32,18 @@ describe("Coverting GameState's and GameStateRow's", () => {
   });
 });
 
-describe('Emiting socket events', () => {
-  it('Correctly emits resolve-three', async () => {
-    const socketEvent = await sails.helpers.gamestate.emitGameState(
-      resolveThreeData.game,
-      resolveThreeData.gameState,
-    );
-    expect(socketEvent).toEqual(resolveThreeSocket);
+describe('resolveThree', () => {
+  it('Converts resolveThree GameStateRow to gameState, then emits socket', async () => {
+    const gameStateRow = await sails.helpers.gamestate.saveGamestate(resolveThree.gameState);
+    stripDbAttributes(gameStateRow);
+    expect(gameStateRow).toEqual(resolveThree.gameStateRow);
+
+    const gameState = await sails.helpers.gamestate.unpackGamestate(gameStateRow);
+    stripDbAttributes(gameState);
+    expect(gameState).toEqual(resolveThree.gameState);
+
+    const socketEvent = await sails.helpers.gamestate.emitGameState(resolveThree.game, gameState);
+    stripDbAttributes(socketEvent.game);
+    expect(socketEvent).toEqual(resolveThree.socket);
   });
 });
