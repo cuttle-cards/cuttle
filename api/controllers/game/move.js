d8f154984a20b0703b934200f57b68f67eded5db
Henry <haviles04@gmail.com>
2024-08-18 12:12:13 -0400

    refactor: updated to make single endpoint reuable

--- api/controllers/game/move.js
+++ api/controllers/game/move.js
@@ -1,14 +1,13 @@
-// api/controller/game/draw.js
-// Request to draw a card
+// api/controller/game/:gameId/move
+// Request to make a move
 module.exports = async function (req, res) {
   try {
-    const { validate, execute } = sails.helpers.moves;
-
+    const { validate, execute, parseRequest } = sails.helpers[req.body.moveType];
     const game = await sails.helpers.lockGame(req.params.gameId);
-    const currentState = sails.helpers.unpackGameState({ gameStateRow: [...game.gameStates.pop()] });
-    const requestedMove = sails.helpers.moves.points.parseRequest({ req });
-    validate({ currentState, requestedMove });
-    const updatedState = execute({ currentState, requestedMove });
+    const gameState = sails.helpers.unpackGameState({ gameStateRow: [...game.gameStates.pop()] });
+    const requestedMove = parseRequest({ req });
+    validate({ gameState, requestedMove });
+    const updatedState = execute({ gameState, requestedMove });
     await sails.helpers.saveGameState({ updatedState });
     await sails.helpers.emitGameState({ game, gameState: updatedState });
     await sails.helpers.unlockGame({ game });
