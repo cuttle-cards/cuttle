6ebb5b48894abfc4d5a29d61b89a7de593f45c18
Henry <haviles04@gmail.com>
2024-08-19 19:33:17 -0400

    fix: testing single file

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,23 @@
 const { defineConfig } = require('cypress');
 
-module.exports = defineConfig({
+const useGameState = process.env.VITE_USE_GAMESTATE_API || 'false';
+
+const excludedFiles =
+  useGameState === 'true'
+    ? ['tests/e2e/specs/playground.spec.js', 'tests/e2e/specs/in-game/**/*.spec.js']
+    : ['test/e2e/specs/playground.spec.js'];
+
+const cypressConfig = {
   projectId: 'i8bxr8',
   // https://docs.cypress.io/guides/references/configuration#e2e
   e2e: {
     baseUrl: process.env.VITE_API_URL || 'http://localhost:8080',
     specPattern: ['tests/e2e/specs/**/*.spec.js'],
-    excludeSpecPattern: ['tests/e2e/specs/playground.spec.js'],
+    excludeSpecPattern: excludedFiles,
     supportFile: 'tests/e2e/support/index.js',
   },
   env: {
-    gameStateAPI: process.env.VITE_USE_GAMESTATE_API || 'false',
+    gameStateAPI: useGameState,
   },
   // Retry tests 2 times headlessly, no retries in UI
   retries: {
@@ -25,4 +32,8 @@ module.exports = defineConfig({
   fixturesFolder: 'tests/e2e/fixtures',
   screenshotsFolder: 'tests/e2e/screenshots',
   videosFolder: 'tests/e2e/videos',
-});
+};
+
+console.log(process.env.VITE_API_URL);
+
+module.exports = defineConfig(cypressConfig);
