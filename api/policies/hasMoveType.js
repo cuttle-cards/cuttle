b55cb753023c7861593c931cfb97ce939bd6c6a0
Henry <haviles04@gmail.com>
2024-08-23 15:49:32 -0400

    refactor: refactored to use values instead of keys

--- api/policies/hasMoveType.js
+++ api/policies/hasMoveType.js
@@ -1,7 +1,7 @@
 const moveType = require('../../utils/MoveType.json');
 
 module.exports = function (req, res, next) {
-  if (Object.keys(moveType).some((key) => moveType[key] === req.body.moveType)) {
+  if (Object.values(moveType).includes(req.body.moveType)) {
     return next();
   }
 
