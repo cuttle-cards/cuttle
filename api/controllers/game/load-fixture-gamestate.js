92cb130089cfdb385e13e2f9fbb2cfba42efdcaf
Henry <haviles04@gmail.com>
2024-09-02 15:29:27 -0400

    refactor: refactored to accept fixtures containing ids

--- api/controllers/game/load-fixture-gamestate.js
+++ api/controllers/game/load-fixture-gamestate.js
@@ -8,37 +8,32 @@ module.exports = async function (req, res) {
       .populate('gameStates')
       .populate('p0')
       .populate('p1');
-    const { addCardId, publishGameState, saveGamestate, convertStrToCard } = sails.helpers.gamestate;
-    const p0Hand = req.body.p0Hand?.map((card) => addCardId(card));
-    const p0Points = req.body.p0Points?.map((card) => addCardId(card));
-    const p0FaceCards = req.body.p0FaceCards?.map((card) => addCardId(card));
-    const p1Hand = req.body.p1Hand?.map((card) => addCardId(card));
-    const p1Points = req.body.p1Points?.map((card) => addCardId(card));
-    const p1FaceCards = req.body.p1FaceCards?.map((card) => addCardId(card));
-    const topCard = req.body.topCard ? addCardId(req.body.topCard) : null;
-    const secondCard = req.body.secondCard ? addCardId(req.body.topCard) : null;
-    const scrap = req.body.scrap?.map((card) => addCardId(card)) ?? [];
 
-    const allFixtureCards = [
+    const { publishGameState, saveGamestate, convertStrToCard } = sails.helpers.gamestate;
+
+    const {
       p0Hand,
-      p0FaceCards,
       p0Points,
+      p0FaceCards,
       p1Hand,
-      p1FaceCards,
       p1Points,
-      topCard,
-      secondCard,
-      scrap,
-    ].flat();
+      p1FaceCards,
+      topCard = null,
+      secondCard = null,
+      scrap = [],
+      deck,
+    } = req.body;
+
+    const allFixtureCards = Object.values(req.body).flat();
 
     //Populate deck with all cards except the cards in the fixture
-    const populatedDeck = req.body.deck
-      ? req.body.deck.map((card) => addCardId(card))
-      : _.shuffle(
-          DeckIds.filter((id) => !allFixtureCards.some((card) => card?.id === id)).map((id) =>
-            convertStrToCard(id),
-          ),
-        );
+    const populatedDeck =
+      deck ??
+      _.shuffle(
+        DeckIds.filter((id) => !allFixtureCards.some((card) => card?.id === id)).map((id) =>
+          convertStrToCard(id),
+        ),
+      );
 
     if (secondCard) {
       populatedDeck.unshift(secondCard);
