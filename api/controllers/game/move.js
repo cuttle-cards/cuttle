5657d9925083e99000bcfe59d884e11f311b7f90
Henry <haviles04@gmail.com>
2024-09-05 15:55:57 -0400

    fix: misc fix + error

--- api/controllers/game/move.js
+++ api/controllers/game/move.js
@@ -1,15 +1,19 @@
 // Request to make a move
 module.exports = async function (req, res) {
   try {
-    const { validate, execute, parseRequest } = sails.helpers[req.body.moveType];
+    const { saveGamestate, publishGameState, unpackGamestate } = sails.helpers.gamestate;
+    const { execute } = sails.helpers.gamestate.moves[req.body.moveType];
     const game = await sails.helpers.lockGame(req.params.gameId);
-    const gameState = sails.helpers.unpackGameState({ gameStateRow: [...game.gameStates].pop() });
-    const requestedMove = parseRequest(req.body);
-    validate({ gameState, requestedMove });
-    const updatedState = execute({ gameState, requestedMove });
-    await sails.helpers.saveGameState({ updatedState });
-    await sails.helpers.emitGameState({ game, gameState: updatedState });
-    await sails.helpers.unlockGame({ game });
+    const gameState = unpackGamestate(game.gameStates.at(-1));
+
+    if (gameState.turn % 2 !== req.body.playedBy) {
+      throw { message: 'game.snackbar.global.notYourTurn' };
+    }
+
+    const updatedState = execute(gameState, req.body);
+    await saveGamestate(updatedState);
+    await publishGameState(game, updatedState);
+    await sails.helpers.unlockGame(game.lock);
 
     return res.ok();
   } catch (err) {
