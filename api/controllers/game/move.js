d490c9375fa877c197f6080371e9eebf4fbbe6f2
Henry <haviles04@gmail.com>
2024-09-06 16:20:19 -0400

    feat: throw error on missing gamestate

--- api/controllers/game/move.js
+++ api/controllers/game/move.js
@@ -5,6 +5,11 @@ module.exports = async function (req, res) {
     const { saveGamestate, publishGameState, unpackGamestate } = sails.helpers.gamestate;
     const { execute, validate } = sails.helpers.gamestate.moves[req.body.moveType];
     game = await sails.helpers.lockGame(req.params.gameId);
+    const gameState = unpackGamestate(game.gameStates.at(-1));
+    if (!game.gameStates.length || !gameState) {
+      throw new Error({ message: 'Game has not yet started' });
+    }
+
     //Check player exist, and is in game
     let playedBy;
     switch (req.session.usr) {
@@ -17,7 +22,6 @@ module.exports = async function (req, res) {
       default:
         throw new Error('You are not a player in this game!');
     }
-    const gameState = unpackGamestate(game.gameStates.at(-1));
     validate(req.body, gameState, playedBy);
     const updatedState = execute(gameState, req.body, playedBy);
     const gameStateRow = await saveGamestate(updatedState);
