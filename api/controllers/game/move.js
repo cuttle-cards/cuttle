f6bb34147a35dc4cc1a88cb870aef86ee52ecfa5
Henry <haviles04@gmail.com>
2024-09-06 13:49:10 -0400

    refactor: changed error handling

--- api/controllers/game/move.js
+++ api/controllers/game/move.js
@@ -1,11 +1,12 @@
 // Request to make a move
 module.exports = async function (req, res) {
+  let game;
   try {
     const { saveGamestate, publishGameState, unpackGamestate } = sails.helpers.gamestate;
     const { execute, validate } = sails.helpers.gamestate.moves[req.body.moveType];
     const game = await sails.helpers.lockGame(req.params.gameId);
     const gameState = unpackGamestate(game.gameStates.at(-1));
-    await validate(req.body, gameState, game.lock);
+    validate(req.body, gameState);
     const updatedState = execute(gameState, req.body);
     await saveGamestate(updatedState);
     await publishGameState(game, updatedState);
@@ -13,6 +14,7 @@ module.exports = async function (req, res) {
 
     return res.ok();
   } catch (err) {
+    await sails.helpers.unlockGame(game.lock);
     return res.badRequest({ message: err.message });
   }
 };
--- api/helpers/gamestate/moves/points/validate.js
+++ api/helpers/gamestate/moves/points/validate.js
@@ -6,36 +6,32 @@ module.exports = {
   description: 'Verifies whether a request to make points is legal, throwing explanatory error if not.',
 
   inputs: {
-    requestedMove: {
-      type: 'ref',
-      description: 'Object containing data needed for current move',
-      required: true,
-    },
     /**
      * @param {Object} requestedMove - Object describing the request to play points
      * @param {1 | 0} requestedMove.playedBy - Which player is playing
      * @param { Card } requestedMove.cardPlayed - Card Played for points
      * @param { MoveType.POINTS } requestedMove.moveType - Specifies that this a Points move
      */
+    requestedMove: {
+      type: 'ref',
+      description: 'Object containing data needed for current move',
+      required: true,
+    },
     currentState: {
       type: 'ref',
       descriptions: 'Object containing the current game state',
       required: true,
     },
-    lock: {
-      type: 'string',
-      descriptions: 'UUID Lock',
-      required: true,
-    },
   },
-  fn: async ({ requestedMove, currentState, lock }, exits) => {
+  sync: true,
+  fn: ({ requestedMove, currentState }, exits) => {
     try {
       const playedBy = requestedMove.playedBy ? 'p1' : 'p0';
 
       const cardPlayed = currentState[playedBy].hand.find(({ id }) => id === requestedMove.cardId);
 
       if (currentState.phase !== GamePhase.MAIN) {
-        throw new Error('Cannot play points during this phase');
+        throw new Error(`Can only play points in main phase, not ${currentState.phase}`);
       }
 
       if (!cardPlayed) {
@@ -53,9 +49,9 @@ module.exports = {
       if (cardPlayed.rank > 10) {
         throw new Error('game.snackbar.points.numberOnlyForPoints');
       }
+
       return exits.success();
     } catch (err) {
-      await sails.helpers.unlockGame(lock);
       return exits.error(err);
     }
   },
