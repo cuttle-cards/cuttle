5713c37a29ffec410b5fdf29e2b01a481833a908
Henry <haviles04@gmail.com>
2024-09-07 08:06:32 -0400

    chore: removed unused policy

--- api/policies/hasMoveType.js
+++
@@ -1,9 +0,0 @@
-const moveType = require('../../utils/MoveType.json');
-
-module.exports = function (req, res, next) {
-  if (Object.values(moveType).includes(req.body.moveType)) {
-    return next();
-  }
-
-  return res.badRequest({ message: 'Error: Missing or invalid moveType' });
-};
