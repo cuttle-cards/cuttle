4ecfdbe6696f4b064a8b35b209b0f452dd7ac96d
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-09-19 14:53:57 -0400

    Feat/resolve gamestate (#1047)
    * chore(moves/draw/validate): correct comments
    
    * feat(moves/resolve/validate): implement validation
    
    * chore: fix comments in move helpers
    
    * feat(moves/resolve/execute): implement skeleton of resolve
    
    * feat(moves/resolve/execute): implement execute and resolve ace
    
    * fix(moves/resolve/execute): set phase to MAIN
    
    * test(1_aces.spec): unskip test
    
    * chore(moves/resolve/execute): remove unused variables
    
    * feat(moves/resolve/execute): set oneOffTarget and oneOffTargetType to null
    
    * chore(moves/draw/validate.js): revise error message
    
    Co-authored-by: Henry Aviles <108551827+Haviles04@users.noreply.github.com>
    
    * test(1_aces): add test case to confirm aces scrap jacks
    
    ---------
    
    Co-authored-by: Henry Aviles <108551827+Haviles04@users.noreply.github.com>

---
+++ api/helpers/gamestate/moves/resolve/execute.js
@@ -0,0 +1,68 @@
+const GamePhase = require('../../../../../utils/GamePhase.json');
+const MoveType = require('../../../../../utils/MoveType.json');
+
+module.exports = {
+  friendlyName: 'Resolve One-Off',
+
+  description: 'Returns new GameState resulting from requested resolve move',
+
+  inputs: {
+    currentState: {
+      type: 'ref',
+      description: 'The latest GameState before the requesting player resolves the pending one-off and counters',
+      required: true,
+    },
+    /**
+     * @param { Object } requestedMove - The move being requested. 
+     * Specifies that the move is to resolve
+     * @param { MoveType.RESOLVE } requestedMove.moveType
+     */
+    requestedMove: {
+      type: 'ref',
+      description: 'The move being requested. Specifies which player is asking to play a one-off ',
+    },
+    playedBy: {
+      type: 'number',
+      description: 'Player number of player requesting move.',
+    },
+  },
+  sync: true, // synchronous helper
+  fn: ({ currentState, requestedMove, playedBy }, exits) => {
+    let result = _.cloneDeep(currentState);
+
+    const { oneOff } = result;
+
+    // Move oneOff + twos into scrap and increment turn
+    result.scrap.push(result.oneOff);
+    result.scrap.push(...result.twos);
+    result = {
+      ...result,
+      ...requestedMove,
+      phase: GamePhase.MAIN,
+      playedBy,
+      resolved: oneOff,
+      oneOff: null,
+      oneOffTarget: null,
+      oneOffTargetType: null,
+      twos: [],
+      turn: result.turn + 1,
+    };
+
+    // If one-off was fizzles, make no other changes
+    const fizzles = result.twos.length % 2 === 1;
+    if (fizzles) {
+      result.moveType = MoveType.FIZZLE;
+      return exits.success(result);
+    }
+
+    switch (oneOff.rank) {
+      case 1:
+        result = sails.helpers.gamestate.moves.resolve.ace(result);
+        break;
+      default:
+        return exits.error(new Error(`${oneOff.rank} is not a valid one-off rank`));
+    }
+
+    return exits.success(result);
+  },
+};
