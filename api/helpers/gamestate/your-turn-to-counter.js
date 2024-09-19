fe21f2897e669baa2a512ea359435cdd981f9f03
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-09-19 15:06:25 -0400

    chore(helpers/your-turn-to-counter): removed uselsess try/catch

--- api/helpers/gamestate/your-turn-to-counter.js
+++ api/helpers/gamestate/your-turn-to-counter.js
@@ -1,7 +1,8 @@
 module.exports = {
   friendlyName: 'Is your chance to counter',
 
-  description: "Verifies whether it is the requesting player's opportunity to counter. Returns `true` if so and `false` otherwise",
+  description:
+    "Verifies whether it is the requesting player's opportunity to counter. Returns `true` if so and `false` otherwise",
 
   inputs: {
     currentState: {
@@ -17,18 +18,13 @@ module.exports = {
   },
   sync: true,
   fn: ({ currentState, playedBy }, exits) => {
-    try {
-      
-      // It's your chance if it is your turn and there are an odd number of twos
-      // or if it's your opponent's turn and there are an even number of twos
-      // (since the opponent of the person who played the one-off counters first)
-      const isYourTurn = currentState.turn % 2 === playedBy;
-      const numTwosIsEven = currentState.twos.length % 2 === 0;
-      const res = isYourTurn !== numTwosIsEven;
+    // It's your chance if it is your turn and there are an odd number of twos
+    // or if it's your opponent's turn and there are an even number of twos
+    // (since the opponent of the person who played the one-off counters first)
+    const isYourTurn = currentState.turn % 2 === playedBy;
+    const numTwosIsEven = currentState.twos.length % 2 === 0;
+    const res = isYourTurn !== numTwosIsEven;
 
-      return exits.success(res);
-    } catch (err) {
-      return exits.error(err);
-    }
+    return exits.success(res);
   },
 };
