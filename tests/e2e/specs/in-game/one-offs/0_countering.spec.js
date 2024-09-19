05e029df292fb47a12928d39ca087db3f642eb26
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-09-19 15:09:47 -0400

    test(0_countering): revise error message assertion

--- tests/e2e/specs/in-game/one-offs/0_countering.spec.js
+++ tests/e2e/specs/in-game/one-offs/0_countering.spec.js
@@ -513,7 +513,7 @@ describe('Opponent May Counter vs Opponent Must Resolve', () => {
           } catch (err) {
             const errors = [
               "Can't play while waiting for opponent to counter",
-              'Can only play draw in main phase',
+              'Can only draw in main phase',
             ];
             expect(err).to.be.oneOf(errors);
           }
