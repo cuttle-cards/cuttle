983bd99811d4617c36c4ba0703a6b7b32044746e
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-09-19 11:22:36 -0400

    refactor: pull yourChanceToCounter into its own helper

--- api/helpers/gamestate/moves/counter/validate.js
+++ api/helpers/gamestate/moves/counter/validate.js
@@ -47,12 +47,8 @@ module.exports = {
       }
 
       // Must be your chance to counter
-      // It's your chance if it is your turn and there are an odd number of twos
-      // or if it's your opponent's turn and there are an even number of twos
-      // (since the opponent of the person who played the one-off counters first)
-      const isYourTurn = currentState.turn % 2 === playedBy;
-      const numTwosIsEven = currentState.twos.length % 2 === 0;
-      if (isYourTurn === numTwosIsEven) {
+      const yourTurnToCounter = sails.helpers.gamestate.yourTurnToCounter(currentState, playedBy);
+      if (!yourTurnToCounter) {
         throw new Error('Waiting for opponent to counter');
       }
 
