954acac981543ebe73ac85083bd6def27a71d6f4
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-10-02 14:29:36 -0400

    Test/player seven unskip (#1067)
    * test(player_sevens.spec): unskip it('Disables move choices when selecting card in hand while resolving seven')
    
    * test(player_sevens.spec): remove duplicate of it('Disables move choices when selecting card in hand while resolving seven')
    
    * fix(one-off/validate): remove 'without a target' from final error msg
    
    * test(end_of_deck_sevens.spec): unskip  it('Plays the top card of the deck when there are two cards left')
    
    * test(end_of_deck_sevens.spec): unskip it('Plays the 2nd card in the deck when there are two cards left')
    
    * test(end_of_deck_sevens.spec): unskip it('Cannot play seven one-off if deck is empty')
    
    * chore(end_of_deck_sevens.spec): remove comment

--- tests/e2e/specs/in-game/one-offs/7_sevens/end_of_deck_sevens.spec.js
+++ tests/e2e/specs/in-game/one-offs/7_sevens/end_of_deck_sevens.spec.js
@@ -28,7 +28,6 @@ describe('Playing sevens at the end of the deck', () => {
   });
 
   it('Plays the top card of the deck when there are two cards left', () => {
-    cy.skipOnGameStateApi();
     cy.setupGameAsP0();
     cy.loadGameFixture(0, {
       p0Hand: [Card.SEVEN_OF_CLUBS],
@@ -65,7 +64,6 @@ describe('Playing sevens at the end of the deck', () => {
   });
 
   it('Plays the 2nd card in the deck when there are two cards left', () => {
-    cy.skipOnGameStateApi();
     cy.setupGameAsP0();
     cy.loadGameFixture(0, {
       p0Hand: [Card.SEVEN_OF_CLUBS],
@@ -102,7 +100,6 @@ describe('Playing sevens at the end of the deck', () => {
   });
 
   it('Cannot play seven one-off if deck is empty', () => {
-    cy.skipOnGameStateApi();
     cy.setupGameAsP1();
     cy.loadGameFixture(1, {
       p0Hand: [],
