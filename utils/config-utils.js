3932253344d6b3a20340b7cfa59c8768be2c3254
Sean Kennedy <sean@codeserious.com>
2024-10-07 20:46:33 -0400

    Update (almost) all of the dependencies

--- utils/config-utils.js
+++ utils/config-utils.js
@@ -1,5 +1,26 @@
 import { version } from '_/package.json';
 
+async function initDevtools() {
+  try {
+    // Check devtools availability
+    const response = await fetch('/api/devtools-health');
+    const { alive } = await response.json();
+    if (!alive) {
+      throw new Error('Devtools not available');
+    }
+    // Add devtools script to the DOM
+    const script = document.createElement('script');
+    script.src = 'http://localhost:8098';
+    script.async = true;
+    script.onerror = () => {
+      console.error('Failed to load the devtools script');
+    };
+    document.head.appendChild(script);
+  } catch (err) {
+    console.error('Error checking devtools availability:', err.message);
+  }
+}
+
 export async function initCuttleGlobals(app) {
   // We work under the assumption that this function will only be called in a context
   // where the window object exists. If we plan to ever call this on the server we'll
@@ -15,12 +36,6 @@ export async function initCuttleGlobals(app) {
 
   // Connect the devtools -- non-prod only
   if (import.meta.env.DEV) {
-    try {
-      const devtools = await import('@vue/devtools');
-      devtools.connect(null, 8098);
-      console.log('Vue devtools connected');
-    } catch (err) {
-      console.warn('Failed to connect vue devtools - try running npm run start:devtools');
-    }
+    document.addEventListener('DOMContentLoaded', initDevtools);
   }
 }
