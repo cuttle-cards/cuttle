a1d51d0669761a4cca5f534f8834f422d43b196e
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-10-09 08:56:34 -0400

    refactor(moves/points/validate): replace playerKey string with player obj

--- api/helpers/game-states/moves/points/validate.js
+++ api/helpers/game-states/moves/points/validate.js
@@ -30,9 +30,9 @@ module.exports = {
   sync: true,
   fn: ({ requestedMove, currentState, playedBy }, exits) => {
     try {
-      const playerKey = playedBy ? 'p1' : 'p0';
+      const player = playedBy ? currentState.p1 : currentState.p0;
 
-      const playedCard = currentState[playerKey].hand.find(({ id }) => id === requestedMove.cardId);
+      const playedCard = player.hand.find(({ id }) => id === requestedMove.cardId);
 
       if (currentState.phase !== GamePhase.MAIN) {
         throw new Error('game.snackbar.global.notInMainPhase');
