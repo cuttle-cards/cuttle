8621f9273d69f83d9b303c6f0deb0ec869c70d98
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-11-05 08:55:29 -0500

    feat(BaseParagraph): implement component

---
+++ src/components/BaseParagraph.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2 v-if="heading">
+      {{ t(heading) }}
+    </h2>
+    <p>
+      <template v-for="(chunk, index) in paragraph" :key="`text-chunk-${index}`">
+        <!-- Remote Link -->
+        <a v-if="chunk.url" :href="chunk.url">{{ t(chunk.text) }}</a>
+        <!-- Plain text -->
+        <template v-else>
+          {{ t(chunk.text) }}
+        </template>
+      </template>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+
+defineProps({
+  heading: {
+    type: String,
+    default: '',
+  },
+  paragraph: {
+    type: Array,
+    required: true,
+  },
+});
+
+const { t } = useI18n();
+
+</script>
