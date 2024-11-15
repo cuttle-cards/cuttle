1b909e0289b7a7cee8dc98d71108f7d43e9de2a2
Sean Kennedy <sean@codeserious.com>
2024-11-15 10:45:54 -0500

    Add better defaults

--- src/plugins/rollbar.js
+++ src/plugins/rollbar.js
@@ -2,18 +2,33 @@ import { version } from '_/package.json';
 
 import Rollbar from 'rollbar';
 
+// https://docs.rollbar.com/docs/rollbarjs-configuration-reference#notifier
 const config = {
   accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+  // Anonymize IP addresses
+  captureIp: 'anonymize',
+  // Capture uncaught exceptions and unhandled rejections
   captureUncaught: true,
   captureUnhandledRejections: true,
+  // Only allow items to be sent from certain domains
+  hostSafeList: [
+    'cuttle.cards',
+    ...(!import.meta.env.PROD) ? [
+      'localhost',
+    ] : [],
+  ],
   payload: {
     environment: import.meta.env.PROD ? 'production' : 'development',
     client: {
       javascript: {
         code_version: version,
+        source_map_enabled: true,
       }
-    }
+    },
   },
+  // Only send "error" or higher items to Rollbar
+  logLevel: 'error',
+  reportLevel: 'error',
 };
 
 const rollbar = new Rollbar(config);
