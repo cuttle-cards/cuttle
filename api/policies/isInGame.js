0565be4dd6381d3902f2b7bcf5e79b62786b14ac
Ryan Emberling <itsalaidbacklife@gmail.com>
2024-12-31 14:49:11 -0500

    fix(isInGame): set error back to badRequest() and add TODO

--- api/policies/isInGame.js
+++ api/policies/isInGame.js
@@ -7,6 +7,8 @@
  * @docs        :: http://sailsjs.org/#!/documentation/concepts/Policies
  *
  */
+
+// TODO #965 - Delete this policy (no longer using req.session.game)
 module.exports = function (req, res, next) {
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
@@ -14,9 +16,8 @@ module.exports = function (req, res, next) {
     if (typeof req.session.game === 'number') {
       return next();
     }
-    // TODO #1094 - Translate errors
     return res.badRequest({ message: `Error with user's session: 'game' must be an integer` });
   }
 
-  return res.forbidden({ message: `You are not a player in this game!` });
+  return res.badRequest({ message: `You are not a player in this game!` });
 };
