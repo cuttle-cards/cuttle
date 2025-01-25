bbd0e1108fba3b4daca88a6387eabd8fd4a47d68
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-01-25 09:42:26 -0500

    feat(stalemate): add stalemate-accept and stalemate-request helpers

---
+++ api/helpers/game-states/moves/stalemate-accept/execute.js
@@ -0,0 +1,47 @@
+const GamePhase = require('../../../../../utils/GamePhase.json');
+
+module.exports = {
+  friendlyName: 'Accept offer for Stalemate',
+
+  description: 'Accept opponent\'s stalemate offer',
+
+  inputs: {
+    currentState: {
+      type: 'ref',
+      descriptions: 'Object containing the current game state',
+      required: true,
+    },
+    /**
+     * @param { Object } requestedMove - Object describing the request for stalemate
+     * @param { MoveType.STALEMATE_ACCDEPT } requestedMove.moveType - Specifies that this accepts a stalemate offer
+     * @param { 1 | 0 } requestedMove.playedBy - Which player is requesting a stalemate
+     */
+    requestedMove: {
+      type: 'ref',
+      description: 'Object containing data needed for current move',
+      required: true,
+    },
+    playedBy: {
+      type: 'number',
+      description: 'Player number of player requesting move',
+      required: true,
+    },
+  },
+  sync: true,
+  fn: ({ currentState, requestedMove, playedBy }, exits) => {
+    let result = _.cloneDeep(currentState);
+
+    result = {
+      ...result,
+      ...requestedMove,
+      playedBy,
+      phase: GamePhase.MAIN,
+      playedCard: null,
+      targetCard: null,
+      discardedCards: [],
+      resolved: null,
+    };
+
+    return exits.success(result);
+  },
+};
---
+++ api/helpers/game-states/moves/stalemate-accept/validate.js
@@ -0,0 +1,47 @@
+const GamePhase = require('../../../../../utils/GamePhase.json');
+
+module.exports = {
+  friendlyName: 'Validate request to accept pending stalemate offer',
+
+  description: 'Verifies request to accept stalemate offer is legal, throwing explanatory error if not.',
+
+  inputs: {
+    currentState: {
+      type: 'ref',
+      descriptions: 'Object containing the current game state',
+      required: true,
+    },
+    /**
+     * @param { Object } requestedMove - Object describing the request for stalemate
+     * @param { MoveType.STALEMATE_ACCEPT } requestedMove.moveType - Specifies that this a to accept a stalemate offer
+     * @param { 1 | 0 } requestedMove.playedBy - Which player is accepting the stalemate
+     */
+    requestedMove: {
+      type: 'ref',
+      description: 'Object containing data needed for current move',
+      required: true,
+    },
+    playedBy: {
+      type: 'number',
+      description: 'Player number of player requesting move',
+      required: true,
+    },
+  },
+  sync: true,
+  fn: ({ currentState, playedBy }, exits) => {
+    try {
+      // Must not already be considering a stalemate
+      if (currentState.phase !== GamePhase.STALEMATE_REQUEST) {
+        throw new Error('game.snackbar.stalemate.noStalemateOffered');
+      }
+
+      if (currentState.playedBy === playedBy) {
+        throw new Error('game.snackbar.stalemate.opponentConsideringStalemate');
+      }
+
+      return exits.success();
+    } catch (err) {
+      return exits.error(err);
+    }
+  },
+};
