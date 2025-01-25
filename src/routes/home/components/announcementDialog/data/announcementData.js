f1c62202a307e13a0e4089a0513ccc6d2d28b411
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-01-25 11:15:38 -0500

    style(announcementDialog): add surface-2 (cream) background to image

--- src/routes/home/components/announcementDialog/data/announcementData.js
+++ src/routes/home/components/announcementDialog/data/announcementData.js
@@ -1,10 +1,11 @@
-import { Card } from '../../../../../../tests/e2e/fixtures/cards';
+// import { Card } from '../../../../../../tests/e2e/fixtures/cards';
 
 export const announcementData = {
   id: 'spades2024Announcement',
   activatorText: 'announcement.activatorText',
   title: 'announcement.title',
-  displayCards: [ Card.JACK_OF_HEARTS, Card.QUEEN_OF_HEARTS, Card.KING_OF_HEARTS, Card.ACE_OF_HEARTS ],
+  displayCards: [],
+  imgSrc: '/img/announcement/spades-2024-bracket.svg',
   startTime: '2025-01-24',
   endTime: '2025-01-26',
   announcementText: [
