b217e4b467a1dd080e8edbf77b1aacb7c549276f
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-01-25 10:51:41 -0500

    feat(announcementData): change id to spades2024Announcement

--- src/routes/home/components/announcementDialog/data/announcementData.js
+++ src/routes/home/components/announcementDialog/data/announcementData.js
@@ -1,7 +1,7 @@
 import { Card } from '../../../../../../tests/e2e/fixtures/cards';
 
 export const announcementData = {
-  id: 'hearts2024Announcement',
+  id: 'spades2024Announcement',
   activatorText: 'announcement.activatorText',
   title: 'announcement.title',
   displayCards: [ Card.JACK_OF_HEARTS, Card.QUEEN_OF_HEARTS, Card.KING_OF_HEARTS, Card.ACE_OF_HEARTS ],
