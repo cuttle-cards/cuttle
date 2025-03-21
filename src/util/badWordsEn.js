df18657d3100463609370b5c92450daebe757c5e
tcox-coding <taylor@coxintl.com>
2025-03-21 14:04:58 -0400

    changed single quotes to double quotes

--- src/util/badWordsEn.js
+++ src/util/badWordsEn.js
@@ -1,69 +1,69 @@
 const badWordsEn = [
   // #
-  "4r5e", "5h1t", "5hit", "God", 
+  '4r5e', '5h1t', '5hit', 'God', 
   
   // A
-  "a55", "a_s_s", "anal", "anus", "ar5e", "arrse", "arse", "ass", 
+  'a55', 'a_s_s', 'anal', 'anus', 'ar5e', 'arrse', 'arse', 'ass', 
   
   // B
-  "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", "balls", "bastard", "beastial", "bellend",
-  "bestial", "bi+ch", "biatch", "bitch", "bloody", "blow job", "blowjob", "boiolas", "bollock",
-  "bollok", "boner", "boob", "booobs", "boooobs", "booooobs", "booooooobs", "breasts", "buceta",
-  "bugger", "bum", "bunny fucker", "butthole", "buttmunch", "buttplug", 
+  'b!tch', 'b00bs', 'b17ch', 'b1tch', 'ballbag', 'balls', 'bastard', 'beastial', 'bellend',
+  'bestial', 'bi+ch', 'biatch', 'bitch', 'bloody', 'blow job', 'blowjob', 'boiolas', 'bollock',
+  'bollok', 'boner', 'boob', 'booobs', 'boooobs', 'booooobs', 'booooooobs', 'breasts', 'buceta',
+  'bugger', 'bum', 'bunny fucker', 'butthole', 'buttmunch', 'buttplug', 
   
   // C
-  "c0ck", "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "cnut", "cock", "cok", "coon",
-  "crap", "cum", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cyalis", "cyberfuc",
+  'c0ck', 'carpet muncher', 'cawk', 'chink', 'cipa', 'cl1t', 'clit', 'cnut', 'cock', 'cok', 'coon',
+  'crap', 'cum', 'cunilingus', 'cunillingus', 'cunnilingus', 'cunt', 'cyalis', 'cyberfuc',
   
   // D
-  "d1ck", "damn", "dick", "dildo", "dink", "dirsa", "dlck", "dog-fucker", "doggin", "donkeyribber",
-  "doosh", "duche", "dyke",
+  'd1ck', 'damn', 'dick', 'dildo', 'dink', 'dirsa', 'dlck', 'dog-fucker', 'doggin', 'donkeyribber',
+  'doosh', 'duche', 'dyke',
   
   // E
-  "ejaculate", "ejaculating", "ejaculation", "ejakulate",
+  'ejaculate', 'ejaculating', 'ejaculation', 'ejakulate',
   
   // F
-  "f u c k", "f4nny", "f4rk", "f_u_c_k", "fag", "fanny", "fanyy", "fark", "fatass", "fcuk", "feck",
-  "felching", "fellate", "fellatio", "fingerfuck", "fistfuck", "flange", "fook", "fuck", "fudge packer",
-  "fudgepacker", "fuk", "fux",
+  'f u c k', 'f4nny', 'f4rk', 'f_u_c_k', 'fag', 'fanny', 'fanyy', 'fark', 'fatass', 'fcuk', 'feck',
+  'felching', 'fellate', 'fellatio', 'fingerfuck', 'fistfuck', 'flange', 'fook', 'fuck', 'fudge packer',
+  'fudgepacker', 'fuk', 'fux',
   
   // G
-  "gangbang", "gaylord", "gaysex", "goatse", "god-dam", "goddamn",
+  'gangbang', 'gaylord', 'gaysex', 'goatse', 'god-dam', 'goddamn',
   
   // H
-  "hardcoresex", "hell", "heshe", "hoar", "hoer", "homo", "hore", "horniest", "horny", "hotsex",
+  'hardcoresex', 'hell', 'heshe', 'hoar', 'hoer', 'homo', 'hore', 'horniest', 'horny', 'hotsex',
   
   // J
-  "jack-off", "jackoff", "jap", "jerk-off", "jism", "jiz",
+  'jack-off', 'jackoff', 'jap', 'jerk-off', 'jism', 'jiz',
   
   // K
-  "kawk", "knob", "kock", "kondum", "kum", "kunilingus",
+  'kawk', 'knob', 'kock', 'kondum', 'kum', 'kunilingus',
   
   // L
-  "l3i+ch", "l3itch", "labia", "lmfao", "lust",
+  'l3i+ch', 'l3itch', 'labia', 'lmfao', 'lust',
   
   // M
-  "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "masterb8",
-  "masterbat*", "masterbat3", "masterbate", "masterbation", "masturbate", "mo-fo", "mof0", "mofo",
-  "mothafuck", "mother fucker", "motherfuck", "muff", "mutha", "muther",
+  'm0f0', 'm0fo', 'm45terbate', 'ma5terb8', 'ma5terbate', 'masochist', 'master-bate', 'masterb8',
+  'masterbat*', 'masterbat3', 'masterbate', 'masterbation', 'masturbate', 'mo-fo', 'mof0', 'mofo',
+  'mothafuck', 'mother fucker', 'motherfuck', 'muff', 'mutha', 'muther',
   
   // N
-  "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "nigger", "nob", "numbnuts", "nutsack",
+  'n1gga', 'n1gger', 'nazi', 'nigg3r', 'nigg4h', 'nigga', 'nigger', 'nob', 'numbnuts', 'nutsack',
   
   // O
-  "orgasim", "orgasm",
+  'orgasim', 'orgasm',
   
   // P
-  "p0rn", "pawn", "pecker", "penis", "phonesex", "phuck", "phuk", "phuq", "pigfucker", "pimpis",
-  "piss", "poop", "porn", "prick", "pron", "pube", "pusse", "pussi", "pussy",
+  'p0rn', 'pawn', 'pecker', 'penis', 'phonesex', 'phuck', 'phuk', 'phuq', 'pigfucker', 'pimpis',
+  'piss', 'poop', 'porn', 'prick', 'pron', 'pube', 'pusse', 'pussi', 'pussy',
   
   // R
-  "rectum", "retard", "rimjaw", "rimming",
+  'rectum', 'retard', 'rimjaw', 'rimming',
   
   // S
-  "s hit", "s.o.b.", "sadist", "schlong", "screwing", "scroat", "scrote", "scrotum", "semen",
-  "sex", "sh!+", "sh!t", "sh1t", "shag", "shemale", "shi+", "shit", "skank", "slut", "smegma",
-  "smut", "snatch", "son-of-a-bitch"
+  's hit', 's.o.b.', 'sadist', 'schlong', 'screwing', 'scroat', 'scrote', 'scrotum', 'semen',
+  'sex', 'sh!+', 'sh!t', 'sh1t', 'shag', 'shemale', 'shi+', 'shit', 'skank', 'slut', 'smegma',
+  'smut', 'snatch', 'son-of-a-bitch'
 ];
 
 module.exports = badWordsEn;
