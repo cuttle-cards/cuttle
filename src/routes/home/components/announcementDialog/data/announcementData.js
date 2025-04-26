2ab9ce4f8fdb3b4bb26e46cabc74510323c49fbe
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-04-26 07:26:41 -0400

    Feat/clubs 2025 announcement 2 (#1184)

--- src/routes/home/components/announcementDialog/data/announcementData.js
+++ src/routes/home/components/announcementDialog/data/announcementData.js
@@ -1,13 +1,13 @@
 // import { Card } from '../../../../../../tests/e2e/fixtures/cards';
 
 export const announcementData = {
-  id: 'clubs2025Announcement1',
+  id: 'clubs2025Announcement2',
   activatorText: 'announcement.activatorText',
   title: 'announcement.title',
   displayCards: [],
   imgSrc: '/img/announcement/clubs_2025_cuttle_season_championship.svg',
-  startTime: '2025-04-021',
-  endTime: '2025-04-27',
+  startTime: '2025-04-020',
+  endTime: '2025-04-22',
   announcementText: [
     {
       heading: 'announcement.heading',
