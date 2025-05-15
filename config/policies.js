7500a08ac321434ca1de400e91de31477f36bbbf
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-05-15 08:50:23 -0400

    fix(policies.js): require hasGameId for rematch-gamestate

--- config/policies.js
+++ config/policies.js
@@ -47,7 +47,7 @@ module.exports.policies = {
 
   // GameStateApi
   'game/move': [ 'isLoggedIn', 'hasValidMoveBody', 'hasGameId' ],
-  'game/rematch-gamestate': 'isLoggedIn',
+  'game/rematch-gamestate': [ 'isLoggedIn', 'hasGameId' ],
 
   /////////////////////////////////
   // DEVELOPMENT OR STAGING ONLY //
