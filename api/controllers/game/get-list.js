a0f50cdf29a8291878b7a5d43f6e8fc6eafebfc7
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-05-16 08:19:47 -0400

    chore(controllers/game/get-list.js): remove req.session.game handling

--- api/controllers/game/get-list.js
+++ api/controllers/game/get-list.js
@@ -14,33 +14,7 @@ module.exports = async function (req, res) {
       openGames,
       spectatableGames,
     };
-    if (req.session.game) {
-      try {
-        // User is currently in game -- find it and subscribe their socket
-        const game = await gameService.populateGame({ gameId: req.session.game });
-        // Add active game to response data
-        response = {
-          ...response,
-          inGame: true,
-          game,
-        };
-        Game.subscribe(req, [ game.id ]);
-        Game.publish(
-          [ req.session.game ],
-          {
-            change: 'deal',
-            pNum: req.session.pNum,
-            game: game,
-          },
-          req,
-        );
-      } catch (e) {
-        // Unable to find user's game -- remove it from their session
-        delete req.session.game;
-        delete req.session.pNum;
-        delete req.session.spectating;
-      }
-    }
+
     return res.ok(response);
   } catch (e) {
     // Failed to find list of games
