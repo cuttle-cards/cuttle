642d6ce563738f15b0c3a60969bbd1e2aecce6f3
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-05-16 08:57:13 -0400

    fix(controllers/game/get-list.js): populate player list from p0 and p1

--- api/controllers/game/get-list.js
+++ api/controllers/game/get-list.js
@@ -1,20 +1,19 @@
-const gameAPI = sails.hooks['customgamehook'];
-
 module.exports = async function (req, res) {
   // HANDLE REQ.SESSION.GAME = GAME ID CASE
   sails.sockets.join(req, 'GameList');
   try {
     const [ openGames, spectatableGames ] = await Promise.all([
-      gameAPI.findOpenGames(),
+      sails.helpers.findOpenGames(),
       sails.helpers.findSpectatableGames(),
     ]);
-    let response = {
+
+    const response = {
       inGame: false,
       userId: req.session.usr,
       openGames,
       spectatableGames,
     };
-
+  
     return res.ok(response);
   } catch (e) {
     // Failed to find list of games
---
+++ api/helpers/find-open-games.js
@@ -0,0 +1,43 @@
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+dayjs.extend(utc);
+
+module.exports = {
+  friendlyName: 'Find Open Games',
+
+  description: 'Finds all the games that are available for players to join',
+
+  fn: async (_, exits) => {
+    const recentUpdateThreshhold = dayjs.utc().subtract(4, 'hour')
+      .toDate();
+    try {
+      let games = await Game.find({
+        status: gameService.GameStatus.CREATED,
+        updatedAt: { '>=': recentUpdateThreshhold },
+      })
+        .populate('p0')
+        .populate('p1');
+
+      games = games.map((game) => {
+        const players = [];
+        if (game.p0) {
+          players.push({ username: game.p0.username });
+        }
+        if (game.p1) {
+          players.push({ username: game.p1.username });
+        }
+        const formattedGame = {
+          ...game,
+          players,
+        };
+        delete game.p0;
+        delete game.p1;
+        return formattedGame;
+      });
+
+      return exits.success(games);
+    } catch (err) {
+      return exits.error(err);
+    }
+  },
+};
