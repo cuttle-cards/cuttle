8b055afa21b4ac16bb066a6a1668be8fb9b484d1
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-05-20 08:43:39 -0400

    fix(game/join.js): send full players array including newest player

--- api/controllers/game/join.js
+++ api/controllers/game/join.js
@@ -50,10 +50,12 @@ module.exports = async function (req, res) {
     if (!game.p0) {
       pNum = 0;
       gameUpdates = { p0: userId };
+      game.players.push({ username: user.username, pNum: 0 });
     // Second player is p1
     } else if (!game.p1) {
       pNum = 1;
       gameUpdates = { p1: userId };
+      game.players.push({ username: user.username, pNum: 1 });
       sails.sockets.blast('gameFull', { id: game.id });
     }
 
