e94d4e098d27b863c11d78a94361dc9e7e66da9e
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-05-26 09:39:56 -0400

    test(winConditions.spec): revise it('Prevents player from accepting their own stalemate request') to only expect one error message

--- tests/e2e/specs/in-game/game-over/winConditions.spec.js
+++ tests/e2e/specs/in-game/game-over/winConditions.spec.js
@@ -434,12 +434,7 @@ describe('Stalemates', () => {
               // Fail test if backend allows request
               expect(true).to.eq(false, 'Expected 400 error when requesting to initiate stalemate while one is already in progress but came back 200');
             } catch (err) {
-              // TODO #965 - expect only opponentConsideringStalemate error
-              const errors = [
-                'game.snackbar.stalemate.previousStalemateRejected',
-                'game.snackbar.stalemate.opponentConsideringStalemate',
-              ];
-              expect(err).to.be.oneOf(errors);
+              expect(err).to.eq('game.snackbar.stalemate.opponentConsideringStalemate');
             }
           });
       });
