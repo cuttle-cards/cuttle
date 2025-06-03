008f78721f7ae5f34ecd2cfc9894c7de3f69533d
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-06-03 17:07:01 -0400

    feat(PlaybackControlls.vue): enable stepping forward on button click

--- src/routes/game/components/PlaybackControls.vue
+++ src/routes/game/components/PlaybackControls.vue
@@ -2,10 +2,40 @@
   <menu data-cy="playback-controls">
     <v-btn variant="text" icon="mdi-skip-backward" data-cy="skip-backward" />
     <v-btn variant="text" icon="mdi-step-backward" data-cy="step-backward" />
-    <v-btn variant="text" icon="mdi-step-forward" data-cy="step-forward" />
-    <v-btn variant="text" icon="mdi-skip-forward" data-cy="skip-forward" />    
+    <v-btn
+      :disabled="!gameHistoryStore.canGoToNextState"
+      variant="text"
+      icon="mdi-step-forward"
+      data-cy="step-forward"
+      @click="stepForward"
+    />
+    <v-btn variant="text" icon="mdi-skip-forward" data-cy="skip-forward" />
   </menu>
 </template>
 
 <script setup>
+import { watch } from 'vue';
+import { useRouter } from 'vue-router';
+import { useGameHistoryStore } from '@/stores/gameHistory';
+import { useGameStore } from '@/stores/game';
+
+const router = useRouter();
+const gameHistoryStore = useGameHistoryStore();
+const gameStore = useGameStore();
+
+function stepForward() {
+  const route = router.currentRoute.value;
+  router.push({
+    ...route,
+    query: {
+      ...route.query,
+      gameStateIndex: gameHistoryStore.currentGameStateIndex + 1,
+    },
+  });
+}
+
+watch(() => gameHistoryStore.currentGameStateIndex, async (newVal) => {
+  console.log(`requesting gamestate ${newVal}`);
+  await gameStore.requestGameState(gameStore.id, newVal);
+});
 </script>
