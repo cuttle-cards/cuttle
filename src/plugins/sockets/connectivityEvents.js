51927285da19b348fd8bad8e89ae9c8207cf6d64
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-06-09 16:44:21 -0400

    fix(connectivityEvents): set gameStateIndex on reconnect for spectate

--- src/plugins/sockets/connectivityEvents.js
+++ src/plugins/sockets/connectivityEvents.js
@@ -1,11 +1,14 @@
 import { useAuthStore } from '@/stores/auth';
 import { useGameStore } from '@/stores/game';
+import { useGameHistoryStore } from '@/stores/gameHistory';
 import router from '@/router.js';
 import { ROUTE_NAME_GAME, ROUTE_NAME_SPECTATE, ROUTE_NAME_HOME } from '@/router';
 
 export async function handleConnect() {
   const authStore = useAuthStore();
   const gameStore = useGameStore();
+  const gameHistoryStore = useGameHistoryStore();
+
   // Request latest game state if socket reconnects during game
   switch (router.currentRoute.value.name) {
     case ROUTE_NAME_GAME: {
@@ -27,7 +30,8 @@ export async function handleConnect() {
         router.push(ROUTE_NAME_HOME);
         return;
       }
-      return gameStore.requestSpectate(gameId);
+
+      return gameStore.requestSpectate(gameId, gameHistoryStore.currentGameStateIndex);
     }
     default:
       return;
