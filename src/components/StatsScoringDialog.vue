48bff9cb1aa0829abba3b73f4669592bf111ec94
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-07-21 11:48:45 -0400

    Fix/stats scoring cards (#1252)
    * fix(StatsScoringDialog): correct references to ()
    
    * fix(StatsScoring): fix chip text translation key
    
    * refactor(StatsScoringDialog): move statsScoring translation block inside stats section

--- src/components/StatsScoringDialog.vue
+++ src/components/StatsScoringDialog.vue
@@ -3,7 +3,7 @@
     id="rankedScoring"
     v-model="show"
     :persistent="false"
-    :title="$t('global.statsScoring.title')"
+    :title="$t('stats.statsScoring.title')"
     :max-width="1250"
     :opacity="1"
   >
@@ -16,7 +16,7 @@
           variant="text"
           data-cy="ranked-info-button"
         >
-          <span v-if="showButtonText">{{ $t('global.statsScoring.button') }}</span>
+          <span v-if="showButtonText">{{ $t('stats.statsScoring.button') }}</span>
           <v-icon class="ml-1" icon="mdi-information-outline" aria-hidden="true" />
         </v-btn>
       </slot>
@@ -24,58 +24,58 @@
     <!-- Dialog -->
     <template #body>
       <p>
-        {{ $t('global.statsScoring.seasonDescription1a') }}
-        <strong>{{ $t('global.statsScoring.seasonDescription1b') }}</strong>
+        {{ $t('stats.statsScoring.seasonDescription1a') }}
+        <strong>{{ $t('stats.statsScoring.seasonDescription1b') }}</strong>
       </p>
       <div class="d-flex justify-space-around flex-wrap mt-4">
-        <AwardCard username="$t('global.statsScoring.award1')" :place="1" class="mb-4" />
-        <AwardCard username="$t('global.statsScoring.award2')" :place="2" class="mb-4" />
-        <AwardCard username="$t('global.statsScoring.award3')" :place="3" class="mb-4" />
+        <AwardCard :username="$t('stats.statsScoring.award1')" :place="1" class="mb-4" />
+        <AwardCard :username="$t('stats.statsScoring.award2')" :place="2" class="mb-4" />
+        <AwardCard :username="$t('stats.statsScoring.award3')" :place="3" class="mb-4" />
       </div>
       <p>
-        {{ $t('global.statsScoring.seasonDescription2') }}
+        {{ $t('stats.statsScoring.seasonDescription2') }}
       </p>
       <v-list class="mt-4" bg-color="surface-2" base-color="surface-1">
         <v-list-item>
           <v-chip variant="elevated" class="mr-2 mb-1" :color="theme.firstPlace">
-            {{ t('global.statsScoring.points.displayName', 5) }}
+            {{ $t('stats.statsScoring.displayName', 5) }}
           </v-chip>
-          {{ $t('global.statsScoring.points.5') }}
+          {{ $t('stats.statsScoring.points.5') }}
         </v-list-item>
         <v-list-item>
           <v-chip variant="elevated" class="mr-2 mb-1" :color="theme.secondPlace">
-            {{ t('global.statsScoring.points.displayName', 4) }}
+            {{ $t('stats.statsScoring.displayName', 4) }}
           </v-chip>
-          {{ $t('global.statsScoring.points.4') }}
+          {{ $t('stats.statsScoring.points.4') }}
         </v-list-item>
         <v-list-item>
           <v-chip variant="elevated" class="mr-2 mb-1" :color="theme.thirdPlace">
-            {{ t('global.statsScoring.points.displayName', 3) }}
+            {{ $t('stats.statsScoring.displayName', 3) }}
           </v-chip>
-          {{ $t('global.statsScoring.points.3') }}
+          {{ $t('stats.statsScoring.points.3') }}
         </v-list-item>
         <v-list-item>
           <v-chip variant="outlined" class="mr-2 mb-1" :color="theme.primary">
-            {{ t('global.statsScoring.points.displayName', 2) }}
+            {{ $t('stats.statsScoring.displayName', 2) }}
           </v-chip>
-          {{ $t('global.statsScoring.points.2') }}
+          {{ $t('stats.statsScoring.points.2') }}
         </v-list-item>
         <v-list-item>
           <v-chip variant="outlined" class="mr-2 mb-1" color="#000">
-            {{ t('global.statsScoring.points.displayName') }}
+            {{ $t('stats.statsScoring.displayName') }}
           </v-chip>
-          {{ $t('global.statsScoring.points.1') }}
+          {{ $t('stats.statsScoring.points.1') }}
         </v-list-item>
       </v-list>
       <p class="mt-4">
-        {{ $t('global.statsScoring.seasonDescription3') }}
-        <strong> {{ $t('global.statsScoring.lordOfDeep') }} </strong>
+        {{ $t('stats.statsScoring.seasonDescription3') }}
+        <strong> {{ $t('stats.statsScoring.lordOfDeep') }} </strong>
       </p>
       <!-- Actions -->
     </template>
     <template #actions>
       <v-btn variant="outlined" color="surface-1" @click="show = false">
-        {{ $t('global.statsScoring.gotIt') }}
+        {{ $t('stats.statsScoring.gotIt') }}
       </v-btn>
     </template>
   </BaseDialog>
