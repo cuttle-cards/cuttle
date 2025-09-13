4838f657c484de13905b44d8f767d5bf52c316a9
Henry Aviles <haviles04@gmail.com>
2025-09-13 09:06:45 -0400

    feat: add form validation rules, and loading

--- src/routes/home/components/OauthSignupDialog.vue
+++ src/routes/home/components/OauthSignupDialog.vue
@@ -4,9 +4,24 @@
       <h2 class="mb-6">
         {{ titleText }}
       </h2>
-      <v-form id="form" @submit.prevent="completeOauth">
-        <v-text-field v-model="username" label="Username" />
-        <v-text-field v-if="!noAccount" v-model="password" label="Password" />
+      <v-form id="form" v-model="isFormValid" @submit.prevent="completeOauth">
+        <v-text-field
+          v-model="username"
+          label="Username"
+          :rules="isAlphaNumeric"
+          :density="$vuetify.display.mdAndDown ? 'compact' : 'default'"
+        />
+        <v-text-field
+          v-if="!noAccount"
+          id="password"
+          v-model="password"
+          label="Password"
+          :rules="passwordRules"
+          :density="$vuetify.display.mdAndDown ? 'compact' : 'default'"
+          :type="showPass ? 'text' : 'password'"
+          :append-inner-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append-inner="showPass = !showPass"
+        />
       </v-form>
     </template>
     <template #actions>
@@ -14,7 +29,13 @@
         <v-btn variant="text" color="surface-1" @click="switchModes">
           {{ buttonText }}
         </v-btn>
-        <v-btn form="form" type="submit" color="newPrimary">
+        <v-btn
+          form="form"
+          type="submit"
+          :loading="loading"
+          :disabled="!isFormValid"
+          :color="isFormValid ? 'newPrimary' : 'disabled'"
+        >
           {{ submitText }}
         </v-btn>
       </div>
@@ -40,7 +61,13 @@ const provider = route.query.oauthsignup;
 
 const username = ref('');
 const password = ref('');
+const showPass = ref(false);
+const loading = ref(false);
 const noAccount = ref(false);
+const isFormValid = ref(false);
+
+const passwordRules = [ (val) => val.length >= 8 || 'Password must contain at least eight characters' ];
+const isAlphaNumeric = [ (val) => /^[\w.@-]+$/.test(val) || 'Username must contain only letters or numbers' ];
 
 const props = defineProps({
   modelValue: {
@@ -70,11 +97,12 @@ const buttonText = computed(() => (t(noAccount.value ?  'login.haveAccount' : 'l
 
 const completeOauth = async() => {
   try{
-    debugger;
+    loading.value = true;
     await authStore.completeOAuth(provider, { username: username.value, password:password.value });
     gameListStore.requestGameList();
     router.replace('/');
     show.value = false;
+    loading.value = false;
   }catch {
     router.push('/signup');
   }
