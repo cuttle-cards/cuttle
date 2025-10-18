052a54154605c6b06eac2951012e8b5deb9975a2
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-10-18 12:24:44 -0400

    style(StatsView): set background image

--- src/routes/stats/StatsView.vue
+++ src/routes/stats/StatsView.vue
@@ -1,109 +1,111 @@
 <template>
-  <template v-if="loadingData">
-    <v-skeleton-loader class="px-4 pa-2 mb-6 mx-auto border mb-2 mt-4" type="heading" />
-    <v-skeleton-loader
-      class="mx-auto border"
-      type="table-heading, table-row-divider, table-row, table-tbody,table-tfoot"
-    />
-  </template>
-  <template v-else>
-    <div id="stats-wrapper">
-      <!-- Upper Service -->
-      <section id="upper-surface" class="px-8 pt-8 mb-6">
-        <v-select
-          v-model="selectedSeason"
-          :items="seasons"
-          item-title="name"
-          return-object
-          :label="t('stats.season.select')"
-          data-cy="season-select"
-        >
-          <template #selection="{ item }">
-            <h1 class="text-h2" data-cy="selected-season-header">
-              {{ item.title }}
-            </h1>
+  <div id="stats-page-wrapper">
+    <template v-if="loadingData">
+      <v-skeleton-loader class="px-4 pa-2 mb-6 mx-auto border mb-2 mt-4" type="heading" />
+      <v-skeleton-loader
+        class="mx-auto border"
+        type="table-heading, table-row-divider, table-row, table-tbody,table-tfoot"
+      />
+    </template>
+    <template v-else>
+      <div id="stats-wrapper">
+        <!-- Upper Service -->
+        <section id="upper-surface" class="px-8 pt-8 mb-6">
+          <v-select
+            v-model="selectedSeason"
+            :items="seasons"
+            item-title="name"
+            return-object
+            :label="t('stats.season.select')"
+            data-cy="season-select"
+          >
+            <template #selection="{ item }">
+              <h1 class="text-h2" data-cy="selected-season-header">
+                {{ item.title }}
+              </h1>
+            </template>
+          </v-select>
+          <h4 v-if="selectedSeason" class="text-h4">
+            <span data-cy="season-start-date">{{ seasonStartFormatted }}</span>
+            -
+            <span data-cy="season-end-date"> {{ seasonEndFormatted }} </span>
+          </h4>
+        </section>
+        <section class="px-8">
+          <!-- Season Champions -->
+          <div class="mb-10">
+            <h2 v-if="showSeasonChampions" class="text-h2 mb-4">
+              {{ t('stats.season.champions') }}
+            </h2>
+            <div class="d-flex justify-space-around flex-wrap">
+              <AwardCard
+                v-if="selectedSeason && selectedSeason.firstPlace"
+                :username="selectedSeason.firstPlace"
+                :place="1"
+                class="mb-4"
+              />
+              <AwardCard
+                v-if="selectedSeason && selectedSeason.secondPlace"
+                :username="selectedSeason.secondPlace"
+                :place="2"
+                class="mb-4"
+              />
+              <AwardCard
+                v-if="selectedSeason && selectedSeason.thirdPlace"
+                :username="selectedSeason.thirdPlace"
+                :place="3"
+                class="mb-4"
+              />
+            </div>
+            <p v-if="selectedSeason && selectedSeason.bracketLink" class="text-body-1">
+              {{ t('global.click') }}
+              <a :href="selectedSeason.bracketLink" target="_blank" data-cy="tournament-bracket-link">
+                {{ t('stats.season.tournamentBracket') }}
+              </a>
+            </p>
+            <p v-if="selectedSeason && selectedSeason.footageLink" class="text-body-1">
+              {{ t('global.click') }}
+              <a :href="selectedSeason.footageLink" target="_blank" data-cy="tournament-footage-link">
+                {{ t('stats.season.tournamentFootage') }}
+              </a>
+              {{ t('stats.season.playByPlay') }}
+            </p>
+          </div>
+          <!-- Rankings Table -->
+          <template v-if="weeklyRanking">
+            <h2 class="text-h2 mb-4">
+              {{ t('stats.weeklyRankings') }}
+              <StatsScoringDialog />
+            </h2>
+            <StatsLeaderboard :loading="loadingData" :season="selectedSeason" />
           </template>
-        </v-select>
-        <h4 v-if="selectedSeason" class="text-h4">
-          <span data-cy="season-start-date">{{ seasonStartFormatted }}</span>
-          -
-          <span data-cy="season-end-date"> {{ seasonEndFormatted }} </span>
-        </h4>
-      </section>
-      <section class="px-8">
-        <!-- Season Champions -->
-        <div class="mb-10">
-          <h2 v-if="showSeasonChampions" class="text-h2 mb-4">
-            {{ t('stats.season.champions') }}
-          </h2>
-          <div class="d-flex justify-space-around flex-wrap">
-            <AwardCard
-              v-if="selectedSeason && selectedSeason.firstPlace"
-              :username="selectedSeason.firstPlace"
-              :place="1"
-              class="mb-4"
-            />
-            <AwardCard
-              v-if="selectedSeason && selectedSeason.secondPlace"
-              :username="selectedSeason.secondPlace"
-              :place="2"
-              class="mb-4"
-            />
-            <AwardCard
-              v-if="selectedSeason && selectedSeason.thirdPlace"
-              :username="selectedSeason.thirdPlace"
-              :place="3"
-              class="mb-4"
+  
+          <!-- Usage stats -->
+          <div v-if="selectedSeason" id="usage-stats-section">
+            <h2 class="text-h2 mt-8 mb-4">
+              {{ t('stats.siteUsage') }}
+            </h2>
+            <StatsUsageChart v-if="showStatsUsageChart" :season="selectedSeason" />
+          </div>
+          <!-- Error display -->
+          <div v-if="error" class="d-flex flex-column align-center text-center">
+            <h3 class="text-h3">
+              {{ t('stats.error.oops') }}
+            </h3>
+            <p class="text-body-1">
+              {{ t('stats.error.message') }}
+            </p>
+            <v-img
+              alt="Dead cuttle logo"
+              src="/img/logo-dead.svg"
+              :width="200"
+              class="mt-4"
             />
           </div>
-          <p v-if="selectedSeason && selectedSeason.bracketLink" class="text-body-1">
-            {{ t('global.click') }}
-            <a :href="selectedSeason.bracketLink" target="_blank" data-cy="tournament-bracket-link">
-              {{ t('stats.season.tournamentBracket') }}
-            </a>
-          </p>
-          <p v-if="selectedSeason && selectedSeason.footageLink" class="text-body-1">
-            {{ t('global.click') }}
-            <a :href="selectedSeason.footageLink" target="_blank" data-cy="tournament-footage-link">
-              {{ t('stats.season.tournamentFootage') }}
-            </a>
-            {{ t('stats.season.playByPlay') }}
-          </p>
-        </div>
-        <!-- Rankings Table -->
-        <template v-if="weeklyRanking">
-          <h2 class="text-h2 mb-4">
-            {{ t('stats.weeklyRankings') }}
-            <StatsScoringDialog />
-          </h2>
-          <StatsLeaderboard :loading="loadingData" :season="selectedSeason" />
-        </template>
-
-        <!-- Usage stats -->
-        <div v-if="selectedSeason" id="usage-stats-section">
-          <h2 class="text-h2 mt-8 mb-4">
-            {{ t('stats.siteUsage') }}
-          </h2>
-          <StatsUsageChart v-if="showStatsUsageChart" :season="selectedSeason" />
-        </div>
-        <!-- Error display -->
-        <div v-if="error" class="d-flex flex-column align-center text-center">
-          <h3 class="text-h3">
-            {{ t('stats.error.oops') }}
-          </h3>
-          <p class="text-body-1">
-            {{ t('stats.error.message') }}
-          </p>
-          <v-img
-            alt="Dead cuttle logo"
-            src="/img/logo-dead.svg"
-            :width="200"
-            class="mt-4"
-          />
-        </div>
-      </section>
-    </div>
-  </template>
+        </section>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -235,15 +237,18 @@ export default {
 </script>
 
 <style scoped>
+#stats-page-wrapper {
+  background: url('/img/game/board-background.webp') repeat-y;
+  background-size: 100% auto;
+  background-attachment: fixed; /* Optional: keeps background fixed during scroll */
+}
 #stats-wrapper h1 {
   background: linear-gradient(268.89deg, rgba(98, 2, 238, 0.87) 73.76%, rgba(253, 98, 34, 0.87) 99.59%);
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
-#upper-surface {
-  background-color: #f3f3f3;
-}
+
 .filter-select {
   width: 50%;
 }
