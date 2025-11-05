7340a003421cbde89693517c741dfd0e5cb8ade1
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-11-05 15:19:38 -0500

    chore(StatsView): format newlines

--- src/routes/stats/StatsView.vue
+++ src/routes/stats/StatsView.vue
@@ -70,7 +70,11 @@
                 {{ t('stats.season.tournamentBracket') }}
               </a>
             </p>
-            <div v-if="selectedSeason && selectedSeason.footageLink" id="tournament-video" class="d-flex flex-column align-items-center">
+            <div
+              v-if="selectedSeason && selectedSeason.footageLink"
+              id="tournament-video"
+              class="d-flex flex-column align-items-center"
+            >
               <h3 class="text-h3 text-center mt-4">
                 Tournament Video
               </h3>
