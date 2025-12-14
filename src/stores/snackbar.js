14a159c68d54118454cd9ab5ca2376c3189b46cd
Jason Wu <jsonwu34@gmail.com>
2025-12-14 15:07:59 -0500

    refactor(snackbar.js): added timeout, alert shows snackbar, clear removes message

--- src/stores/snackbar.js
+++ src/stores/snackbar.js
@@ -5,14 +5,18 @@ export const useSnackbarStore = defineStore('snackbar', () => {
   const showSnackbar = ref(false);
   const snackMessage = ref('');
   const snackColor = ref('error');
+  const snackTimeout = ref(0);
 
-  function alert(message, color = 'error') {
+  function alert(message, color = 'error', timeout = 0) {
     snackColor.value = color;
     snackMessage.value = message;
+    snackTimeout = timeout;
+    showSnackbar = true;
   }
 
   function clear() {
     showSnackbar.value = false;
+    snackMessage.value = '';
   }
 
   return {
