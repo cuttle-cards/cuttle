d0d17cff3bb3aa3bb65cef7a94d52cb6d2993020
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-12-18 11:41:43 -0500

    feat(ThreeDialog.vue): enable minimizing dialog

--- src/routes/game/components/dialogs/components/ThreeDialog.vue
+++ src/routes/game/components/dialogs/components/ThreeDialog.vue
@@ -3,9 +3,35 @@
     v-if="oneOff"
     id="three-dialog"
     v-model="show"
-    :title="t('game.dialogs.threeDialog.title')"
     scrollable
   >
+    <template v-if="isMinimized" #activator>
+      <v-fab
+        color="primary"
+        variant="flat"
+        absolute
+        location="center center"
+        size="large"
+        transition="scale-transition"
+        data-cy="three-dialog-activator"
+        @click="isMinimized = false"
+      >
+        {{ t('game.dialogs.threeDialog.title') }}
+      </v-fab>
+    </template>
+    <template #title>
+      <div class="d-flex justify-space-between align-center w-100">
+        <h1>{{ t('game.dialogs.threeDialog.title') }}</h1>
+        <v-btn
+          icon="mdi-window-minimize"
+          color="surface-2"
+          variant="text"
+          data-cy="close-three-dialog-x"
+          aria-label="Close three dialog"
+          @click="minimizeDialog"
+        />
+      </div>
+    </template>
     <template #body>
       <div class="d-flex flex-wrap justify-center align-center my-8">
         <CardListSortable
@@ -32,6 +58,7 @@
 </template>
 
 <script>
+import { ref, computed, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import BaseDialog from '@/components/BaseDialog.vue';
 import CardListSortable from '@/routes/game/components/CardListSortable.vue';
@@ -58,48 +85,68 @@ export default {
     },
   },
   emits: [ 'resolveThree' ],
-  setup() {
+  setup(props, { emit }) {
     const { t } = useI18n();
-    return { t };
-  },
-  data() {
-    return {
-      choseToCounter: false,
-      selectedCard: null,
-    };
-  },
-  computed: {
-    show: {
+    const isMinimized = ref(false);
+    const selectedCard = ref(null);
+
+    // Dialog shows only when modelValue is true AND isMinimized is false
+    const show = computed({
       get() {
-        return this.modelValue;
+        return props.modelValue && !isMinimized.value;
       },
       set() {
         // do nothing - parent controls whether dialog is open
       },
-    },
-    selectedIds() {
+    });
+
+    // Reset isMinimized when modelValue becomes true (dialog reopened via activator)
+    watch(() => props.modelValue, (newValue) => {
+      if (newValue) {
+        isMinimized.value = false;
+      }
+    });
+
+    const minimizeDialog = () => {
+      isMinimized.value = true;
+    };
+
+    const selectedIds = computed(() => {
       const res = [];
-      if (this.selectedCard) {
-        res.push(this.selectedCard.id);
+      if (selectedCard.value) {
+        res.push(selectedCard.value.id);
       }
       return res;
-    },
-  },
-  methods: {
-    moveToHand() {
-      this.$emit('resolveThree', this.selectedCard.id);
-      this.clearSelection();
-    },
-    selectCard(card) {
-      if (this.selectedCard && card.id === this.selectedCard.id) {
-        this.clearSelection();
+    });
+
+    const moveToHand = () => {
+      emit('resolveThree', selectedCard.value.id);
+      clearSelection();
+    };
+
+    const selectCard = (card) => {
+      if (selectedCard.value && card.id === selectedCard.value.id) {
+        clearSelection();
       } else {
-        this.selectedCard = card;
+        selectedCard.value = card;
       }
-    },
-    clearSelection() {
-      this.selectedCard = null;
-    },
+    };
+
+    const clearSelection = () => {
+      selectedCard.value = null;
+    };
+
+    return {
+      t,
+      show,
+      isMinimized,
+      selectedCard,
+      selectedIds,
+      minimizeDialog,
+      moveToHand,
+      selectCard,
+      clearSelection,
+    };
   },
 };
 </script>
