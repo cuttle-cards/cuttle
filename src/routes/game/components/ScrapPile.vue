c3b6949c6a98c8fdd1a88c82b37e97da9cc65b6c
Ryan Emberling <itsalaidbacklife@gmail.com>
2025-12-31 09:50:17 -0500

    chore(ScrapPile): lint

--- src/routes/game/components/ScrapPile.vue
+++ src/routes/game/components/ScrapPile.vue
@@ -273,10 +273,10 @@ function openDialog(e) {
     opacity: 0;
   }
 
-#scrap .scrap-card.threes-player-enter-from,
-#scrap .scrap-card.threes-opponent-enter-from {
-  transform: translateY(-200px);
-  opacity: 0;
-}
+  #scrap .scrap-card.threes-player-enter-from,
+  #scrap .scrap-card.threes-opponent-enter-from {
+    transform: translateY(-200px);
+    opacity: 0;
+  }
 }
 </style>
