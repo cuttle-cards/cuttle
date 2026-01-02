a365a6bc5d63c26fef75244f2844089c0601a4dc
Ryan Emberling <itsalaidbacklife@gmail.com>
2026-01-02 11:25:11 -0500

    feat(ai-move.js): add validation

---
+++ api/controllers/game/ai/ai-move.js
@@ -0,0 +1,55 @@
+// Request to have the AI make a move
+const CustomErrorType = require('../../../errors/customErrorType');
+const NotFoundError = require('../../../errors/notFoundError');
+const ForbiddenError = require('../../../errors/forbiddenError');
+const BadRequestError = require('../../../errors/badRequestError');
+const ConflictError = require('../../../errors/conflictError');
+const GameStatus = require('../../../../utils/GameStatus');
+
+module.exports = async function (req, res) {
+  let game;
+  const { saveGamestate, publishGameState, unpackGamestate } = sails.helpers.gameStates;
+  try {
+    ///////////////////////////
+    // Fetch and format data //
+    ///////////////////////////
+    game = await sails.helpers.lockGame(req.params.gameId);
+
+    //////////////////////
+    // Validate Request //
+    //////////////////////
+    if (!game) {
+      throw new NotFoundError(`Could not find game ${ req.params.gameId }`);
+    }
+
+    if (!game.isVsAi) {
+      throw new BadRequestError('This game is pvp!');
+    }
+
+    const gameState = unpackGamestate(game.gameStates.at(-1));
+    // Game must be in progress
+    if (!game?.gameStates?.length || !gameState) {
+      throw new BadRequestError('Game has not yet started');
+    }
+
+    // Requesting user must be in this game
+    let playedBy;
+    switch (req.session.usr) {
+      case game.p0.id:
+        playedBy = 0;
+        break;
+      case game.p1.id:
+        playedBy = 1;
+        break;
+      default:
+        throw new ForbiddenError('You are not a player in this game!');
+    }
+
+    if ([ GameStatus.FINISHED, GameStatus.ARCHIVED ].includes(game.status)) {
+      throw new ConflictError('game', game.id, 'Can\'t make move: this game is over');
+    }
+  } catch (err) {
+    return res.serverError(err);
+  }
+  return res.ok();
+};
