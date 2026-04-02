4291347f498306973302639f3eb65692ecce6b9b
Ryan Emberling <ryan.emberling@gmail.com>
2026-04-01 22:34:51 -0400

    feat(announcementDialog): add clubs2026Invite

--- src/routes/home/components/announcementDialog/data/announcementData.js
+++ src/routes/home/components/announcementDialog/data/announcementData.js
@@ -1,19 +1,22 @@
 import { Card } from '../../../../../../tests/e2e/fixtures/cards';
 
 export const announcementData = {
-  id: 'noThreeForThree',
+  id: 'clubs2025Participants',
   activatorText: 'announcement.activatorText',
   title: 'announcement.title',
   displayCards: [ Card.THREE_OF_CLUBS, Card.THREE_OF_DIAMONDS, Card.THREE_OF_HEARTS, Card.THREE_OF_SPADES ],
   // imgSrc: '/img/announcement/clubs_2025_cuttle_season_championship.svg',
-  startTime: '2026-01-14',
+  startTime: '2026-04-01',
   // endTime: '2025-04-27',
   announcementText: [
     {
       heading: 'announcement.heading',
       paragraph: [
         { text: 'announcement.paragraph' },
-        // { text: 'announcement.twitchLink', url: 'https://twitch.tv/cuttle_cards' }
+        { text: 'announcement.when2MeetLink', url: 'https://twitch.tv/cuttle_cards' },
+        { text: 'announcement.paragraph2' },
+        { text: 'announcement.discordLink', url: 'https://discord.gg/TrT6qkJJ5u' },
+        { text: 'announcement.paragraph3' },
       ],
     },
   ],
