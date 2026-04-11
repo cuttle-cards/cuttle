6fc826e99b60ae35ac87e7452ec6f08586d07a92
Ryan Emberling <ryan.emberling@gmail.com>
2026-04-11 12:19:50 -0400

    feat: create cwc5 announcement

--- src/routes/home/components/announcementDialog/data/announcementData.js
+++ src/routes/home/components/announcementDialog/data/announcementData.js
@@ -1,23 +1,20 @@
 // import { Card } from '../../../../../../tests/e2e/fixtures/cards';
 
 export const announcementData = {
-  id: 'clubs2026Participants',
+  id: 'cwc2026',
   activatorText: 'announcement.activatorText',
   title: 'announcement.title',
   displayCards: [],
-  // imgSrc: '/img/announcement/clubs_2025_cuttle_season_championship.svg',
-  startTime: '2026-04-01',
-  endTime: '2026-04-10',
-  userIds: [ 1879, 37341, 204, 887, 1058, 456, 1076, 212 ],
+  imgSrc: '/img/announcement/cuttle-world-championship-v-bracket.svg',
+  startTime: '2026-04-11',
+  endTime: '2026-04-12',
+  // userIds: [ 1879, 37341, 204, 887, 1058, 456, 1076, 212 ],
   announcementText: [
     {
       heading: 'announcement.heading',
-      paragraph: [
+      paragraph: [ 
         { text: 'announcement.paragraph' },
-        { text: 'announcement.when2MeetLink', url: 'https://www.when2meet.com/?35931369-XljOu' },
-        { text: 'announcement.paragraph2' },
-        { text: 'announcement.discordLink', url: 'https://discord.gg/TrT6qkJJ5u' },
-        { text: 'announcement.paragraph3' },
+        { text: 'announcement.twitchLink', url: 'https://twitch.tv/cuttle_cards' }
       ],
     },
   ],
