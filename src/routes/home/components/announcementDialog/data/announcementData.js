54d8ecbcfaac6faecbf9e9be6847309cbad0147e
Ryan Emberling <ryan.emberling@gmail.com>
2026-08-26 20:48:25 -0400

    feat(announcement): add diamongs 2026

--- src/routes/home/components/announcementDialog/data/announcementData.js
+++ src/routes/home/components/announcementDialog/data/announcementData.js
@@ -1,20 +1,21 @@
 // import { Card } from '../../../../../../tests/e2e/fixtures/cards';
 
 export const announcementData = {
-  id: 'handLimitRework',
+  id: 'diamonds2026Announcement',
   activatorText: 'announcement.activatorText',
   title: 'announcement.title',
   displayCards: [],
+  imgSrc: '/img/announcement/diamonds_2026_cuttle_season_championship.svg',
+  startTime: '2026-08-25',
+  endTime: '2026-08-31',
   announcementText: [
     {
       heading: 'announcement.heading',
       paragraph: [
         { text: 'announcement.paragraph' },
-      ],
-    },
-    {
-      paragraph: [
-        { text: 'announcement.paragraph2' },
+        { text: 'announcement.twitchLink', url: 'https://www.twitch.tv/cuttle_cards' },
+        { text: 'announcement.linkSeparator' },
+        { text: 'announcement.youtubeLink', url: 'https://www.youtube.com/live/mMYULGUXrfs' },
       ],
     },
   ],
